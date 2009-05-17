b092596c410c1b6a2ec97ef0401aba41ee475aed
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-05-17 18:37:16 +0000

    Adding all the config files for the core of Silverpeas
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@9 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

---
+++ config-core/src/main/config/properties/com/stratelia/silverpeas/wysiwyg/settings/myconfig.js
@@ -0,0 +1,15 @@
+FCKConfig.ToolbarSets["Default"] = [
+	['Source','-','Save','NewPage','Preview'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	'/',
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['Image','Flash','Table','Rule','Smiley','SpecialChar','UniversalKey'],
+	'/',
+	['Style','FontFormat','FontName','FontSize'],
+	['TextColor','BGColor'],
+	['About']
+] ;
