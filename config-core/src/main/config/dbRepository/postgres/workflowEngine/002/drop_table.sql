93ad81a2a87ca20c5b83d4528fca08bf7378b004
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-05-27 13:23:16 +0000

    @add:;Workflow Engine Configuration
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@26 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

---
+++ config-core/src/main/config/dbRepository/postgres/workflowEngine/002/drop_table.sql
@@ -0,0 +1,32 @@
+DROP TABLE SB_Workflow_ProcessInstance
+;
+
+DROP TABLE SB_Workflow_ActiveState
+;
+
+DROP TABLE SB_Workflow_HistoryStep
+;
+
+DROP TABLE SB_Workflow_Undo_Step
+;
+
+DROP TABLE SB_Workflow_InterestedUser
+;
+
+DROP TABLE SB_Workflow_LockingUser
+;
+
+DROP TABLE SB_Workflow_WorkingUser
+;
+
+DROP TABLE SB_Workflow_Question
+;
+
+DROP TABLE SB_Workflow_UserInfo
+;
+
+DROP TABLE SB_Workflow_UserSettings
+;
+
+DROP TABLE SB_Workflow_Error
+;
