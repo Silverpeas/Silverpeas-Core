4d776cde3e44e52e593188c5ee2b81a29402e89b
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-08-14 13:00:55 +0000

    Removed some warnings
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@101 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- web-core/src/main/java/com/stratelia/webactiv/util/viewGenerator/html/buttons/ButtonTag.java
+++ web-core/src/main/java/com/stratelia/webactiv/util/viewGenerator/html/buttons/ButtonTag.java
@@ -9,70 +9,71 @@ import com.stratelia.webactiv.util.viewGenerator.html.GraphicElementFactory;
 
 public class ButtonTag extends TagSupport {
 
-	private String label = "";
-	private String action = "#";
-	private boolean disabled = false;
+  private static final long serialVersionUID = 1L;
+  private String label = "";
+  private String action = "#";
+  private boolean disabled = false;
 
-	public int doEndTag() throws JspException {
-		try {
-			GraphicElementFactory gef = (GraphicElementFactory) pageContext
-					.getSession().getAttribute(
-							GraphicElementFactory.GE_FACTORY_SESSION_ATT);
-			Button button = gef.getFormButton(label, action, disabled);
-			pageContext.getOut().println(button.print());
-		} catch (IOException e) {
-			throw new JspException("ButtonTag Tag", e);
-		}
-		return EVAL_PAGE;
-	}
+  @Override
+  public int doEndTag() throws JspException {
+    try {
+      GraphicElementFactory gef = (GraphicElementFactory) pageContext.getSession().getAttribute(
+          GraphicElementFactory.GE_FACTORY_SESSION_ATT);
+      Button button = gef.getFormButton(label, action, disabled);
+      pageContext.getOut().println(button.print());
+    } catch (IOException e) {
+      throw new JspException("ButtonTag Tag", e);
+    }
+    return EVAL_PAGE;
+  }
 
-	public int doStartTag() throws JspException {
-		return EVAL_BODY_INCLUDE;
-	}
+  @Override
+  public int doStartTag() throws JspException {
+    return EVAL_BODY_INCLUDE;
+  }
 
-	/**
-	 * @return the label
-	 */
-	public String getLabel() {
-		return label;
-	}
+  /**
+   * @return the label
+   */
+  public String getLabel() {
+    return label;
+  }
 
-	/**
-	 * @param label
-	 *            the label to set
-	 */
-	public void setLabel(String label) {
-		this.label = label;
-	}
+  /**
+   * @param label
+   *            the label to set
+   */
+  public void setLabel(String label) {
+    this.label = label;
+  }
 
-	/**
-	 * @return the action
-	 */
-	public String getAction() {
-		return action;
-	}
+  /**
+   * @return the action
+   */
+  public String getAction() {
+    return action;
+  }
 
-	/**
-	 * @param action
-	 *            the action to set
-	 */
-	public void setAction(String action) {
-		this.action = action;
-	}
+  /**
+   * @param action
+   *            the action to set
+   */
+  public void setAction(String action) {
+    this.action = action;
+  }
 
-	/**
-	 * @return the disabled
-	 */
-	public boolean isDisabled() {
-		return disabled;
-	}
+  /**
+   * @return the disabled
+   */
+  public boolean isDisabled() {
+    return disabled;
+  }
 
-	/**
-	 * @param disabled
-	 *            the disabled to set
-	 */
-	public void setDisabled(boolean disabled) {
-		this.disabled = disabled;
-	}
-
+  /**
+   * @param disabled
+   *            the disabled to set
+   */
+  public void setDisabled(boolean disabled) {
+    this.disabled = disabled;
+  }
 }
