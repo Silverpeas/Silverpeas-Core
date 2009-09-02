905adae85f9a0f5217387af92dbfcc6a1f3faa25
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-09-02 14:51:17 +0000

    Adding missing classes for myLinks
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@129 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

---
+++ war-core/src/main/java/com/stratelia/silverpeas/myLinksPeas/control/MyLinksPeasSessionController.java
@@ -0,0 +1,261 @@
+package com.silverpeas.myLinksPeas.control;
+
+import java.util.Collection;
+
+import com.silverpeas.myLinks.MyLinksRuntimeException;
+import com.silverpeas.myLinks.ejb.MyLinksBm;
+import com.silverpeas.myLinks.ejb.MyLinksBmHome;
+import com.silverpeas.myLinks.model.LinkDetail;
+import com.stratelia.silverpeas.peasCore.AbstractComponentSessionController;
+import com.stratelia.silverpeas.peasCore.ComponentContext;
+import com.stratelia.silverpeas.peasCore.MainSessionController;
+import com.stratelia.silverpeas.silvertrace.SilverTrace;
+import com.stratelia.webactiv.util.EJBUtilitaire;
+import com.stratelia.webactiv.util.JNDINames;
+import com.stratelia.webactiv.util.exception.SilverpeasException;
+import com.stratelia.webactiv.util.exception.SilverpeasRuntimeException;
+
+public class MyLinksPeasSessionController extends AbstractComponentSessionController
+{
+	public static final int SCOPE_USER = 0;
+	public static final int SCOPE_COMPONENT = 1;
+	public static final int SCOPE_OBJECT = 2;
+	
+	private int scope = SCOPE_USER;
+	
+	private String url = null;
+	private String instanceId = null;
+	private String objectId = null;
+	
+    /**
+     * Standard Session Controller Constructeur
+     *
+     *
+     * @param mainSessionCtrl   The user's profile
+     * @param componentContext  The component's profile
+     *
+     * @see
+     */
+	public MyLinksPeasSessionController(MainSessionController mainSessionCtrl, ComponentContext componentContext)
+	{
+		super(mainSessionCtrl, componentContext,  "com.silverpeas.mylinks.multilang.myLinksBundle", "com.silverpeas.mylinks.settings.myLinksIcons");
+	}
+	
+	public Collection getAllLinksByUser() 
+	{
+		Collection links = null;
+		try
+		{
+			links = getMyLinksBm().getAllLinksByUser(getUserId());
+			SilverTrace.debug(
+					"myLinks",
+					"MyLinksPeasSessionController.getAllLinksByUser()",
+					"root.MSG_GEN_PARAM_VALUE",
+					"nombre de liens = " + links.size());					
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.getAllLinksByUser",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+		return links;
+	}
+	
+	public Collection getAllLinksByInstance() 
+	{
+		Collection links = null;
+		try
+		{
+			links = getMyLinksBm().getAllLinksByInstance(instanceId);
+			SilverTrace.debug(
+					"myLinks",
+					"MyLinksPeasSessionController.getAllLinksByInstance()",
+					"root.MSG_GEN_PARAM_VALUE",
+					"nombre de liens = " + links.size() + " instanceId = " + instanceId);					
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.getAllLinksByInstance",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+		return links;
+	}
+	
+	public Collection getAllLinksByObject() 
+	{
+		Collection links = null;
+		try
+		{
+			links = getMyLinksBm().getAllLinksByObject(instanceId, objectId);
+			SilverTrace.debug(
+					"myLinks",
+					"MyLinksPeasSessionController.getAllLinksByObject()",
+					"root.MSG_GEN_PARAM_VALUE",
+					"nombre de liens = " + links.size());					
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.getAllLinksByObject",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+		return links;
+	}
+	
+	public LinkDetail getLink(String linkId) 
+	{
+		LinkDetail link = null;
+		try
+		{
+			link = getMyLinksBm().getLink(linkId);
+			SilverTrace.debug(
+					"myLinks",
+					"MyLinksPeasSessionController.getLink()",
+					"root.MSG_GEN_PARAM_VALUE",
+					"linkId = " + linkId);					
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.getLink",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+		return link;
+	}
+	
+	public void createLink(LinkDetail link)
+	{
+		
+		try
+		{
+			String userId = getUserId();
+			link.setUserId(userId);
+			
+			//ajout de l'instanceId si existe
+			if (instanceId != null)
+				link.setInstanceId(instanceId);
+			
+			getMyLinksBm().createLink(link);
+			SilverTrace.debug(
+					"myLinks",
+					"MyLinksPeasSessionController.createLink()",
+					"root.MSG_GEN_PARAM_VALUE",
+					"liens = " + link.toString());	
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.createLink",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+	}
+	
+	public void updateLink(LinkDetail link)
+	{
+		try
+		{
+			//ajout de l'instanceId si existe
+			if (instanceId != null)
+				link.setInstanceId(instanceId);
+			
+			String userId = getUserId();
+			link.setUserId(userId);
+			getMyLinksBm().updateLink(link);
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.updateLink",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+	}
+	
+	public void deleteLinks(String[] links) 
+	{
+		try
+		{
+			getMyLinksBm().deleteLinks(links);
+			SilverTrace.debug(
+					"myLinks",
+					"MyLinksPeasSessionController.deleteLink()",
+					"root.MSG_GEN_PARAM_VALUE",
+					"links = " + links.toString());	
+		}
+		catch (Exception e)
+		{
+			// traitement des exceptions
+			throw new MyLinksRuntimeException(
+					"MyLinksPeasSessionController.createLink",
+					SilverpeasException.ERROR, "root.EX_RECORD_NOT_FOUND", e);
+		}
+	}
+	
+		
+	private MyLinksBm getMyLinksBm()
+	{
+		MyLinksBm myLinksBm = null;
+		try
+		{
+			MyLinksBmHome myLinksBmHome = (MyLinksBmHome) EJBUtilitaire
+					.getEJBObjectRef(JNDINames.MYLINKSBM_EJBHOME,
+							MyLinksBmHome.class);
+			myLinksBm = myLinksBmHome.create();
+		}
+		catch (Exception e)
+		{
+			throw new MyLinksRuntimeException("MyLInksSessionController.getMyLInksBm()",
+					SilverpeasRuntimeException.ERROR, "root.EX_CANT_GET_REMOTE_OBJECT",
+					e);
+		}
+		return myLinksBm;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getInstanceId() {
+		return instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+		scope = SCOPE_COMPONENT;
+	}
+
+	public String getObjectId() {
+		return objectId;
+	}
+
+	public void setObjectId(String objectId) {
+		this.objectId = objectId;
+		scope = SCOPE_OBJECT;
+	}
+	
+	public void setScope(int scope)
+	{
+		this.scope = scope;
+		if (scope == SCOPE_USER)
+		{
+			instanceId = null;
+        	objectId = null;
+		}		
+	}
+
+	public int getScope() {
+		return scope;
+	}
+	
+}
---
+++ war-core/src/main/java/com/stratelia/silverpeas/myLinksPeas/servlets/MyLinksPeasRequestRouter.java
@@ -0,0 +1,213 @@
+package com.silverpeas.myLinksPeas.servlets;
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpUtils;
+
+import com.silverpeas.myLinks.model.LinkDetail;
+import com.silverpeas.myLinksPeas.control.MyLinksPeasSessionController;
+import com.stratelia.silverpeas.peasCore.ComponentContext;
+import com.stratelia.silverpeas.peasCore.ComponentSessionController;
+import com.stratelia.silverpeas.peasCore.MainSessionController;
+import com.stratelia.silverpeas.peasCore.URLManager;
+import com.stratelia.silverpeas.peasCore.servlets.ComponentRequestRouter;
+import com.stratelia.silverpeas.silvertrace.SilverTrace;
+
+public class MyLinksPeasRequestRouter extends ComponentRequestRouter
+{
+    /**
+     * This method has to be implemented in the component request rooter class.
+     * returns the session control bean name to be put in the request object
+     * ex : for almanach, returns "almanach"
+     */
+    public String getSessionControlBeanName()
+	 {
+	    return "MyLinks";
+	 }
+
+    /**
+     * Method declaration
+     *
+     *
+     * @param mainSessionCtrl
+     * @param componentContext
+     *
+     * @return
+     *
+     * @see
+     */
+    public ComponentSessionController createComponentSessionController(MainSessionController mainSessionCtrl, ComponentContext componentContext)
+    {
+        return new MyLinksPeasSessionController(mainSessionCtrl, componentContext);
+    }
+
+    /**
+     * This method has to be implemented by the component request rooter
+     * it has to compute a destination page
+     * @param function The entering request function (ex : "Main.jsp")
+     * @param componentSC The component Session Control, build and initialised.
+     * @return The complete destination URL for a forward (ex : "/almanach/jsp/almanach.jsp?flag=user")
+     */
+    public String getDestination(String function, ComponentSessionController componentSC, HttpServletRequest request)
+    {
+        MyLinksPeasSessionController  myLinksSC = (MyLinksPeasSessionController)componentSC;
+        SilverTrace.info("myLinksPeas", "MyLinksPeasRequestRouter.getDestination()", "root.MSG_GEN_PARAM_VALUE", "User=" + componentSC.getUserId() + " Function=" + function);
+
+        String destination = "";
+		String rootDest = "/myLinksPeas/jsp/";
+		
+        try
+        {
+            if (function.startsWith("Main"))
+            {
+            	myLinksSC.setScope(MyLinksPeasSessionController.SCOPE_USER);
+            	myLinksSC.setUrl(null);
+            	
+            	destination = getDestination("ViewLinks", myLinksSC, request);
+            }
+            else if (function.equals("ComponentLinks"))
+            {
+            	//recupere l'id de l'instance
+            	String instanceId = (String) request.getParameter("InstanceId");
+            	String url = (String) request.getParameter("UrlReturn");
+            	myLinksSC.setInstanceId(instanceId);
+            	myLinksSC.setUrl(url);
+            	request.setAttribute("UrlReturn", url);
+            	request.setAttribute("InstanceId", instanceId);
+            	
+ 	            destination = getDestination("ViewLinks", myLinksSC, request);
+            }
+            else if (function.equals("ObjectLinks"))
+            {
+            	//recupere l'id de l'objet et de l'instance
+            	String objectId = (String) request.getParameter("ObjectId");
+            	String instanceId = (String) request.getParameter("InstanceId");
+            	String url = (String) request.getParameter("UrlReturn");
+            	myLinksSC.setUrl(url);
+            	myLinksSC.setInstanceId(instanceId);
+            	myLinksSC.setObjectId(objectId);
+            	request.setAttribute("UrlReturn", url);
+            	
+ 	            destination = getDestination("ViewLinks", myLinksSC, request);
+            }
+            else if (function.equals("ViewLinks"))
+            {
+            	Collection links = null;
+            	int scope = myLinksSC.getScope();
+            	switch (scope) {
+					case MyLinksPeasSessionController.SCOPE_COMPONENT:
+						links = myLinksSC.getAllLinksByInstance();
+						break;
+					case MyLinksPeasSessionController.SCOPE_OBJECT:
+						links = myLinksSC.getAllLinksByObject();
+						break;
+					default:
+						links = myLinksSC.getAllLinksByUser();
+				}
+            	request.setAttribute("Links",links);
+            	request.setAttribute("UrlReturn", myLinksSC.getUrl());
+            	request.setAttribute("InstanceId", myLinksSC.getInstanceId());
+ 	            destination = rootDest + "viewLinks.jsp";
+            }
+            else if (function.equals("NewLink"))
+			{
+            	boolean isVisible = false;
+            	if (myLinksSC.getScope() == MyLinksPeasSessionController.SCOPE_USER)
+            		isVisible = true;
+            	request.setAttribute("IsVisible",new Boolean(isVisible));
+ 				// appel jsp
+				destination = rootDest + "linkManager.jsp";
+			}
+			else if (function.equals("CreateLink"))
+			{
+				// récupération des paramètres venus de l'écran de saisie et création de l'objet LinkDetail
+				LinkDetail link = generateLink(request);
+				myLinksSC.createLink(link);
+				// retour sur le liste des liens
+				destination = getDestination("ViewLinks", myLinksSC,request);
+			}
+			else if (function.equals("CreateLinkFromComponent"))
+			{
+				// récupération des paramètres transmis et création de l'objet LinkDetail
+				LinkDetail link = generateLink(request);
+				myLinksSC.createLink(link);
+				// affichage d'une fenêtre de confirmation
+				destination = rootDest + "confirm.jsp";
+			}
+			else if (function.equals("EditLink"))
+			{
+				String linkId = request.getParameter("LinkId");
+				LinkDetail link = myLinksSC.getLink(linkId);
+            	request.setAttribute("Link",link);
+            	boolean isVisible = false;
+            	if (myLinksSC.getScope() == MyLinksPeasSessionController.SCOPE_USER)
+            		isVisible = true;
+            	request.setAttribute("IsVisible",new Boolean(isVisible));
+				// appel jsp
+				destination = rootDest + "linkManager.jsp";  
+			}
+			else if (function.equals("UpdateLink"))
+			{
+				// récupération des paramètres venus de l'écran de saisie
+				String linkId = request.getParameter("LinkId");
+				LinkDetail link = generateLink(request);
+				link.setLinkId(Integer.parseInt(linkId));
+				// modification du lien
+				myLinksSC.updateLink(link);
+				// retour sur le liste des liens
+				destination = getDestination("ViewLinks", myLinksSC,request); 
+			}
+			else if (function.equals("DeleteLinks")) 
+			{
+                Object o = request.getParameterValues("linkCheck");
+                if (o != null)
+                {
+                   	String[] links = (String[]) o;
+                   	myLinksSC.deleteLinks(links);
+                }
+                destination = getDestination("ViewLinks", myLinksSC, request);
+			}
+			else
+            {
+            	destination = getDestination("ViewLinks", myLinksSC, request);
+            }
+            
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("javax.servlet.jsp.jspException", e);
+            destination = "/admin/jsp/errorpageMain.jsp";
+        }
+
+        SilverTrace.info("myLinks", "MyLinksPeasRequestRouter.getDestination()", "root.MSG_GEN_PARAM_VALUE", "Destination=" + destination);
+        return destination;
+    }
+
+    private LinkDetail generateLink(HttpServletRequest request)
+    {
+    	String name = request.getParameter("Name");
+		String description = request.getParameter("Description");
+		String url = request.getParameter("Url");
+		// supprimer le context en début d'url
+		String sRequestURL = HttpUtils.getRequestURL(request).toString();
+	   	String m_sAbsolute = sRequestURL.substring(0, sRequestURL.length() - request.getRequestURI().length());
+		String urlServeur = m_sAbsolute;
+		if (url.startsWith(urlServeur))
+			url = url.substring(urlServeur.length(),url.length());
+		String context = URLManager.getApplicationURL();
+		if (url.startsWith(context))
+			url = url.substring(context.length(),url.length());   
+		boolean visible = false;
+		if ("true".equals(request.getParameter("Visible")))
+			visible = true;
+		boolean popup = false;
+		if ("true".equals(request.getParameter("Popup")))
+			popup = true;
+		/*String instanceId = request.getParameter("InstanceId");
+		if (instanceId != null)
+			return new LinkDetail(name, description,url,visible,popup, instanceId);
+		else  */
+			return new LinkDetail(name, description,url,visible,popup);
+    }
+}
