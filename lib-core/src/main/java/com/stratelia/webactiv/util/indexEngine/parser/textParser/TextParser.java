f6fc92e304b6704f71d931b88b91daa5942daa35
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-09-08 10:39:05 +0000

    Adding missing class
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@139 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

---
+++ lib-core/src/main/java/com/stratelia/webactiv/util/indexEngine/parser/textParser/TextParser.java
@@ -0,0 +1,78 @@
+package com.stratelia.webactiv.util.indexEngine.parser.textParser;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import com.stratelia.silverpeas.silvertrace.SilverTrace;
+import com.stratelia.webactiv.util.indexEngine.parser.Parser;
+
+/*
+ * CVS Informations
+ * 
+ * $Id: TextParser.java,v 1.2 2007/12/05 10:32:35 neysseri Exp $
+ * 
+ * $Log: TextParser.java,v $
+ * Revision 1.2  2007/12/05 10:32:35  neysseri
+ * no message
+ *
+ * Revision 1.1.1.1  2002/08/06 14:47:48  nchaix
+ * no message
+ *
+ * Revision 1.3  2002/01/09 17:22:29  dwenzek
+ * mise en place des silvertrace
+ *
+ */
+ 
+/**
+ * A TextParser parse a text file without any processing.
+ *
+ * @author $Author: neysseri $
+ */
+public class TextParser implements Parser
+{
+
+    /**
+     * Constructor declaration
+     */
+    public TextParser() {}
+
+    /**
+     * Method declaration
+     * 
+     * 
+     * @param path
+     * @param encoding
+     * 
+     * @return
+     */
+    public Reader getReader(String path, String encoding)
+    {
+        Reader reader = null;
+
+        try
+        {
+            InputStream file = new FileInputStream(path);
+
+            if (encoding == null)
+            {
+                reader = new InputStreamReader(file);
+            }
+            else
+            {
+                reader = new InputStreamReader(file, encoding);
+            }
+        }
+        catch (Exception e)
+        {
+	        SilverTrace.error("indexEngine", "TextParser",
+		                      "indexEngine.MSG_IO_ERROR_WHILE_READING",
+							  path, e);
+        }
+        return reader;
+    }
+
+}
+
+
