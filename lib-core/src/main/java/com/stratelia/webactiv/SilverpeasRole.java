7e53b38e44558e131a37d50f5b5255aafd08f1d9
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-09-10 15:27:54 +0000

    Adding completly the Gallery Component
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@151 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- lib-core/src/main/java/com/stratelia/webactiv/SilverpeasRole.java
+++ lib-core/src/main/java/com/stratelia/webactiv/SilverpeasRole.java
@@ -2,4 +2,8 @@ package com.stratelia.webactiv;
 
 public enum SilverpeasRole {
   admin, publisher, writer, user;
+
+  public boolean isInRole(String role) {
+    return this.equals(valueOf(role));
+  }
 }
