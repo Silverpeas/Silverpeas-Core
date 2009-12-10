f8eee37afee330539d38a863658dbc06ec4f4126
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-12-10 13:48:33 +0000

    Fixing CDDL Headers
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@378 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- web-core/src/main/java/com/sun/portal/portletcontainer/admin/deployment/WebAppDeployerUtil.java
+++ web-core/src/main/java/com/sun/portal/portletcontainer/admin/deployment/WebAppDeployerUtil.java
@@ -1,26 +1,27 @@
-/**
- * Copyright (C) 2000 - 2009 Silverpeas
+/*
+ * CDDL HEADER START
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * You can obtain a copy of the License at
+ * http://www.sun.com/cddl/cddl.html and legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
  *
- * As a special exception to the terms and conditions of version 3.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have recieved a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * "http://repository.silverpeas.com/legal/licensing"
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ * CDDL HEADER END
  */
+
 package com.sun.portal.portletcontainer.admin.deployment;
 
 import java.io.BufferedOutputStream;
@@ -32,48 +33,41 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * A util class which provides the helper methods to copy files which will be
- * used by the implementations of WebAppDeployer interface.
+ * A util class which provides the helper methods to copy files which will
+ * be used by the implementations of WebAppDeployer interface.
  */
 public class WebAppDeployerUtil {
 
-  /**
-   * Copies contents from the InputStream to the OutputStream.
-   * 
-   * @param in
-   *          InputStream from which to copy contents.
-   * @param out
-   *          OutputStream to which contents are written.
-   */
-  public static void copyInputStream(InputStream in, OutputStream out)
-      throws IOException {
-    byte[] buffer = new byte[2048];
-    int len;
+    /**
+     * Copies contents from the InputStream to the OutputStream.
+     *
+     * @param in InputStream from which to copy contents.
+     * @param out OutputStream to which contents are written.
+     */
+    public static void copyInputStream(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[2048];
+        int len;
 
-    while ((len = in.read(buffer)) >= 0)
-      out.write(buffer, 0, len);
+        while((len = in.read(buffer)) >= 0)
+            out.write(buffer, 0, len);
 
-    in.close();
-    out.close();
-  }
+        in.close();
+        out.close();
+    }
 
-  /**
-   * Copies contents from one file to another
-   * 
-   * @param fromFile
-   *          File from which to copy contents.
-   * @param toFile
-   *          File to which contents are written.
-   */
-  public static void copyFile(String fromFile, String toFile)
-      throws IOException {
-    File from = new File(fromFile);
-    File to = new File(toFile);
+    /**
+     * Copies contents from one file to another
+     *
+     * @param fromFile File from which to copy contents.
+     * @param toFile File to which contents are written.
+     */
+    public static void copyFile(String fromFile, String toFile) throws IOException {
+        File from = new File(fromFile);
+        File to = new File(toFile);
 
-    FileInputStream inStream = new FileInputStream(from);
+        FileInputStream inStream = new FileInputStream(from);
 
-    BufferedOutputStream out = new BufferedOutputStream(
-        new FileOutputStream(to));
-    copyInputStream(inStream, out);
-  }
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(to));
+        copyInputStream(inStream, out);
+    }
 }
