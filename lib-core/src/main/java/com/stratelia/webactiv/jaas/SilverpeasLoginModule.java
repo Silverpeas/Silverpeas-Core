2d8832365f5a7dbb45b481439d5f8ff7a7c06b82
Ludovic Bertin <ludovic.bertin@oosphere.com>
2009-12-24 11:15:36 +0000

    Add more details traces in case of exception (use of getNested instead of getCause)
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@427 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- lib-core/src/main/java/com/stratelia/webactiv/jaas/SilverpeasLoginModule.java
+++ lib-core/src/main/java/com/stratelia/webactiv/jaas/SilverpeasLoginModule.java
@@ -47,170 +47,180 @@ import com.stratelia.webactiv.beans.admin.Admin;
 import com.stratelia.webactiv.beans.admin.AdminException;
 import com.stratelia.webactiv.beans.admin.Domain;
 import com.stratelia.webactiv.beans.admin.OrganizationController;
+import com.stratelia.webactiv.util.exception.WithNested;
+
 import org.apache.jackrabbit.core.security.authentication.CredentialsCallback;
 
 public class SilverpeasLoginModule implements LoginModule {
 
-  private String anonymousUserId;
+	private String anonymousUserId;
 
-  private String userId;
+	private String userId;
 
-  // initial state
-  private Subject subject;
-  private CallbackHandler callbackHandler;
-  private Set principals = new HashSet();
+	// initial state
+	private Subject subject;
+	private CallbackHandler callbackHandler;
+	private Set principals = new HashSet();
 
-  private LoginPasswordAuthentication authenticator;
+	private LoginPasswordAuthentication authenticator;
 
-  private OrganizationController controller;
+	private OrganizationController controller;
 
-  private Admin administrator;
+	private Admin administrator;
 
-  public String getAnonymousUserId() {
-    return anonymousUserId;
-  }
+	public String getAnonymousUserId() {
+		return anonymousUserId;
+	}
 
-  public void setAnonymousUserId(String anonymousUserId) {
-    this.anonymousUserId = anonymousUserId;
-  }
+	public void setAnonymousUserId(String anonymousUserId) {
+		this.anonymousUserId = anonymousUserId;
+	}
 
-  public String getUserId() {
-    return userId;
-  }
+	public String getUserId() {
+		return userId;
+	}
 
-  public Subject getSubject() {
-    return subject;
-  }
+	public Subject getSubject() {
+		return subject;
+	}
 
-  public void setAuthenticator(LoginPasswordAuthentication authenticator) {
-    this.authenticator = authenticator;
-  }
+	public void setAuthenticator(LoginPasswordAuthentication authenticator) {
+		this.authenticator = authenticator;
+	}
 
-  public void setController(OrganizationController controller) {
-    this.controller = controller;
-  }
+	public void setController(OrganizationController controller) {
+		this.controller = controller;
+	}
 
-  public void setAdministrator(Admin administrator) {
-    this.administrator = administrator;
-  }
+	public void setAdministrator(Admin administrator) {
+		this.administrator = administrator;
+	}
 
-  public boolean abort() throws LoginException {
-    if (principals.isEmpty()) {
-      return false;
-    } else {
-      logout();
-    }
-    return true;
-  }
+	public boolean abort() throws LoginException {
+		if (principals.isEmpty()) {
+			return false;
+		} else {
+			logout();
+		}
+		return true;
+	}
 
-  public boolean commit() throws LoginException {
-    if (principals.isEmpty()) {
-      return false;
-    } else {
-      subject.getPrincipals().addAll(principals);
-      return true;
-    }
-  }
+	public boolean commit() throws LoginException {
+		if (principals.isEmpty()) {
+			return false;
+		} else {
+			subject.getPrincipals().addAll(principals);
+			return true;
+		}
+	}
 
-  public void initialize(Subject subject, CallbackHandler callbackHandler,
-      Map sharedState, Map options) {
-    this.subject = subject;
-    this.callbackHandler = callbackHandler;
-  }
+	public void initialize(Subject subject, CallbackHandler callbackHandler,
+			Map sharedState, Map options) {
+		this.subject = subject;
+		this.callbackHandler = callbackHandler;
+	}
 
-  public boolean login() throws LoginException {
-    // prompt for a user name and password
-    if (callbackHandler == null) {
-      throw new LoginException("no CallbackHandler available");
-    }
-    boolean authenticated = false;
-    principals.clear();
-    try {
-      // Get credentials using a JAAS callback
-      CredentialsCallback ccb = new CredentialsCallback();
-      callbackHandler.handle(new Callback[] { ccb });
-      Credentials creds = ccb.getCredentials();
-      // Use the credentials to set up principals
-      if (creds != null) {
-        if (creds instanceof SimpleCredentials) {
-          SimpleCredentials sc = (SimpleCredentials) creds;
-          // authenticate
-          Domain[] domains = controller.getAllDomains();
-          for (int i = 0; i < domains.length; i++) {
-            String key = authenticator.authenticate(sc.getUserID(), new String(
-                sc.getPassword()), domains[i].getId(), null);
-            if (key != null && !key.startsWith("Error_")) {
-              String userId = administrator.authenticate(key, null, false);
-              SilverpeasUserPrincipal principal = new SilverpeasUserPrincipal(
-                  userId);
-              fillPrincipal(principal);
-              principals.add(principal);
-            }
-          }
-          if (principals.isEmpty()
-              && getAnonymousUserId().equals(sc.getUserID())) {
-            principals.add(new AnonymousPrincipal());
-          }
-          authenticated = true;
-        } else if (creds instanceof SilverpeasCredentials) {
-          String userId = ((SilverpeasCredentials) creds).getUserId();
-          SilverpeasUserPrincipal principal = new SilverpeasUserPrincipal(
-              userId);
-          fillPrincipal(principal);
-          principals.add(principal);
-          authenticated = true;
-        } else if (creds instanceof SilverpeasSystemCredentials) {
-          SilverpeasSystemPrincipal principal = new SilverpeasSystemPrincipal();
-          principals.add(principal);
-          authenticated = true;
-        }
-      } else {
-        principals.add(new AnonymousPrincipal());
-        authenticated = true;
-      }
-    } catch (java.io.IOException ioe) {
-      throw new LoginException(ioe.toString());
-    } catch (UnsupportedCallbackException uce) {
-      throw new LoginException(uce.getCallback().toString() + " not available");
-    } catch (AdminException e) {
-    	StringBuffer message = new StringBuffer(e.getMessage());
-    	Throwable ex = e.getNested();
-    	int i = 0;
-    	while ( (ex != null) && (i<10) ) {
-    		i++;
-    		message.append(" - nested : ").append(ex.getMessage());
-    		ex = ex.getCause();
-    	}
-    	throw new LoginException(message.toString());
-    }
-    if (authenticated) {
-      return !principals.isEmpty();
-    } else {
-      principals.clear();
-      throw new FailedLoginException();
-    }
-  }
+	public boolean login() throws LoginException {
+		// prompt for a user name and password
+		if (callbackHandler == null) {
+			throw new LoginException("no CallbackHandler available");
+		}
+		boolean authenticated = false;
+		principals.clear();
+		try {
+			// Get credentials using a JAAS callback
+			CredentialsCallback ccb = new CredentialsCallback();
+			callbackHandler.handle(new Callback[] { ccb });
+			Credentials creds = ccb.getCredentials();
+			// Use the credentials to set up principals
+			if (creds != null) {
+				if (creds instanceof SimpleCredentials) {
+					SimpleCredentials sc = (SimpleCredentials) creds;
+					// authenticate
+					Domain[] domains = controller.getAllDomains();
+					for (int i = 0; i < domains.length; i++) {
+						String key = authenticator.authenticate(sc.getUserID(),
+								new String(sc.getPassword()), domains[i]
+										.getId(), null);
+						if (key != null && !key.startsWith("Error_")) {
+							String userId = administrator.authenticate(key,
+									null, false);
+							SilverpeasUserPrincipal principal = new SilverpeasUserPrincipal(
+									userId);
+							fillPrincipal(principal);
+							principals.add(principal);
+						}
+					}
+					if (principals.isEmpty()
+							&& getAnonymousUserId().equals(sc.getUserID())) {
+						principals.add(new AnonymousPrincipal());
+					}
+					authenticated = true;
+				} else if (creds instanceof SilverpeasCredentials) {
+					String userId = ((SilverpeasCredentials) creds).getUserId();
+					SilverpeasUserPrincipal principal = new SilverpeasUserPrincipal(
+							userId);
+					fillPrincipal(principal);
+					principals.add(principal);
+					authenticated = true;
+				} else if (creds instanceof SilverpeasSystemCredentials) {
+					SilverpeasSystemPrincipal principal = new SilverpeasSystemPrincipal();
+					principals.add(principal);
+					authenticated = true;
+				}
+			} else {
+				principals.add(new AnonymousPrincipal());
+				authenticated = true;
+			}
+		} catch (java.io.IOException ioe) {
+			throw new LoginException(ioe.toString());
+		} catch (UnsupportedCallbackException uce) {
+			throw new LoginException(uce.getCallback().toString()
+					+ " not available");
+		} catch (AdminException e) {
+			StringBuffer message = new StringBuffer();
 
-  public boolean logout() throws LoginException {
-    subject.getPrincipals().removeAll(principals);
-    principals.clear();
-    return true;
-  }
+			Throwable ex = e;
+			int i = 0;
+			while ((ex != null) && (i < 10)) {
+				i++;
+				message.append(" - ").append(ex.getMessage());
+				if (ex instanceof WithNested) {
+					ex = ((WithNested) ex).getNested();
+				} else {
+					ex = null;
+				}
+			}
+			throw new LoginException(message.toString());
+		}
+		if (authenticated) {
+			return !principals.isEmpty();
+		} else {
+			principals.clear();
+			throw new FailedLoginException();
+		}
+	}
 
-  protected void fillPrincipal(SilverpeasUserPrincipal principal) {
-    String[] spaceIds = controller.getAllSpaceIds(principal.getUserId());
-    for (int i = 0; i < spaceIds.length; i++) {
-      String[] componentIds = controller.getAvailCompoIds(spaceIds[i],
-          principal.getUserId());
-      for (int j = 0; j < componentIds.length; j++) {
-        String[] profiles = controller.getUserProfiles(principal.getUserId(),
-            componentIds[j]);
-        for (int k = 0; k < profiles.length; k++) {
-          principal.addUserProfile(new SilverpeasUserProfileEntry(
-              componentIds[j], profiles[k]));
-        }
-      }
-    }
-  }
+	public boolean logout() throws LoginException {
+		subject.getPrincipals().removeAll(principals);
+		principals.clear();
+		return true;
+	}
+
+	protected void fillPrincipal(SilverpeasUserPrincipal principal) {
+		String[] spaceIds = controller.getAllSpaceIds(principal.getUserId());
+		for (int i = 0; i < spaceIds.length; i++) {
+			String[] componentIds = controller.getAvailCompoIds(spaceIds[i],
+					principal.getUserId());
+			for (int j = 0; j < componentIds.length; j++) {
+				String[] profiles = controller.getUserProfiles(principal
+						.getUserId(), componentIds[j]);
+				for (int k = 0; k < profiles.length; k++) {
+					principal.addUserProfile(new SilverpeasUserProfileEntry(
+							componentIds[j], profiles[k]));
+				}
+			}
+		}
+	}
 
 }
