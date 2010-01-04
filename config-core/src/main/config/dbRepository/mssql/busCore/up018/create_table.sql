dc559262b128805dfe02de9795ea6f06a6826d2b
Ludovic Bertin <ludovic.bertin@oosphere.com>
2010-01-04 10:57:06 +0000

    release feature #92 - "I forgot my password" - with license info included
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@465 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- config-core/src/main/config/dbRepository/mssql/busCore/up018/create_table.sql
+++ config-core/src/main/config/dbRepository/mssql/busCore/up018/create_table.sql
@@ -1,1 +1,7 @@
-exec sp_rename 'sc_kmelia_modelused', 'st_instance_modelused'
+ALTER TABLE ST_User
+ADD COLUMN loginquestion varchar(200)
+;
+
+ALTER TABLE ST_User
+ADD COLUMN loginanswer varchar(200)
+;
