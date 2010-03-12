373ebeef3b716847fb3c61fcd5c71550e305196e
Nicolas Eysseric <nicolas.eysseric@silverpeas.com>
2010-03-12 14:20:42 +0000

    force personal question on login only if dedicated parameter turned on (related to features #92 and #241)
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@945 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- web-core/src/main/java/com/stratelia/webactiv/filter/LoginFilter.java
+++ web-core/src/main/java/com/stratelia/webactiv/filter/LoginFilter.java
@@ -13,7 +13,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.silverpeas.util.StringUtil;
@@ -23,91 +22,80 @@ import com.stratelia.webactiv.beans.admin.UserDetail;
 import com.stratelia.webactiv.util.ResourceLocator;
 
 public class LoginFilter
-	implements Filter
-{
-	
-	private boolean isUserLoginQuestionMandatory;
-	private String anonymousId;
-	
-	public void init(FilterConfig filterConfig)
-	{
-		ResourceLocator general = new ResourceLocator("com.stratelia.silverpeas.lookAndFeel.generalLook", "");
-		isUserLoginQuestionMandatory = "true".equals(general.getString("userLoginQuestionMandatory"));
-		anonymousId = general.getString("anonymousId");
-	}
+    implements Filter {
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-		throws IOException, ServletException
-	{
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse res = (HttpServletResponse) response;		
+  private boolean isUserLoginQuestionMandatory;
+  private String anonymousId;
 
-		HttpSession session = req.getSession(true);
-		Map parameters = req.getParameterMap();
-		Iterator it = parameters.entrySet().iterator();
-		while (it.hasNext()) {
-			Entry entry = (Entry) it.next();
-			String paramName = (String) entry.getKey();
-			String[] paramValues = (String[]) entry.getValue();
-			if (paramValues.length == 1)
-				session.setAttribute("svplogin_"+paramName, paramValues[0]);
-			else
-				session.setAttribute("svplogin_"+paramName, paramValues);
-		}
-		
-		String destination = null;
-		if (isUserLoginQuestionMandatory)
-		{
-			String sessionId = session.getId();
-			Admin admin = new Admin();
-			try
-			{
-				String key = req.getParameter("Key");
-				String userId = admin.authenticate(key, sessionId, false, false);
-				UserDetail userDetail = admin.getUserDetail(userId);
-				
-				if (userDetail != null
-					&& !userDetail.getId().equals(anonymousId)
-					&& ( !StringUtil.isDefined(userDetail.getLoginQuestion() ) )
-					)
-				{
-					request.setAttribute("userDetail", userDetail);
-					destination = "/CredentialsServlet/ChangeQuestion";
-				}
-			}
-			catch (AdminException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		// destination = "/CredentialsServlet/ForcePasswordChange";
-		if (destination != null)
-		{
-			RequestDispatcher dispatcher = request.getRequestDispatcher(destination);
-			dispatcher.forward(request, response);
-		}
-		else
-		{
-			chain.doFilter(request, response);
-		}
-	}
+  public void init(FilterConfig filterConfig) {
+    ResourceLocator general =
+        new ResourceLocator("com.stratelia.silverpeas.lookAndFeel.generalLook", "");
+    isUserLoginQuestionMandatory =
+        "personalQuestion".equals(general.getString("forgottenPwdActive")) &&
+        "true".equals(general.getString("userLoginQuestionMandatory"));
+    anonymousId = general.getString("anonymousId");
+  }
 
-	public FilterConfig getFilterConfig()
-	{
-		return null;
-	}
+  @SuppressWarnings("unchecked")
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
 
-	public void setFilterConfig(FilterConfig config)
-	{
-		//
-	}
+    HttpSession session = req.getSession(true);
+    Map<String, String[]> parameters = req.getParameterMap();
+    Iterator<Entry<String, String[]>> it = parameters.entrySet().iterator();
+    while (it.hasNext()) {
+      Entry<String, String[]> entry = it.next();
+      String paramName = (String) entry.getKey();
+      String[] paramValues = (String[]) entry.getValue();
+      if (paramValues.length == 1)
+        session.setAttribute("svplogin_" + paramName, paramValues[0]);
+      else
+        session.setAttribute("svplogin_" + paramName, paramValues);
+    }
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
+    String destination = null;
+    if (isUserLoginQuestionMandatory) {
+      String sessionId = session.getId();
+      Admin admin = new Admin();
+      try {
+        String key = req.getParameter("Key");
+        String userId = admin.authenticate(key, sessionId, false, false);
+        UserDetail userDetail = admin.getUserDetail(userId);
+
+        if (userDetail != null
+            && !userDetail.getId().equals(anonymousId)
+            && (!StringUtil.isDefined(userDetail.getLoginQuestion()))) {
+          request.setAttribute("userDetail", userDetail);
+          destination = "/CredentialsServlet/ChangeQuestion";
+        }
+      } catch (AdminException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+
+    // destination = "/CredentialsServlet/ForcePasswordChange";
+    if (destination != null) {
+      RequestDispatcher dispatcher = request.getRequestDispatcher(destination);
+      dispatcher.forward(request, response);
+    } else {
+      chain.doFilter(request, response);
+    }
+  }
+
+  public FilterConfig getFilterConfig() {
+    return null;
+  }
+
+  public void setFilterConfig(FilterConfig config) {
+    //
+  }
+
+  @Override
+  public void destroy() {
+    // TODO Auto-generated method stub
+
+  }
 
 }
