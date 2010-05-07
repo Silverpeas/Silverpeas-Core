4be9bbdcf1acde30c4bb37f8c9e5e760a4e64a9b
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-05-07 09:19:18 +0000

    Fixing bug #382: Now you can define a property logout.page in com/silverpeas/authentication/settings/authenticationSettings.properties to redirect to a local or external URL
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@1350 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- web-core/src/main/java/com/stratelia/webactiv/servlets/LogoutServlet.java
+++ web-core/src/main/java/com/stratelia/webactiv/servlets/LogoutServlet.java
@@ -32,80 +32,38 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.stratelia.silverpeas.peasCore.SessionManager;
-import com.stratelia.silverpeas.silvertrace.SilverTrace;
+import com.stratelia.webactiv.util.ResourceLocator;
 
 public class LogoutServlet extends HttpServlet {
-  private String m_sContext = "";
-  private String m_sAbsolute = "";
 
-  private String errorCode = "";
+  private static final long serialVersionUID = 996291597161289526L;
 
+  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
     // Get the session
-    HttpSession session = request.getSession(true);
-
-    // Get the context
-    String sURI = request.getRequestURI();
-    String sServletPath = request.getServletPath();
-    String sPathInfo = request.getPathInfo();
-    String sRequestURL = request.getRequestURL().toString();
-    // String sRequestURL = HttpUtils.getRequestURL(request).toString();
-
-    m_sAbsolute = sRequestURL.substring(0, sRequestURL.length()
-        - request.getRequestURI().length());
-
-    SilverTrace.info("peasCore", "LogoutServlet.doPost()",
-        "root.MSG_GEN_PARAM_VALUE", "sURI=" + sURI);
-    SilverTrace.info("peasCore", "LogoutServlet.doPost()",
-        "root.MSG_GEN_PARAM_VALUE", "sServletPath=" + sServletPath);
-    SilverTrace.info("peasCore", "LogoutServlet.doPost()",
-        "root.MSG_GEN_PARAM_VALUE", "sPathInfo=" + sPathInfo);
-    SilverTrace.info("peasCore", "LogoutServlet.doPost()",
-        "root.MSG_GEN_PARAM_VALUE", "sRequestURL=" + sRequestURL);
-    SilverTrace.info("peasCore", "LogoutServlet.doPost()",
-        "root.MSG_GEN_PARAM_VALUE", "sAbsolute=" + m_sAbsolute);
-
-    if (sPathInfo != null)
-      sURI = sURI.substring(0, sURI.lastIndexOf(sPathInfo));
-
-    m_sContext = sURI.substring(0, sURI.lastIndexOf(sServletPath));
-    if (m_sContext.charAt(m_sContext.length() - 1) == '/') {
-      m_sContext = m_sContext.substring(0, m_sContext.length() - 1);
-    }
-
-    // Empty the MainSessionController and all SilverPeas variables
-    // already done by SessionManager cleanSession(session);
-
+    HttpSession session = request.getSession(false);
+    StringBuilder buffer = new StringBuilder();
+    buffer.append(request.getScheme()).append("://").append(request.getServerName()).append(':');
+    buffer.append(request.getServerPort()).append(request.getContextPath());
     // Notify session manager : invalidate unbinds any objects bound.
     SessionManager.getInstance().closeHttpSession(session);
-
+    ResourceLocator resource =
+        new ResourceLocator("com.silverpeas.authentication.settings.authenticationSettings", "");
+    String postLogoutPage = resource.getString("logout.page", "/Login.jsp?ErrorCode=4&logout=true");
+    if (postLogoutPage.startsWith("http")) {
+      response.sendRedirect(postLogoutPage);
+      return;
+    }
+    buffer.append(postLogoutPage);
     // Route on login page
-    errorCode = "4";
-    response.sendRedirect(response.encodeRedirectURL(m_sAbsolute + m_sContext
-        + "/Login.jsp?ErrorCode=" + errorCode + "&logout=true"));
+    response.sendRedirect(response.encodeRedirectURL(buffer.toString()));
+    return;
   }
 
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     doPost(request, response);
   }
-
-  /*
-   * protected void cleanSession(HttpSession session) { Enumeration spSessionAttNames =
-   * session.getAttributeNames(); String spName; while (spSessionAttNames.hasMoreElements()) {
-   * spName = (String) spSessionAttNames.nextElement(); //
-   * SilverTrace.info("peasCore","LogoutServlet.doPost()" ,"root.MSG_GEN_PARAM_VALUE","spName=" +
-   * spName); if ((spName != null) && ((spName.startsWith("Silverpeas_")) ||
-   * (spName.startsWith("WYSIWYG_")))) { //
-   * SilverTrace.info("peasCore","LogoutServlet.doPost()","root.MSG_GEN_PARAM_VALUE" ,"Remove=" +
-   * spName); session.removeAttribute(spName); } }
-   * session.removeAttribute("SilverSessionController");
-   * session.removeAttribute("SessionGraphicElementFactory"); session.removeAttribute("spaceModel");
-   * // For Portlets session.removeAttribute("quizzUnderConstruction"); // For Quizz
-   * session.removeAttribute("questionsVector"); // For Quizz
-   * session.removeAttribute("currentQuizzId"); // For Quizz
-   * session.removeAttribute("questionsResponses"); // For Quizz
-   * session.removeAttribute("currentParticipationId"); // For Quizz }
-   */
 }
