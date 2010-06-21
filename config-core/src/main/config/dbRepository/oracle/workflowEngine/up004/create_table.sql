3aade8b5ccc6bcafd87d3fbf6fbea5b7619e30ed
David Lesimple <david.lesimple@silverpeas.com>
2010-06-21 15:20:44 +0000

    fixing bug #378
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@1751 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- config-core/src/main/config/dbRepository/oracle/workflowEngine/up004/create_table.sql
+++ config-core/src/main/config/dbRepository/oracle/workflowEngine/up004/create_table.sql
@@ -5,3 +5,27 @@ ADD groupid varchar (50) NULL
 ALTER TABLE SB_Workflow_WorkingUser
 ADD groupid varchar (50) NULL
 ;
+
+ALTER TABLE SB_Workflow_Error
+MODIFY actiondate timestamp
+;
+
+ALTER TABLE SB_Workflow_HistoryStep
+MODIFY actiondate timestamp
+;
+
+ALTER TABLE SB_Workflow_LockingUser
+MODIFY lockdate timestamp
+;
+
+ALTER TABLE SB_Workflow_Question
+MODIFY questiondate timestamp
+;
+
+ALTER TABLE SB_Workflow_Question
+MODIFY responsedate timestamp
+;
+
+
+
+
--- config-core/src/main/config/dbRepository/postgres/workflowEngine/up004/create_table.sql
+++ config-core/src/main/config/dbRepository/postgres/workflowEngine/up004/create_table.sql
@@ -5,3 +5,23 @@ ADD groupid varchar (50) NULL
 ALTER TABLE SB_Workflow_WorkingUser
 ADD groupid varchar (50) NULL
 ;
+
+ALTER TABLE SB_Workflow_Error
+ALTER COLUMN actiondate TYPE timestamp
+;
+
+ALTER TABLE SB_Workflow_HistoryStep
+ALTER COLUMN actiondate TYPE timestamp
+;
+
+ALTER TABLE SB_Workflow_LockingUser
+ALTER COLUMN lockdate TYPE timestamp
+;
+
+ALTER TABLE SB_Workflow_Question
+ALTER COLUMN questiondate TYPE timestamp
+;
+
+ALTER TABLE SB_Workflow_Question
+ALTER COLUMN responsedate TYPE timestamp
+;
