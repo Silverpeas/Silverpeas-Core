5a927060560a858c678ca04452166d1748e42c22
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-06-22 08:13:23 +0000

    Preparing extraction of integration tests
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@1752 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- lib-core/src/test/java/com/silverpeas/util/web/servlet/FileUploadUtilTest.java
+++ lib-core/src/test/java/com/silverpeas/util/web/servlet/FileUploadUtilTest.java
@@ -22,10 +22,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.silverpeas.util.web.servlet;
 
 import org.apache.commons.fileupload.FileUploadBase;
