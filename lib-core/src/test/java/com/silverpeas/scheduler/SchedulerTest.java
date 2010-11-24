b8702d1552c6a560091235a2b560ce9abfbc1374
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-11-24 14:00:12 +0000

    Test can now be run with JUnit 4.8
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@2788 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- lib-core/src/test/java/com/silverpeas/scheduler/SchedulerTest.java
+++ lib-core/src/test/java/com/silverpeas/scheduler/SchedulerTest.java
@@ -33,10 +33,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.annotation.ExpectedException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import static org.junit.Assert.*;
 import static com.jayway.awaitility.Awaitility.*;
 import static java.util.concurrent.TimeUnit.*;
@@ -51,8 +49,8 @@ import static java.util.concurrent.TimeUnit.*;
  * This test checks the current concrete scheduler is ok.
  * @author mmoquillon
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/spring-scheduling.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "/spring-scheduling.xml")
 public class SchedulerTest {
 
   private static final String JOB_NAME = "test";
@@ -64,7 +62,8 @@ public class SchedulerTest {
   }
 
   @BeforeClass
-  public static void setUpClass() throws Exception {
+  public static void setUpClass() throws Exception {    
+    ApplicationContext context = new ClassPathXmlApplicationContext("/spring-scheduling.xml");
   }
 
   @AfterClass
@@ -94,16 +93,14 @@ public class SchedulerTest {
     assertTrue(true);
   }
 
-  @Test
-  @ExpectedException(SchedulerException.class)
+  @Test(expected=SchedulerException.class)
   public void schedulingAnAlreadyScheduledJobShouldThrowASchedulerException() throws Exception {
     scheduleAJob(JOB_NAME);
     JobTrigger trigger = JobTrigger.triggerEvery(1, TimeUnit.SECOND);
     scheduler.scheduleJob(JOB_NAME, trigger, eventHandler);
   }
 
-  @Test
-  @ExpectedException(SchedulerException.class)
+  @Test(expected=SchedulerException.class)
   public void schedulingAnAlreadyScheduledJobExecutionShouldThrowASchedulerException() throws
       Exception {
     scheduleAJob(JOB_NAME);
