e2c4afd471a8ab9b5d13a2183931960ebd695dd9
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-12-06 09:34:28 +0000

    JdbcFieldDisplayer performance boost
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/core/trunk@2843 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- lib-core/src/main/java/com/silverpeas/form/fieldDisplayer/JdbcFieldDisplayer.java
+++ lib-core/src/main/java/com/silverpeas/form/fieldDisplayer/JdbcFieldDisplayer.java
@@ -21,7 +21,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.silverpeas.form.fieldDisplayer;
 
 import java.io.PrintWriter;
@@ -54,6 +53,9 @@ import com.stratelia.webactiv.util.DBUtil;
  */
 public class JdbcFieldDisplayer extends AbstractFieldDisplayer {
 
+  private final static String[] MANAGED_TYPES = new String[]{JdbcField.TYPE};
+  private final static String mandatoryImg = Util.getIcon("mandatoryField");
+
   /**
    * Constructeur
    */
@@ -63,11 +65,10 @@ public class JdbcFieldDisplayer extends AbstractFieldDisplayer {
 
   /**
    * Returns the name of the managed types.
+   * @return 
    */
   public String[] getManagedTypes() {
-    String[] s = new String[0];
-    s[0] = JdbcField.TYPE;
-    return s;
+    return MANAGED_TYPES;
   }
 
   /**
@@ -75,51 +76,53 @@ public class JdbcFieldDisplayer extends AbstractFieldDisplayer {
    * The error messages may be adapted to a local language. The FieldTemplate gives the field type
    * and constraints. The FieldTemplate gives the local labeld too. Never throws an Exception but
    * log a silvertrace and writes an empty string when :
-   * <UL>
-   * <LI>the fieldName is unknown by the template.
-   * <LI>the field type is not a managed type.
-   * </UL>
+   * <ul>
+   * <li>the fieldName is unknown by the template.</li>
+   * <li>the field type is not a managed type.</li>
+   * </ul>
+   *
+   * @param out
+   * @param template
+   * @param pagesContext
+   * @throws java.io.IOException 
    */
-  public void displayScripts(PrintWriter out,
-      FieldTemplate template,
-      PagesContext PagesContext) throws java.io.IOException {
-
-    String language = PagesContext.getLanguage();
-
-    if (!template.getTypeName().equals(JdbcField.TYPE))
+  @Override
+  public void displayScripts(PrintWriter out, FieldTemplate template, PagesContext pagesContext)
+      throws java.io.IOException {
+    String language = pagesContext.getLanguage();
+    if (!JdbcField.TYPE.equals(template.getTypeName())) {
       SilverTrace.info("form", "JdbcFieldDisplayer.displayScripts", "form.INFO_NOT_CORRECT_TYPE",
           JdbcField.TYPE);
-
-    if (template.isMandatory() && PagesContext.useMandatory()) {
+    }
+    if (template.isMandatory() && pagesContext.useMandatory()) {
       out.println("	if (isWhitespace(stripInitialWhitespace(field.value))) {");
-      out.println("		errorMsg+=\"  - '" + template.getLabel(language) + "' " +
-          Util.getString("GML.MustBeFilled", language) + "\\n \";");
+      out.println("		errorMsg+=\"  - '" + template.getLabel(language) + "' "
+          + Util.getString("GML.MustBeFilled", language) + "\\n \";");
       out.println("		errorNb++;");
       out.println("	}");
     }
-
-    Util.getJavascriptChecker(template.getFieldName(), PagesContext, out);
+    Util.getJavascriptChecker(template.getFieldName(), pagesContext, out);
   }
 
   /**
    * Prints the HTML value of the field. The displayed value must be updatable by the end user. The
    * value format may be adapted to a local language. The fieldName must be used to name the html
    * form input. Never throws an Exception but log a silvertrace and writes an empty string when :
-   * <UL>
-   * <LI>the field type is not a managed type.
-   * </UL>
+   * <ul>
+   * <li>the field type is not a managed type.</li>
+   * </ul>
+   *
+   * @param out
+   * @param field
+   * @param template
+   * @param pagesContext
+   * @throws FormException 
    */
-  public void display(PrintWriter out,
-      Field field,
-      FieldTemplate template,
-      PagesContext PagesContext) throws FormException {
-    String value = "";
-    String html = "";
-    String currentUserId = PagesContext.getUserId();
-    String language = PagesContext.getLanguage();
-
-    String mandatoryImg = Util.getIcon("mandatoryField");
-
+  @Override
+  public void display(PrintWriter out, Field field, FieldTemplate template,
+      PagesContext pagesContext) throws FormException {
+    String currentUserId = pagesContext.getUserId();
+    String language = pagesContext.getLanguage();
     String fieldName = template.getFieldName();
     Map<String, String> parameters = template.getParameters(language);
     JdbcField jdbcField = null;
@@ -130,191 +133,174 @@ public class JdbcFieldDisplayer extends AbstractFieldDisplayer {
     } else {
       jdbcField = (JdbcField) field;
     }
-
+    String value = "";
     if (!field.isNull()) {
       value = field.getValue(language);
     }
-    Collection<String> listRes = null; // liste de valeurs String
-
-    // Parameters
-    String driverName = null;
-    String url = null;
-    String login = null;
-    String password = null;
-    String query = null;
-    String valueFieldType = "1"; // valeurs possibles 1 = choix restreint a la liste ou 2 = saisie
-    // libre, par defaut 1
-    if (parameters.containsKey("driverName")) {
-      driverName = parameters.get("driverName");
-    }
-    if (parameters.containsKey("url")) {
-      url = parameters.get("url");
-    }
-    if (parameters.containsKey("login")) {
-      login = parameters.get("login");
-    }
-    if (parameters.containsKey("password")) {
-      password = parameters.get("password");
-    }
-    if (parameters.containsKey("query")) {
-      query = parameters.get("query");
-    }
+    // valeurs possibles 1 = choix restreint a la liste ou 2 = saisie libre
+    String valueFieldType = "1";
     if (parameters.containsKey("valueFieldType")) {
-      valueFieldType = parameters.get("valueFieldType"); // valeurs possibles 1 = choix
-      // restreint a la liste ou 2 =
-      // saisie libre, par defaut 1
+      valueFieldType = parameters.get("valueFieldType");
     }
-
+    Collection<String> listRes = null;
     if (jdbcField != null) {
       Connection jdbcConnection = null;
       try {
-        jdbcConnection = jdbcField.connectJdbc(driverName, url, login, password);
-        listRes = jdbcField.selectSql(jdbcConnection, query, currentUserId);
+        jdbcConnection = jdbcField.connectJdbc(parameters.get("driverName"), parameters.get("url"), parameters.
+            get("login"), parameters.get("password"));
+        listRes = jdbcField.selectSql(jdbcConnection, parameters.get("query"), currentUserId);
       } finally {
         DBUtil.close(jdbcConnection);
       }
     }
-
+    StringBuilder html = new StringBuilder(10000);
     if (listRes != null && !listRes.isEmpty()) {
       int zindex =
-          (PagesContext.getLastFieldIndex() - Integer.parseInt(PagesContext.getCurrentFieldIndex())) * 9000;
-      html += "<style type=\"text/css\">\n";
-      html += "	#listAutocomplete" + fieldName + " {\n";
-      html += "		width:15em;\n";
-      html += "		padding-bottom:2em;\n";
-      html += "	}\n";
-      html += "	#listAutocomplete" + fieldName + " {\n";
-      html +=
-          "		z-index:" + zindex +
-          "; /* z-index needed on top instance for ie & sf absolute inside relative issue */\n";
-      html += "	}\n";
-      html += "	#" + fieldName + " {\n";
-      html += "		_position:absolute; /* abs pos needed for ie quirks */\n";
-      html += "	}\n";
-      html += "</style>\n";
+          (pagesContext.getLastFieldIndex() - Integer.parseInt(pagesContext.getCurrentFieldIndex())) * 9000;
+      html.append("<style type=\"text/css\">\n").append("	#listAutocomplete").append(fieldName).
+          append(" {\n");
+      html.append("		width:15em;\n");
+      html.append("		padding-bottom:2em;\n");
+      html.append("	}\n");
+      html.append("	#listAutocomplete").append(fieldName).append(" {\n");
+      html.append("		z-index:").append(zindex).append(
+          "; /* z-index needed on top instance for ie & sf absolute inside relative issue */\n");
+      html.append("	}\n");
+      html.append("	#").append(fieldName).append(" {\n");
+      html.append("		_position:absolute; /* abs pos needed for ie quirks */\n");
+      html.append("	}\n");
+      html.append("</style>\n");
 
-      html += "<div id=\"listAutocomplete" + fieldName + "\">\n";
-      html += "<input id=\"" + fieldName + "\" name=\"" + fieldName + "\" type=\"text\"";
+      html.append("<div id=\"listAutocomplete").append(fieldName).append("\">\n");
+      html.append("<input id=\"").append(fieldName).append("\" name=\"").append(fieldName).append(
+          "\" type=\"text\"");
       if (value != null) {
-        html += " value=\"" + value + "\"";
+        html.append(" value=\"").append(value).append("\"");
       }
       if (template.isDisabled() || template.isReadOnly()) {
-        html += " disabled";
+        html.append(" disabled");
       }
-      html += "/>\n";
-      html += "<div id=\"container" + fieldName + "\"/>\n";
-      html += "</div>\n";
+      html.append("/>\n");
+      html.append("<div id=\"container").append(fieldName).append("\"/>\n");
+      html.append("</div>\n");
 
-      if (template.isMandatory() && !template.isDisabled() && !template.isReadOnly() &&
-          !template.isHidden() && PagesContext.useMandatory()) {
-        html +=
-            "<img src=\"" +
-                mandatoryImg +
-                "\" width=\"5\" height=\"5\" border=\"0\" alt=\"\" style=\"position:absolute;left:16em;top:5px\"/>\n";
+      if (template.isMandatory() && !template.isDisabled() && !template.isReadOnly()
+          && !template.isHidden() && pagesContext.useMandatory()) {
+        html.append("<img src=\"").append(mandatoryImg).append(
+            "\" width=\"5\" height=\"5\" border=\"0\" alt=\"\" style=\"position:absolute;left:16em;top:5px\"/>\n");
       }
 
-      html += "<script type=\"text/javascript\">\n";
-      html += "listArray" + fieldName + " = [\n";
+      html.append("<script type=\"text/javascript\">\n");
+      html.append("listArray").append(fieldName).append(" = [\n");
 
       Iterator<String> itRes = listRes.iterator();
       while (itRes.hasNext()) {
-        html += "\"" + EncodeHelper.javaStringToJsString(itRes.next()) + "\"";
+        html.append("\"").
+            append(EncodeHelper.javaStringToJsString(itRes.next())).append("\"");
         if (itRes.hasNext()) {
-          html += ",\n";
+          html.append(",\n");
         }
       }
 
-      html += "];\n";
-      html += "</script>\n";
+      html.append("];\n");
+      html.append("</script>\n");
 
-      html += "<script type=\"text/javascript\">\n";
-      html +=
-          " this.oACDS" + fieldName + " = new YAHOO.util.LocalDataSource(listArray" + fieldName +
-          ");\n";
-      html +=
-          "	this.oAutoComp" + fieldName + " = new YAHOO.widget.AutoComplete('" + fieldName +
-          "','container" + fieldName + "', this.oACDS" + fieldName + ");\n";
-      html += "	this.oAutoComp" + fieldName + ".prehighlightClassName = \"yui-ac-prehighlight\";\n";
-      html += "	this.oAutoComp" + fieldName + ".typeAhead = true;\n";
-      html += "	this.oAutoComp" + fieldName + ".useShadow = true;\n";
-      html += "	this.oAutoComp" + fieldName + ".minQueryLength = 0;\n";
+      html.append("<script type=\"text/javascript\">\n");
+      html.append(" this.oACDS").append(fieldName).
+          append(" = new YAHOO.util.LocalDataSource(listArray").append(fieldName).append(
+          ");\n");
+      html.append("	this.oAutoComp").append(fieldName).append(" = new YAHOO.widget.AutoComplete('").
+          append(fieldName).append("','container").append(fieldName).append("', this.oACDS").
+          append(fieldName).append(
+          ");\n");
+      html.append("	this.oAutoComp").append(fieldName).append(
+          ".prehighlightClassName = \"yui-ac-prehighlight\";\n");
+      html.append("	this.oAutoComp").append(fieldName).append(".typeAhead = true;\n");
+      html.append("	this.oAutoComp").append(fieldName).append(".useShadow = true;\n");
+      html.append("	this.oAutoComp").append(fieldName).append(".minQueryLength = 0;\n");
 
       if ("1".equals(valueFieldType)) {// valeurs possibles 1 = choix restreint a la liste ou 2 =
         // saisie libre, par defaut 1
-        html += "	this.oAutoComp" + fieldName + ".forceSelection = true;\n";
+        html.append("	this.oAutoComp").append(fieldName).append(".forceSelection = true;\n");
       }
 
-      html += "	this.oAutoComp" + fieldName + ".textboxFocusEvent.subscribe(function(){\n";
-      html += "		var sInputValue = YAHOO.util.Dom.get('" + fieldName + "').value;\n";
-      html += "		if(sInputValue.length == 0) {\n";
-      html += "			var oSelf = this;\n";
-      html += "			setTimeout(function(){oSelf.sendQuery(sInputValue);},0);\n";
-      html += "		}\n";
-      html += "	});\n";
-      html += "</script>\n";
+      html.append("	this.oAutoComp").append(fieldName).append(
+          ".textboxFocusEvent.subscribe(function(){\n");
+      html.append("		var sInputValue = YAHOO.util.Dom.get('").append(fieldName).append("').value;\n");
+      html.append("		if(sInputValue.length == 0) {\n");
+      html.append("			var oSelf = this;\n");
+      html.append("			setTimeout(function(){oSelf.sendQuery(sInputValue);},0);\n");
+      html.append("		}\n");
+      html.append("	});\n");
+      html.append("</script>\n");
 
     } else {
 
       if ("1".equals(valueFieldType)) {// valeurs possibles 1 = choix restreint a la liste ou 2 =
         // saisie libre, par defaut 1
 
-        html += "<select name=\"" + fieldName + "\"";
+        html.append("<select name=\"").append(fieldName).append("\"");
 
         if (template.isDisabled() || template.isReadOnly()) {
-          html += " disabled";
+          html.append(" disabled");
         }
-        html += " >\n";
-        html += "</select>\n";
+        html.append(" >\n");
+        html.append("</select>\n");
 
       } else {
-        html += "<input type=\"text\" name=\"" + fieldName + "\"";
+        html.append("<input type=\"text\" name=\"").append(fieldName).append("\"");
 
         if (template.isDisabled() || template.isReadOnly()) {
-          html += " disabled=\"disabled\"";
+          html.append(" disabled=\"disabled\"");
         }
-        html += " />\n";
+        html.append(" />\n");
       }
-      if (template.isMandatory() && !template.isDisabled() && !template.isReadOnly() &&
-          !template.isHidden()) {
-        html +=
-            "&nbsp;<img src=\"" + mandatoryImg +
-            "\" width=\"5\" height=\"5\" border=\"0\" alt=\"\"/>&nbsp;\n";
+      if (template.isMandatory() && !template.isDisabled() && !template.isReadOnly()
+          && !template.isHidden()) {
+        html.append("&nbsp;<img src=\"").append(mandatoryImg).append(
+            "\" width=\"5\" height=\"5\" border=\"0\" alt=\"\"/>&nbsp;\n");
       }
-    }
 
-    out.println(html);
+    }
+    out.println(html.toString());
+
   }
+
+  
 
   /**
    * Updates the value of the field. The fieldName must be used to retrieve the HTTP parameter from
-   * the request.
-   * @throw FormException if the field type is not a managed type.
-   * @throw FormException if the field doesn't accept the new value.
+   * the request.  
+   * @param newValue
+   * @param field
+   * @param template
+   * @param pagesContext
+   * @return
+   * @throws FormException if the field type is not a managed type or if the field doesn't accept the new value.
    */
-  public List<String> update(String newValue,
-      Field field,
-      FieldTemplate template,
-      PagesContext PagesContext)
-      throws FormException {
-
-    if (!field.getTypeName().equals(JdbcField.TYPE))
+  public List<String> update(String newValue, Field field, FieldTemplate template,
+      PagesContext pagesContext) throws FormException {
+    if (!JdbcField.TYPE.equals(field.getTypeName())) {
       throw new FormException("JdbcFieldDisplayer.update", "form.EX_NOT_CORRECT_TYPE",
           JdbcField.TYPE);
+    }
 
-    if (field.acceptValue(newValue, PagesContext.getLanguage()))
-      field.setValue(newValue, PagesContext.getLanguage());
-    else
+    if (field.acceptValue(newValue, pagesContext.getLanguage())) {
+      field.setValue(newValue, pagesContext.getLanguage());
+    } else {
       throw new FormException("JdbcFieldDisplayer.update", "form.EX_NOT_CORRECT_VALUE",
           JdbcField.TYPE);
+    }
     return new ArrayList<String>();
   }
 
+  @Override
   public boolean isDisplayedMandatory() {
     return true;
   }
 
+  @Override
   public int getNbHtmlObjectsDisplayed(FieldTemplate template, PagesContext pagesContext) {
     return 1;
   }
-
 }
---
+++ lib-core/src/test/java/com/silverpeas/form/fieldDisplayer/JdbcFieldDisplayerTest.java
@@ -0,0 +1,185 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.silverpeas.form.fieldDisplayer;
+
+import com.google.common.collect.Lists;
+import com.google.common.base.Charsets;
+import com.silverpeas.form.fieldType.JdbcField;
+import com.silverpeas.form.Field;
+import com.silverpeas.form.FieldTemplate;
+import com.silverpeas.form.PagesContext;
+import com.silverpeas.jcrutil.RandomGenerator;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author ehugonnet
+ */
+public class JdbcFieldDisplayerTest {
+
+  public JdbcFieldDisplayerTest() {
+  }
+  
+  /**
+   * Test of getManagedTypes method, of class JdbcFieldDisplayer.
+   */
+  @Test
+  public void testGetManagedTypes() {
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    String[] result = instance.getManagedTypes();
+    assertNotNull(result);
+    assertThat(result, org.hamcrest.collection.IsArrayWithSize.arrayWithSize(1));
+    assertThat(result, org.hamcrest.collection.IsArrayContaining.hasItemInArray(JdbcField.TYPE));
+  }
+
+  /**
+   * Test of displayScripts method, of class JdbcFieldDisplayer.
+   */
+  @Test
+  public void testDisplayScripts() throws Exception {
+    ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
+    PrintWriter printer = new PrintWriter(new OutputStreamWriter(out, Charsets.UTF_8), true);
+    FieldTemplate template = mock(FieldTemplate.class);
+    when(template.getTypeName()).thenReturn(JdbcField.TYPE);
+    when(template.isMandatory()).thenReturn(true);
+    when(template.getLabel("fr")).thenReturn("Mon champs JDBC");
+    when(template.getFieldName()).thenReturn("monChamps");
+    PagesContext pagesContext = new PagesContext();
+    pagesContext.setUseMandatory(true);
+    pagesContext.setCurrentFieldIndex("10");
+    pagesContext.setLastFieldIndex(20);
+    pagesContext.setLanguage("fr");
+    pagesContext.setEncoding("UTF-8");
+    pagesContext.setUserId("0");
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    instance.displayScripts(printer, template, pagesContext);
+    assertEquals("if (isWhitespace(stripInitialWhitespace(field.value))) {\n"
+        + "\t\terrorMsg+=\"  - 'Mon champs JDBC' doit être renseigné\\n \";\n"
+        + "\t\terrorNb++;\n"
+        + "\t}\n"
+        + " try { \n"
+        + "if (typeof(checkmonChamps) == 'function')\n"
+        + " 	checkmonChamps('fr');\n"
+        + " } catch (e) { \n"
+        + " 	//catch all exceptions\n"
+        + " }", new String(out.toByteArray(), Charsets.UTF_8).trim());
+  }
+
+  /**
+   * Test of display method, of class JdbcFieldDisplayer.
+   */
+  @Test
+  public void testDisplay() throws Exception {
+    ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
+    PrintWriter printer = new PrintWriter(new OutputStreamWriter(out, Charsets.UTF_8), true);
+    FieldTemplate template = mock(FieldTemplate.class);    
+    when(template.getTypeName()).thenReturn(JdbcField.TYPE);
+    when(template.isMandatory()).thenReturn(true);
+    when(template.getLabel("fr")).thenReturn("Mon champs JDBC");
+    when(template.getFieldName()).thenReturn("monChamps");
+    PagesContext pagesContext = new PagesContext();
+    pagesContext.setUseMandatory(true);
+    pagesContext.setCurrentFieldIndex("10");
+    pagesContext.setLastFieldIndex(20);
+    pagesContext.setLanguage("fr");
+    pagesContext.setEncoding("UTF-8");
+    pagesContext.setUserId("0");
+    JdbcField field = mock(JdbcField.class);
+    when(field.getTypeName()).thenReturn(JdbcField.TYPE);
+    int size = 5;
+    List<String> resList = new ArrayList<String>(size);
+    for(int i = 0; i< size; i++) {
+      resList.add(String.valueOf(i));
+    }
+    when(field.selectSql(null, null, "0")).thenReturn(resList);
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    long start = System.currentTimeMillis();
+    instance.display(printer, field, template, pagesContext);
+    String display = new String(out.toByteArray(), Charsets.UTF_8).trim();
+    assertNotNull(display);
+    assertEquals(1390, display.length());
+  }
+
+  /**
+   * Test of update method, of class JdbcFieldDisplayer.
+   */
+  @Test
+  public void testUpdate() throws Exception {
+    String newValue = "newValue";
+    PagesContext pagesContext = new PagesContext();
+    pagesContext.setUseMandatory(true);
+    pagesContext.setCurrentFieldIndex("10");
+    pagesContext.setLastFieldIndex(20);
+    pagesContext.setLanguage("fr");
+    pagesContext.setEncoding("UTF-8");
+    pagesContext.setUserId("0");
+    JdbcField field =  mock(JdbcField.class);
+    when(field.getTypeName()).thenReturn(JdbcField.TYPE);
+    when(field.isReadOnly()).thenReturn(false);
+    when(field.acceptValue("newValue", "fr")).thenReturn(true);    
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    instance.update(newValue, field, null, pagesContext);
+    verify(field).setValue("newValue", "fr");
+  }
+  
+  @Test(expected=com.silverpeas.form.FormException.class)
+  public void testUpdateIncorrectField() throws Exception {
+    String newValue = "";
+    Field field = mock(Field.class);
+    when(field.getTypeName()).thenReturn(RandomGenerator.getRandomString());
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    instance.update(newValue, field, null, null);
+  }
+  
+  @Test(expected=com.silverpeas.form.FormException.class)
+  public void testUpdateIncorrectValue() throws Exception {
+    String newValue = "";
+    PagesContext pagesContext = new PagesContext();
+    pagesContext.setUseMandatory(true);
+    pagesContext.setCurrentFieldIndex("10");
+    pagesContext.setLastFieldIndex(20);
+    pagesContext.setLanguage("fr");
+    pagesContext.setEncoding("UTF-8");
+    pagesContext.setUserId("0");
+    JdbcField field =  mock(JdbcField.class);
+    when(field.getTypeName()).thenReturn(JdbcField.TYPE);
+    when(field.isReadOnly()).thenReturn(true);
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    instance.update(newValue, field, null, pagesContext);
+  }
+
+  /**
+   * Test of isDisplayedMandatory method, of class JdbcFieldDisplayer.
+   */
+  @Test
+  public void testIsDisplayedMandatory() {
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    assertTrue(instance.isDisplayedMandatory());
+  }
+
+  /**
+   * Test of getNbHtmlObjectsDisplayed method, of class JdbcFieldDisplayer.
+   */
+  @Test
+  public void testGetNbHtmlObjectsDisplayed() {
+    FieldTemplate template = null;
+    PagesContext pagesContext = null;
+    JdbcFieldDisplayer instance = new JdbcFieldDisplayer();
+    int expResult = 1;
+    int result = instance.getNbHtmlObjectsDisplayed(template, pagesContext);
+    assertEquals(expResult, result);
+  }
+}
