9cef8f8163e33dd68e6485a81ceeb8be724859d8
NicolasEYSSERIC <nicolas.eysseric@silverpeas.com>
2011-03-07 16:59:46 +0100

    fixing bug #1662 Improving ThumbnailDAO.java

--- lib-core/src/main/java/com/silverpeas/thumbnail/model/ThumbnailDAO.java
+++ lib-core/src/main/java/com/silverpeas/thumbnail/model/ThumbnailDAO.java
@@ -36,6 +36,9 @@ import com.stratelia.webactiv.util.exception.UtilException;
 public class ThumbnailDAO {
 
   private static String thumbnailTableName = "SB_Thumbnail_Thumbnail";
+  private static String thumbnailColumnNames =
+      " instanceid, objectid, objecttype, originalattachmentname, modifiedattachmentname, "
+          + "mimetype, xstart, ystart, xlength, ylength ";
 
   public ThumbnailDAO() {
   }
@@ -43,7 +46,7 @@ public class ThumbnailDAO {
   public static ThumbnailDetail insertThumbnail(Connection con,
       ThumbnailDetail thumbnailDetail) throws SQLException, UtilException {
     String insertQuery = "insert into "
-        + thumbnailTableName
+        + thumbnailTableName + " (" + thumbnailColumnNames + ")"
         + " values ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ?)";
     PreparedStatement prepStmt = null;
     try {
@@ -52,41 +55,41 @@ public class ThumbnailDAO {
       prepStmt.setInt(2, thumbnailDetail.getObjectId());
       prepStmt.setInt(3, thumbnailDetail.getObjectType());
       prepStmt.setString(4, thumbnailDetail.getOriginalFileName());
-      if(thumbnailDetail.getCropFileName() != null){
-    	  prepStmt.setString(5, thumbnailDetail.getCropFileName());
-      }else{
-    	  prepStmt.setNull(5, Types.VARCHAR);
+      if (thumbnailDetail.getCropFileName() != null) {
+        prepStmt.setString(5, thumbnailDetail.getCropFileName());
+      } else {
+        prepStmt.setNull(5, Types.VARCHAR);
       }
-      if(thumbnailDetail.getMimeType() != null){
-    	  prepStmt.setString(6, thumbnailDetail.getMimeType());
-      }else{
-    	  prepStmt.setNull(6, Types.VARCHAR);
+      if (thumbnailDetail.getMimeType() != null) {
+        prepStmt.setString(6, thumbnailDetail.getMimeType());
+      } else {
+        prepStmt.setNull(6, Types.VARCHAR);
       }
-      if(thumbnailDetail.getXStart() != -1){
-    	  prepStmt.setInt(7, thumbnailDetail.getXStart());
-      }else{
-    	  prepStmt.setNull(7, Types.INTEGER);
+      if (thumbnailDetail.getXStart() != -1) {
+        prepStmt.setInt(7, thumbnailDetail.getXStart());
+      } else {
+        prepStmt.setNull(7, Types.INTEGER);
       }
-      if(thumbnailDetail.getYStart() != -1){
-      prepStmt.setInt(8, thumbnailDetail.getYStart());
-      }else{
-    	  prepStmt.setNull(8, Types.INTEGER);
+      if (thumbnailDetail.getYStart() != -1) {
+        prepStmt.setInt(8, thumbnailDetail.getYStart());
+      } else {
+        prepStmt.setNull(8, Types.INTEGER);
       }
-      if(thumbnailDetail.getXLength() != -1){
-      prepStmt.setInt(9, thumbnailDetail.getXLength());
-      }else{
-    	  prepStmt.setNull(9, Types.INTEGER);
+      if (thumbnailDetail.getXLength() != -1) {
+        prepStmt.setInt(9, thumbnailDetail.getXLength());
+      } else {
+        prepStmt.setNull(9, Types.INTEGER);
       }
-      if(thumbnailDetail.getYLength() != -1){
-      	prepStmt.setInt(10, thumbnailDetail.getYLength());
-      }else{
-    	  prepStmt.setNull(10, Types.INTEGER);
+      if (thumbnailDetail.getYLength() != -1) {
+        prepStmt.setInt(10, thumbnailDetail.getYLength());
+      } else {
+        prepStmt.setNull(10, Types.INTEGER);
       }
       prepStmt.executeUpdate();
     } finally {
       DBUtil.close(prepStmt);
     }
-    
+
     return thumbnailDetail;
   }
 
@@ -94,20 +97,22 @@ public class ThumbnailDAO {
       throws SQLException {
     String updateQuery =
         "update " + thumbnailTableName +
-        " set xstart = ?, ystart = ?, xlength = ?, ylength = ?, modifiedattachmentname = ?"
-        + " where objectId = ? and objectType = ? and instanceId = ? ";
+            " set xstart = ?, ystart = ?, xlength = ?, ylength = ?, "
+            + " originalattachmentname = ?, modifiedattachmentname = ? "
+            + " where objectId = ? and objectType = ? and instanceId = ? ";
     PreparedStatement prepStmt = null;
     try {
       prepStmt = con.prepareStatement(updateQuery);
-      prepStmt.setInt(1, thumbToUpdate.getXStart() );
-      prepStmt.setInt(2, thumbToUpdate.getYStart() );
-      prepStmt.setInt(3, thumbToUpdate.getXLength() );
-      prepStmt.setInt(4, thumbToUpdate.getYLength() );
-      prepStmt.setString(5, thumbToUpdate.getCropFileName() );
-      prepStmt.setInt(6, thumbToUpdate.getObjectId() );
-      prepStmt.setInt(7, thumbToUpdate.getObjectType() );
-      prepStmt.setString(8, thumbToUpdate.getInstanceId() );
-      
+      prepStmt.setInt(1, thumbToUpdate.getXStart());
+      prepStmt.setInt(2, thumbToUpdate.getYStart());
+      prepStmt.setInt(3, thumbToUpdate.getXLength());
+      prepStmt.setInt(4, thumbToUpdate.getYLength());
+      prepStmt.setString(5, thumbToUpdate.getOriginalFileName());
+      prepStmt.setString(6, thumbToUpdate.getCropFileName());
+      prepStmt.setInt(7, thumbToUpdate.getObjectId());
+      prepStmt.setInt(8, thumbToUpdate.getObjectType());
+      prepStmt.setString(9, thumbToUpdate.getInstanceId());
+
       prepStmt.executeUpdate();
     } finally {
       DBUtil.close(prepStmt);
@@ -127,68 +132,70 @@ public class ThumbnailDAO {
       prepStmt.setInt(2, objectType);
       prepStmt.setString(3, instanceId);
       prepStmt.executeUpdate();
-      
+
     } finally {
       DBUtil.close(prepStmt);
     }
   }
-  
+
   public static void deleteAllThumbnails(Connection con, String instanceId)
-  throws SQLException {
-	PreparedStatement prepStmt = null;
-	
-	try {
-	  String deleteQuery = "delete from " + thumbnailTableName
-	      + " where instanceId = ?";
-	
-	  prepStmt = con.prepareStatement(deleteQuery);
-	  prepStmt.setString(1, instanceId);
-	  prepStmt.executeUpdate();
-	  
-	} finally {
-	  DBUtil.close(prepStmt);
-	}
-}
-  
+      throws SQLException {
+    PreparedStatement prepStmt = null;
+
+    try {
+      String deleteQuery = "delete from " + thumbnailTableName
+          + " where instanceId = ?";
+
+      prepStmt = con.prepareStatement(deleteQuery);
+      prepStmt.setString(1, instanceId);
+      prepStmt.executeUpdate();
+
+    } finally {
+      DBUtil.close(prepStmt);
+    }
+  }
+
   public static ThumbnailDetail selectByKey(Connection con,
-	      String instanceId, int objectId, int objectType) throws SQLException {
-	       SilverTrace.info("publication", "ThumbnailDAO.selectByPubId()",
-		        "root.MSG_GEN_ENTER_METHOD", "objectId = " + objectId + "objectType" + objectType + "instanceId" + instanceId);
-		    ResultSet rs = null;
-		    ThumbnailDetail thumbnailDetail = null;
-		    String selectStatement = "select instanceid, objectid, objecttype, " +
-								     "originalattachmentname, modifiedattachmentname, " +
-								     "mimetype, xstart, ystart, xlength, ylength " + 
-								     "from sb_thumbnail_thumbnail where objectId = ? and objectType = ? and instanceId = ?";
-		    PreparedStatement prepStmt = null;
+        String instanceId, int objectId, int objectType) throws SQLException {
+    SilverTrace.info("publication", "ThumbnailDAO.selectByPubId()",
+            "root.MSG_GEN_ENTER_METHOD", "objectId = " + objectId + "objectType" + objectType +
+                "instanceId" + instanceId);
+    ResultSet rs = null;
+    ThumbnailDetail thumbnailDetail = null;
+    String selectStatement =
+        "select " +
+            thumbnailColumnNames +
+                     "from sb_thumbnail_thumbnail where objectId = ? and objectType = ? and instanceId = ?";
+    PreparedStatement prepStmt = null;
 
-		    try {
-		      prepStmt = con.prepareStatement(selectStatement);
-		      prepStmt.setInt(1, objectId);
-		      prepStmt.setInt(2, objectType);
-		      prepStmt.setString(3, instanceId);
+    try {
+      prepStmt = con.prepareStatement(selectStatement);
+      prepStmt.setInt(1, objectId);
+      prepStmt.setInt(2, objectType);
+      prepStmt.setString(3, instanceId);
 
-		      rs = prepStmt.executeQuery();
-		      if (rs.next()) {
-		    	  thumbnailDetail = resultSet2ThumbDetail(rs);
-		      }
-		    } finally {
-		      DBUtil.close(rs, prepStmt);
-		    }
+      rs = prepStmt.executeQuery();
+      if (rs.next()) {
+        thumbnailDetail = resultSet2ThumbDetail(rs);
+      }
+    } finally {
+      DBUtil.close(rs, prepStmt);
+    }
 
-		    return thumbnailDetail;
-	  }
- 
- private static ThumbnailDetail resultSet2ThumbDetail(ResultSet rs) throws SQLException {
-	    
-	    ThumbnailDetail thumbnailDetail = new ThumbnailDetail(rs.getString(1), rs.getInt(2), rs.getInt(3));
-	    thumbnailDetail.setOriginalFileName(rs.getString(4));
-	    thumbnailDetail.setCropFileName(rs.getString(5));
-	    thumbnailDetail.setMimeType(rs.getString(6));
-	    thumbnailDetail.setXStart(rs.getInt(7));
-	    thumbnailDetail.setYStart(rs.getInt(8));
-	    thumbnailDetail.setXLength(rs.getInt(9));
-	    thumbnailDetail.setYLength(rs.getInt(10));
-	    return thumbnailDetail;
- }
+    return thumbnailDetail;
+  }
+
+  private static ThumbnailDetail resultSet2ThumbDetail(ResultSet rs) throws SQLException {
+    ThumbnailDetail thumbnailDetail =
+        new ThumbnailDetail(rs.getString("instanceid"), rs.getInt("objectid"), rs
+            .getInt("objecttype"));
+    thumbnailDetail.setOriginalFileName(rs.getString("originalattachmentname"));
+    thumbnailDetail.setCropFileName(rs.getString("modifiedattachmentname"));
+    thumbnailDetail.setMimeType(rs.getString("mimetype"));
+    thumbnailDetail.setXStart(rs.getInt("xstart"));
+    thumbnailDetail.setYStart(rs.getInt("ystart"));
+    thumbnailDetail.setXLength(rs.getInt("xlength"));
+    thumbnailDetail.setYLength(rs.getInt("ylength"));
+    return thumbnailDetail;
+  }
 }
