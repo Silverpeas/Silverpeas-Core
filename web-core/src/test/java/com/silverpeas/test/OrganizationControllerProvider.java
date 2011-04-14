3f394c17984ee958408b8757becff1470af41655
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2011-04-14 09:14:01 +0200

    update the copyright header of the Silverpeas license to take into account this year

--- web-core/src/test/java/com/silverpeas/test/OrganizationControllerProvider.java
+++ web-core/src/test/java/com/silverpeas/test/OrganizationControllerProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000 - 2009 Silverpeas
+ * Copyright (C) 2000 - 2011 Silverpeas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
