b6af52d24dfe482d28847fb9fb7ac7cebf369038
Emmanuel Hugonnet <emmanuel.hugonnet@gmail.com>
2011-12-29 16:35:27 +0100

    feature #2824 : Adding support for Digest Authentication on Webdav

--- lib-core/src/main/java/com/stratelia/webactiv/jaas/SilverpeasLoginModule.java
+++ lib-core/src/main/java/com/stratelia/webactiv/jaas/SilverpeasLoginModule.java
@@ -1,38 +1,37 @@
 /**
  * Copyright (C) 2000 - 2011 Silverpeas
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * As a special exception to the terms and conditions of version 3.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
- * the FLOSS exception, and it is also available here:
+ * As a special exception to the terms and conditions of version 3.0 of the GPL, you may
+ * redistribute this Program in connection with Free/Libre Open Source Software ("FLOSS")
+ * applications as described in Silverpeas's FLOSS exception. You should have received a copy of the
+ * text describing the FLOSS exception, and it is also available here:
  * "http://repository.silverpeas.com/legal/licensing"
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.stratelia.webactiv.jaas;
 
+import com.silverpeas.jcrutil.security.impl.DigestCredentials;
 import com.silverpeas.jcrutil.security.impl.SilverpeasCredentials;
 import com.silverpeas.jcrutil.security.impl.SilverpeasSystemCredentials;
 import com.silverpeas.jcrutil.security.impl.SilverpeasSystemPrincipal;
 import com.stratelia.silverpeas.authentication.LoginPasswordAuthentication;
 import com.stratelia.webactiv.beans.admin.Admin;
 import com.stratelia.webactiv.beans.admin.AdminException;
+import com.stratelia.webactiv.beans.admin.AdminReference;
 import com.stratelia.webactiv.beans.admin.Domain;
 import com.stratelia.webactiv.beans.admin.OrganizationController;
 import com.stratelia.webactiv.beans.admin.UserDetail;
+import com.stratelia.webactiv.beans.admin.UserFull;
 import com.stratelia.webactiv.util.exception.WithNested;
 import org.apache.jackrabbit.core.security.AnonymousPrincipal;
 import org.apache.jackrabbit.core.security.authentication.CredentialsCallback;
@@ -49,20 +48,17 @@ import javax.security.auth.spi.LoginModule;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import org.apache.jackrabbit.util.Text;
 
 public class SilverpeasLoginModule implements LoginModule {
 
   private String userId;
-
   // initial state
   private Subject subject;
   private CallbackHandler callbackHandler;
   private Set principals = new HashSet();
-
   private LoginPasswordAuthentication authenticator;
-
   private OrganizationController controller;
-
   private Admin administrator;
 
   public String getUserId() {
@@ -85,6 +81,7 @@ public class SilverpeasLoginModule implements LoginModule {
     this.administrator = administrator;
   }
 
+  @Override
   public boolean abort() throws LoginException {
     if (principals.isEmpty()) {
       return false;
@@ -94,6 +91,7 @@ public class SilverpeasLoginModule implements LoginModule {
     return true;
   }
 
+  @Override
   public boolean commit() throws LoginException {
     if (principals.isEmpty()) {
       return false;
@@ -103,8 +101,9 @@ public class SilverpeasLoginModule implements LoginModule {
     }
   }
 
-  public void initialize(Subject subject, CallbackHandler callbackHandler,
-      Map sharedState, Map options) {
+  @Override
+  public void initialize(Subject subject, CallbackHandler callbackHandler, Map sharedState,
+      Map options) {
     this.subject = subject;
     this.callbackHandler = callbackHandler;
   }
@@ -119,7 +118,7 @@ public class SilverpeasLoginModule implements LoginModule {
     try {
       // Get credentials using a JAAS callback
       CredentialsCallback ccb = new CredentialsCallback();
-      callbackHandler.handle(new Callback[] { ccb });
+      callbackHandler.handle(new Callback[]{ccb});
       Credentials creds = ccb.getCredentials();
       // Use the credentials to set up principals
       if (creds != null) {
@@ -128,17 +127,15 @@ public class SilverpeasLoginModule implements LoginModule {
           // authenticate
           Domain[] domains = controller.getAllDomains();
           for (Domain domain : domains) {
-            String key = authenticator.authenticate(sc.getUserID(),
-                new String(sc.getPassword()), domain
-                .getId(), null);
+            String key = authenticator.authenticate(sc.getUserID(), new String(sc.getPassword()),
+                domain.getId(), null);
             if (key != null && !key.startsWith("Error_")) {
-              String userId = administrator.authenticate(key, null, false);
+              userId = administrator.authenticate(key, null, false);
               SilverpeasUserPrincipal principal = new SilverpeasUserPrincipal(userId);
               fillPrincipal(principal);
               principals.add(principal);
             }
           }
-
           if (principals.isEmpty() && UserDetail.isAnonymousUser(sc.getUserID())) {
             principals.add(new AnonymousPrincipal());
           }
@@ -153,6 +150,24 @@ public class SilverpeasLoginModule implements LoginModule {
           SilverpeasSystemPrincipal principal = new SilverpeasSystemPrincipal();
           principals.add(principal);
           authenticated = true;
+        } else if (creds instanceof DigestCredentials) {
+          DigestCredentials sc = (DigestCredentials) creds;
+          // authenticate
+          Domain[] domains = controller.getAllDomains();
+          for (Domain domain : domains) {
+            String key = authenticator.authenticate(sc.getUsername(), domain.getId(), null);
+            if (key != null && !key.startsWith("Error_")) {
+              userId = administrator.authenticate(key, null, false);
+              SilverpeasUserPrincipal principal = new SilverpeasUserPrincipal(userId);
+              validateDigestUser(principal, sc);
+              fillPrincipal(principal);
+              principals.add(principal);
+            }
+          }
+          if (principals.isEmpty() && UserDetail.isAnonymousUser(sc.getUsername())) {
+            principals.add(new AnonymousPrincipal());
+          }
+          authenticated = true;
         }
       } else {
         principals.add(new AnonymousPrincipal());
@@ -163,7 +178,7 @@ public class SilverpeasLoginModule implements LoginModule {
     } catch (UnsupportedCallbackException uce) {
       throw new LoginException(uce.getCallback().toString() + " not available");
     } catch (AdminException e) {
-      StringBuffer message = new StringBuffer();
+      StringBuilder message = new StringBuilder();
 
       Throwable ex = e;
       int i = 0;
@@ -186,6 +201,7 @@ public class SilverpeasLoginModule implements LoginModule {
     }
   }
 
+  @Override
   public boolean logout() throws LoginException {
     subject.getPrincipals().removeAll(principals);
     principals.clear();
@@ -202,4 +218,13 @@ public class SilverpeasLoginModule implements LoginModule {
     }
   }
 
+  public boolean validateDigestUser(SilverpeasUserPrincipal principal, DigestCredentials sc) throws
+      AdminException {
+    UserFull user = AdminReference.getAdminService().getUserFull(userId);
+    String md5a1 = Text.md5(user.getPassword());
+    String serverDigestValue = md5a1 + ":" + sc.getNonce() + ":" + sc.getNc() + ":" +
+        sc.getCnonce() + ":" + sc.getQop() + ":" + sc.getMd5a2();
+    String serverDigest = Text.md5(serverDigestValue);
+    return serverDigest.equals(sc.getClientDigest());
+  }
 }
