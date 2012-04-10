4f5ee9ffa1e63fcaeb2f55d9a8b5dca2a4ad33ed
Etienne Bonnet <etienne.bonnet@silverpeas.com>
2012-04-10 09:19:11 +0200

    update bug #3093, update other database script (without any changes)

---
+++ config-core/src/main/config/dbRepository/postgres/pdc/008/drop_table.sql
@@ -0,0 +1,11 @@
+drop table SB_Pdc_Axis;
+drop table SB_Pdc_Utilization;
+drop table SB_Pdc_AxisI18N;
+drop table SB_Pdc_User_Rights;
+drop table SB_Pdc_Group_Rights;
+
+drop table PdcPosition_PdcAxisValue;
+drop table PdcClassification_PdcPosition;
+drop table PdcClassification;
+drop table PdcPosition;
+drop table PdcAxisValue;
