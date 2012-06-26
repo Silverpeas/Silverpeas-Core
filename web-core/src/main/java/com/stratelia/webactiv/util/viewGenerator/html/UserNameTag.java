50a971d0157b387831ca23bea245e6edd713f1d6
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2012-06-26 14:39:57 +0200

    Feature #3111. Wrap the use of the javascript plugin userzoom by a JSTL tag named username so that the behaviour with the userzoom is located at a single place and can be modified without changing all the JSPs that use it.

---
+++ web-core/src/main/java/com/stratelia/webactiv/util/viewGenerator/html/UserNameTag.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2000 - 2012 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection withWriter Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have recieved a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "http://www.silverpeas.org/legal/licensing"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stratelia.webactiv.util.viewGenerator.html;
+
+import static com.silverpeas.util.StringUtil.isDefined;
+import com.stratelia.silverpeas.peasCore.MainSessionController;
+import static com.stratelia.silverpeas.peasCore.MainSessionController.MAIN_SESSION_CONTROLLER_ATT;
+import com.stratelia.webactiv.beans.admin.UserDetail;
+import static com.stratelia.webactiv.util.viewGenerator.html.JavascriptPluginInclusion.includeUserZoom;
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import org.apache.ecs.ElementContainer;
+import org.apache.ecs.xhtml.span;
+
+/**
+ * This tag prints out the full name of a given user.
+ *
+ * Yet, it prints the user name and uses the Silverpeas userzoom javascript plugin to render its
+ * status and to provide a way for interacting with him.
+ *
+ * If the user is the current one behind the HTTP session, then by default the user zoom isn't
+ * activated.
+ *
+ * @author mmoquillon
+ */
+public class UserNameTag extends SimpleTagSupport {
+
+  private String userId;
+  private boolean zoom = true;
+
+  /**
+   * Gets the unique identifier of the user concerned by this tag.
+   *
+   * @return the user unique identifier.
+   */
+  public String getUserId() {
+    return userId;
+  }
+
+  /**
+   * Sets the unique identifier of the user whose the name has to be printed out.
+   *
+   * @param userId the user unique identifier.
+   */
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  /**
+   * Is the user zoom to be used with this user? By default the user zoom is used. This method is to
+   * force the deactivation of the user zoom feature.
+   *
+   * @return true if the user zoom is activated, false otherwise.
+   */
+  public boolean isZoom() {
+    return zoom;
+  }
+
+  /**
+   * Sets explicitly the use or not of the user zoom.
+   *
+   * @param zoom true if the user zoom has to be used (default behaviour), false otherwise.
+   */
+  public void setZoom(boolean zoom) {
+    this.zoom = zoom;
+  }
+
+  @Override
+  public void doTag() throws JspException, IOException {
+    if (isDefined(userId)) {
+      UserDetail user = UserDetail.getById(userId);
+      span userName = new span(user.getDisplayedName());
+      if (!userId.equals(getCurrentUserIdInSession())) {
+        userName.setClass("userToZoom");
+        userName.addAttribute("rel", userId);
+      }
+      ElementContainer container = includeUserZoom(new ElementContainer());
+      container.addElement(userName);
+      container.output(getOut());
+    }
+  }
+
+  protected String getCurrentUserIdInSession() {
+    MainSessionController session = (MainSessionController) getJspContext().getAttribute(
+            MAIN_SESSION_CONTROLLER_ATT, PageContext.SESSION_SCOPE);
+    return session.getUserId();
+  }
+  
+  protected JspWriter getOut() {
+    return getJspContext().getOut();
+  }
+}
