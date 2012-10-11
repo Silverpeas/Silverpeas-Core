cad4ad97542389dedef120ea737c4d493aba99b4
Nicolas Eysseric <nicolas.eysseric@silverperpeas.com>
2012-10-11 14:57:13 +0200

    fixing bug #3810 Now, string is trimmed in StringUtil.isDefined(str)...

--- lib-core/src/test/java/com/silverpeas/util/StringUtilTest.java
+++ lib-core/src/test/java/com/silverpeas/util/StringUtilTest.java
@@ -51,7 +51,7 @@ public class StringUtilTest {
   @Test
   public void testIsDefined() {
     assertTrue(StringUtil.isDefined("1"));
-    assertTrue(StringUtil.isDefined("   "));
+    assertFalse(StringUtil.isDefined("   "));
     assertFalse(StringUtil.isDefined(""));
     assertFalse(StringUtil.isDefined("null"));
     assertFalse(StringUtil.isDefined("NuLl"));
