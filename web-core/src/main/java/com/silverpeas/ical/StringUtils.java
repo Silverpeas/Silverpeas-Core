3540c678d40dd1591fc4007347e12e50ebba2006
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2012-11-07 12:11:02 +0100

    Adding utility method to convert a String to a Charset

--- web-core/src/main/java/com/silverpeas/ical/StringUtils.java
+++ web-core/src/main/java/com/silverpeas/ical/StringUtils.java
@@ -22,21 +22,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//
+// Based on code from 
 // GCALDaemon is an OS-independent Java program that offers two-way
 // synchronization between Google Calendar and various iCalalendar (RFC 2445)
 // compatible calendar applications (Sunbird, Rainlendar, iCal, Lightning, etc).
-//
-// Apache License
-// Version 2.0, January 2004
-// http://www.apache.org/licenses/
-// 
 // Project home:
 // http://gcaldaemon.sourceforge.net
 //
 package com.silverpeas.ical;
 
-import org.apache.commons.compress.utils.Charsets;
 import org.apache.commons.lang3.CharEncoding;
 
 import javax.xml.bind.DatatypeConverter;
@@ -46,9 +40,10 @@ import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 
+import org.silverpeas.util.Charsets;
+
 /**
- * Common String utilities (formatters, converters, etc). Created: Jan 03, 2007 12:50:56 PM
- * @author Andras Berkes
+ * Common String utilities (formatters, converters, etc). 
  */
 public final class StringUtils {
 
