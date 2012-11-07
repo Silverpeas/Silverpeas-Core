39bdbff964e1792338aa9524c004c031afecfa6a
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2012-11-07 11:31:10 +0100

    Simplifying the code and removing another Base64 encoder/decoder implementation

--- web-core/src/main/java/com/silverpeas/ical/StringUtils.java
+++ web-core/src/main/java/com/silverpeas/ical/StringUtils.java
@@ -36,14 +36,15 @@
 //
 package com.silverpeas.ical;
 
+import org.apache.commons.compress.utils.Charsets;
+import org.apache.commons.lang3.CharEncoding;
+
+import javax.xml.bind.DatatypeConverter;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
-import java.util.HashMap;
 
 /**
  * Common String utilities (formatters, converters, etc). Created: Jan 03, 2007 12:50:56 PM
@@ -51,79 +52,44 @@ import java.util.HashMap;
  */
 public final class StringUtils {
 
-  private static final byte[] BASE64_CHARS = new byte[256];
-  private final static char[] ALPHABETS =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-      .toCharArray();
-
-  public static final String US_ASCII = "US-ASCII";
-  public static final String ISO_8859_1 = "ISO-8859-1";
-  public static final String ISO_8859_2 = "ISO-8859-2";
-  public static final String UTF_8 = "UTF-8";
-
-  private static final HashMap charsets = new HashMap();
-
-  static {
-    for (int i = 0; i < 256; i++)
-      BASE64_CHARS[i] = -1;
-    for (int i = 'A'; i <= 'Z'; i++)
-      BASE64_CHARS[i] = (byte) (i - 'A');
-    for (int i = 'a'; i <= 'z'; i++)
-      BASE64_CHARS[i] = (byte) (26 + i - 'a');
-    for (int i = '0'; i <= '9'; i++)
-      BASE64_CHARS[i] = (byte) (52 + i - '0');
-    BASE64_CHARS['+'] = 62;
-    BASE64_CHARS['/'] = 63;
-    try {
-      charsets.put(ISO_8859_1, Charset.forName(ISO_8859_1));
-      charsets.put(ISO_8859_2, Charset.forName(ISO_8859_2));
-      charsets.put(UTF_8, Charset.forName(UTF_8));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  static final Charset getCharset(String encoding) {
-    Charset charset = (Charset) charsets.get(encoding);
-    if (charset == null) {
-      charset = Charset.forName(encoding);
-      charsets.put(encoding, charset);
-    }
-    return charset;
-  }
-
-  public static final byte[] encodeString(String string, String encoding)
+  public static byte[] encodeString(String string, Charset encoding)
       throws CharacterCodingException {
     return encodeArray(string.toCharArray(), encoding);
   }
-
-  static final byte[] encodeArray(char[] chars, String encoding)
+  public static byte[] encodeString(String string, String encoding)
       throws CharacterCodingException {
-    if (encoding.equals(US_ASCII)) {
+    return encodeString(string, Charsets.toCharset(encoding));
+  }
+
+  static byte[] encodeArray(char[] chars, Charset encoding)
+      throws CharacterCodingException {
+    if (CharEncoding.US_ASCII.equals(encoding.name())) {
       byte[] array = new byte[chars.length];
       for (int i = 0; i < array.length; i++) {
         array[i] = (byte) chars[i];
       }
       return array;
     }
-    ByteBuffer buffer = getEncoder(encoding).encode(CharBuffer.wrap(chars));
+    ByteBuffer buffer = encoding.newEncoder().encode(CharBuffer.wrap(chars));
     byte[] array = new byte[buffer.limit()];
     System.arraycopy(buffer.array(), 0, array, 0, array.length);
     return array;
   }
 
-  public static final CharsetEncoder getEncoder(String encoding) {
-    return getCharset(encoding).newEncoder();
+  public static String decodeToString(byte[] bytes, String encoding)
+      throws UnsupportedEncodingException {
+    return new String(decodeToArray(bytes, Charsets.toCharset(encoding)));
   }
-
-  public static final String decodeToString(byte[] bytes, String encoding)
+ 
+  
+  public static String decodeToString(byte[] bytes, Charset encoding)
       throws UnsupportedEncodingException {
     return new String(decodeToArray(bytes, encoding));
   }
 
-  static final char[] decodeToArray(byte[] bytes, String encoding)
+  static char[] decodeToArray(byte[] bytes, Charset encoding)
       throws UnsupportedEncodingException {
-    if (encoding.equals(US_ASCII)) {
+    if (CharEncoding.US_ASCII.equals(encoding.name())) {
       char[] array = new char[bytes.length];
       for (int i = 0; i < array.length; i++) {
         array[i] = (char) bytes[i];
@@ -131,7 +97,7 @@ public final class StringUtils {
       return array;
     }
     try {
-      CharBuffer buffer = getDecoder(encoding).decode(ByteBuffer.wrap(bytes));
+      CharBuffer buffer = encoding.newDecoder().decode(ByteBuffer.wrap(bytes));
       char[] array = new char[buffer.limit()];
       System.arraycopy(buffer.array(), 0, array, 0, array.length);
       return array;
@@ -140,18 +106,11 @@ public final class StringUtils {
     }
   }
 
-  public static final String decodePassword(String encodedPassword)
-      throws Exception {
+  public static String decodePassword(String encodedPassword) throws Exception {
     StringBuilder buffer = new StringBuilder(encodedPassword.substring(3));
     return decodeBASE64(buffer.reverse().toString().replace('$', '=')).trim();
   }
-
-  static final CharsetDecoder getDecoder(String encoding) {
-    return getCharset(encoding).newDecoder();
-  }
-
-  public static final long stringToLong(String string)
-      throws NumberFormatException {
+  public static long stringToLong(String string) throws NumberFormatException {
     StringBuffer buffer = new StringBuffer(string.toLowerCase());
     long unit = resolveUnit(buffer);
     long value = Long.parseLong(buffer.toString().trim());
@@ -161,7 +120,7 @@ public final class StringUtils {
     return value;
   }
 
-  private static final long resolveUnit(StringBuffer buffer) {
+  private static long resolveUnit(StringBuffer buffer) {
     long unit = 1;
     int i = -1;
     for (;;) {
@@ -237,46 +196,14 @@ public final class StringUtils {
     return unit;
   }
 
-  // --- FAST BASE64 PASSWORD ENCODER/DECODER ---
-
   /**
    * Decodes a BASE64-encoded string.
    * @param string BASE64 string
    * @return String the decoded bytes
+   * @throws UnsupportedEncodingException  
    */
-  public static final String decodeBASE64(String string) throws Exception {
-    char[] data = string.toCharArray();
-    int tempLen = data.length;
-    for (char aData : data) {
-      if ((aData > 255) || BASE64_CHARS[aData] < 0) {
-        --tempLen;
-      }
-    }
-    int len = (tempLen / 4) * 3;
-    if ((tempLen % 4) == 3) {
-      len += 2;
-    }
-    if ((tempLen % 4) == 2) {
-      len += 1;
-    }
-    byte[] bytes = new byte[len];
-    int shift = 0;
-    int accum = 0;
-    int index = 0;
-    int value;
-    for (char aData : data) {
-      value = (aData > 255) ? -1 : BASE64_CHARS[aData];
-      if (value >= 0) {
-        accum <<= 6;
-        shift += 6;
-        accum |= value;
-        if (shift >= 8) {
-          shift -= 8;
-          bytes[index++] = (byte) ((accum >> shift) & 0xff);
-        }
-      }
-    }
-    return decodeToString(bytes, UTF_8);
+  public static String decodeBASE64(String string) throws UnsupportedEncodingException {
+    return decodeToString(DatatypeConverter.parseBase64Binary(string), CharEncoding.UTF_8);
   }
 
   /**
@@ -284,31 +211,8 @@ public final class StringUtils {
    * @param data - byte array to encode
    * @return encoded string
    */
-  public static final String encodeBASE64(byte[] data) throws Exception {
-    char[] chars = new char[((data.length + 2) / 3) * 4];
-    for (int i = 0, index = 0; i < data.length; i += 3, index += 4) {
-      boolean quad = false;
-      boolean trip = false;
-      int val = (0xFF & data[i]);
-      val <<= 8;
-      if ((i + 1) < data.length) {
-        val |= (0xFF & data[i + 1]);
-        trip = true;
-      }
-      val <<= 8;
-      if ((i + 2) < data.length) {
-        val |= (0xFF & data[i + 2]);
-        quad = true;
-      }
-      chars[index + 3] = ALPHABETS[(quad ? (val & 0x3F) : 64)];
-      val >>= 6;
-      chars[index + 2] = ALPHABETS[(trip ? (val & 0x3F) : 64)];
-      val >>= 6;
-      chars[index + 1] = ALPHABETS[val & 0x3F];
-      val >>= 6;
-      chars[index] = ALPHABETS[val & 0x3F];
-    }
-    return new String(chars);
+  public static String encodeBASE64(byte[] data) {
+    return DatatypeConverter.printBase64Binary(data);
   }
 
 }
