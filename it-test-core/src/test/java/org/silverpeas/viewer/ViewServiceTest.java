ecadb3d30a2983bc60057367372d3ba71fb71e0a
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-02-07 22:24:53 +0100

    Fix a pb with an existing temporary directory when deleting it in the test ViewServiceTest

--- it-test-core/src/test/java/org/silverpeas/viewer/ViewServiceTest.java
+++ it-test-core/src/test/java/org/silverpeas/viewer/ViewServiceTest.java
@@ -1,23 +1,19 @@
 package org.silverpeas.viewer;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-
+import com.stratelia.webactiv.util.FileRepositoryManager;
 import java.io.File;
 import java.net.URL;
-
 import javax.inject.Inject;
-
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.stratelia.webactiv.util.FileRepositoryManager;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/spring-viewer.xml")
@@ -26,9 +22,14 @@ public class ViewServiceTest {
   @Inject
   private ViewService viewService;
 
+  @Before
+  public void setup() throws Exception {
+    (new File(FileRepositoryManager.getTemporaryPath())).mkdirs();
+  }
+
   @After
   public void tearDown() throws Exception {
-    FileUtils.cleanDirectory(new File(FileRepositoryManager.getTemporaryPath()));
+    FileUtils.deleteQuietly(new File(FileRepositoryManager.getTemporaryPath()));
   }
 
   @Test
