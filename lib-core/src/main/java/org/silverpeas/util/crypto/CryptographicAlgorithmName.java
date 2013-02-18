08e9e023345f3f4d7f7159df515d9d3c75b7edbc
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-02-18 15:01:20 +0100

    feature #4147 :  Now, the old code to encrypt and decrypt digital data by using a PKS infrastructure (CMS or Cryptographic Message Syntax) is based on the new Silverpeas Cryptography API and it is provided as a Cipher implementation to perform PKS#12 and PKS#7 tasks

--- lib-core/src/main/java/org/silverpeas/util/crypto/CryptographicAlgorithmName.java
+++ lib-core/src/main/java/org/silverpeas/util/crypto/CryptographicAlgorithmName.java
@@ -5,5 +5,14 @@ package org.silverpeas.util.crypto;
  */
 public enum CryptographicAlgorithmName {
 
-  Blowfish;
+  /**
+   * The Blowfish cipher.
+   */
+  Blowfish,
+  /**
+   * The Cryptographic Message Syntax (CMS) based on the syntax of PKCS#7. More than just a cipher,
+   * it is an asymmetric-keys and certificate based enciphering process to encrypt and to decrypt
+   * messages exchanged between two interlocutors.
+   */
+  CMS;
 }
