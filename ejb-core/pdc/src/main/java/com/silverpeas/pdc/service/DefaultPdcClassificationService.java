91bb9b617793b9c24843b630bd9938d39266882e
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-03-08 11:40:02 +0100

    bug #4361 :  Now the existence of axis values are checked before saving a position on the PdC for a predefined classification; the JPA implementation by hibernate doesn't seem to respect correctly the JPA specification with the 'persist' cascading (with the persist cascading in a relationship, the related entities are saved only if they don't exist. Here Hibernate want absolutly create them whatever they already exist or not)

--- ejb-core/pdc/src/main/java/com/silverpeas/pdc/service/DefaultPdcClassificationService.java
+++ ejb-core/pdc/src/main/java/com/silverpeas/pdc/service/DefaultPdcClassificationService.java
@@ -27,6 +27,7 @@ import com.silverpeas.SilverpeasContent;
 import com.silverpeas.pdc.dao.PdcAxisValueRepository;
 import com.silverpeas.pdc.dao.PdcClassificationRepository;
 import com.silverpeas.pdc.model.PdcAxisValue;
+import com.silverpeas.pdc.model.PdcAxisValuePk;
 import com.silverpeas.pdc.model.PdcClassification;
 import static com.silverpeas.pdc.model.PdcClassification.NONE_CLASSIFICATION;
 import com.silverpeas.pdc.model.PdcPosition;
@@ -180,11 +181,14 @@ public class DefaultPdcClassificationService implements PdcClassificationService
             && classification.isEmpty()) {
       classificationRepository.delete(classification);
     } else {
-      List<PdcAxisValue> allValues = new ArrayList<PdcAxisValue>();
       for (PdcPosition aPosition : classification.getPositions()) {
-        allValues.addAll(aPosition.getValues());
+        for (PdcAxisValue aValue : aPosition.getValues()) {
+          PdcAxisValuePk pk = PdcAxisValuePk.aPdcAxisValuePk(aValue.getId(), aValue.getAxisId());
+          if (!valueRepository.exists(pk)) {
+            valueRepository.save(aValue);
+          }
+        }
       }
-      valueRepository.save(allValues);
       savedClassification = classificationRepository.saveAndFlush(classification);
     }
     return savedClassification;
