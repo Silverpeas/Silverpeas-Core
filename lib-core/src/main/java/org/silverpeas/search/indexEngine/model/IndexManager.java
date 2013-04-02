b24f24ed0c60decbf660619cb16ec3dc713b58ed
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-04-02 17:54:15 +0200

    hibernate4 :  Now antlr is provided only for tests (it is required by Hibernate) and clean up properties about truststore settings (normaly they have to be left commented

--- lib-core/src/main/java/org/silverpeas/search/indexEngine/model/IndexManager.java
+++ lib-core/src/main/java/org/silverpeas/search/indexEngine/model/IndexManager.java
@@ -1,25 +1,29 @@
 /**
  * Copyright (C) 2000 - 2012 Silverpeas
- * 
+ *
 * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU Affero General Public License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
 * As a special exception to the terms and conditions of version 3.0 of the GPL, you may
  * redistribute this Program in connection with Free/Libre Open Source Software ("FLOSS")
  * applications as described in Silverpeas's FLOSS exception. You should have received a copy of the
  * text describing the FLOSS exception, and it is also available here:
  * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
- * 
+ *
 * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Affero General Public License for more details.
- * 
+ *
 * You should have received a copy of the GNU Affero General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  */
 package org.silverpeas.search.indexEngine.model;
 
+import com.silverpeas.util.StringUtil;
+import com.silverpeas.util.i18n.I18NHelper;
+import com.stratelia.silverpeas.silvertrace.SilverTrace;
+import com.stratelia.webactiv.util.ResourceLocator;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
@@ -29,11 +33,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.MissingResourceException;
 import java.util.Set;
-
-import javax.ws.rs.HEAD;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LimitTokenCountAnalyzer;
@@ -48,17 +48,10 @@ import org.apache.lucene.index.LogDocMergePolicy;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
-
 import org.silverpeas.attachment.AttachmentServiceFactory;
 import org.silverpeas.search.indexEngine.parser.Parser;
 import org.silverpeas.search.indexEngine.parser.ParserManager;
 import org.silverpeas.search.util.SearchEnginePropertiesManager;
-
-import com.silverpeas.util.StringUtil;
-import com.silverpeas.util.i18n.I18NHelper;
-
-import com.stratelia.silverpeas.silvertrace.SilverTrace;
-import com.stratelia.webactiv.util.ResourceLocator;
 
 /**
  * An IndexManager manage all the web'activ's index. An IndexManager is NOT thread safe : to share
@@ -447,8 +440,9 @@ public class IndexManager {
         }
       }
     }
-
-    AttachmentServiceFactory.getAttachmentService().updateIndexEntryWithDocuments(indexEntry);
+    if (StringUtil.isDefined(indexEntry.getObjectId())) {
+      AttachmentServiceFactory.getAttachmentService().updateIndexEntryWithDocuments(indexEntry);
+    }
 
     List<FileDescription> list2 = indexEntry.getFileContentList();
     for (FileDescription f : list2) {
