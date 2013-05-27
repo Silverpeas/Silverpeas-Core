36404921e8e48dc1e88c2c5df46f5523ce427695
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2013-05-27 14:24:33 +0200

    Fixing extraction of HTML content in multipart format

--- lib-core/src/test/java/org/silverpeas/util/mail/MailExtractorTest.java
+++ lib-core/src/test/java/org/silverpeas/util/mail/MailExtractorTest.java
@@ -36,29 +36,20 @@ public class MailExtractorTest {
 
   private void testBrut(String filename, Calendar calendar) throws Exception {
     File file = new File(dir + filename);
-
     MailExtractor extractor = Extractor.getExtractor(file);
     Mail mail = extractor.getMail();
-
     // check basic attributes
     assertThat(mail.getSubject(), is("Silverpeas : test archivage 1/3 (brut)"));
-
     // check date of transmission
-
-    assertThat(DateUtils.truncate(mail.getDate(), Calendar.MINUTE),
-        is(DateUtils.truncate(calendar.getTime(), Calendar.MINUTE)));
-
-    // check from
+    assertThat(DateUtils.truncate(mail.getDate(), Calendar.MINUTE), is(DateUtils.truncate(
+        calendar.getTime(), Calendar.MINUTE)));    // check from
     assertThat(mail.getFrom().getAddress(), is("nicolas.eysseric@silverpeas.com"));
-
     // check to
     assertThat(mail.getTo().length, is(2));
     assertThat(mail.getCc().length, is(1));
     assertThat(mail.getAllRecipients().length, is(3));
-
     // check body
     assertThat(mail.getBody(), is(notNullValue()));
-
     // check attachments
     List<MailAttachment> attachments = extractor.getAttachments();
     assertThat(attachments.size(), is(0));
@@ -95,12 +86,10 @@ public class MailExtractorTest {
     assertThat(mail.getTo().length, is(2));
     assertThat(mail.getCc(), is(nullValue()));
     assertThat(mail.getAllRecipients().length, is(2));
-
     // check body
     assertThat(mail.getBody(), is(notNullValue()));
     assertThat(mail.getBody().startsWith("Bonjour"), is(true));
     assertThat(mail.getBody().lastIndexOf("Bonjour"), is(0));
-
     // check attachments
     List<MailAttachment> attachments = extractor.getAttachments();
     assertThat(attachments.size(), is(0));
@@ -111,7 +100,18 @@ public class MailExtractorTest {
     String fileName = "Silverpeas test archivage 3_3 (html avec pieces jointes).eml";
     Calendar calendar = Calendar.getInstance();
     calendar.set(2013, 0, 21, 10, 11);
-    testHTMLWithFiles(fileName, calendar);
+    String body = "Bonjour,<div><br></div><div>Ce mail est au format html est permet&nbsp;<u>"
+        + "quelques fantaisies</u>.</div><div>Le glisser/d&eacute;poser des mails dans la GED est "
+        + "une&nbsp;<b>nouvelle fonction tr&egrave;s int&eacute;ressante</b>.</div><div><br></div>"
+        + "<div>\n\nR&eacute;alis&eacute;e &agrave; la demande d&#39;"
+        + "<a href=\"http://www.hydrostadium.com/\" target=\"_blank\">Hydrostadium</a>, elle va "
+        + "&ecirc;tre certainement utilis&eacute;e par d&#39;autres clients de la communaut&eacute;."
+        + "</div><div><br></div>-- <br>Nicolas EYSSERIC<br>\n\n<span style=\"color:rgb(102,102,102)\">"
+        + "Directeur produit</span><br style=\"color:rgb(102,102,102)\">"
+        + "<span style=\"color:rgb(102,102,102)\">06 59 55 18 25 <b>(Attention nouveau "
+        + "num&eacute;ro)</b></span><br>\n-----------------------------<br>Silverpeas<br>1, "
+        + "place Firmin Gautier<br>38000 Grenoble<br>\n";
+    testHTMLWithFiles(fileName, calendar, body);
   }
 
   @Test
@@ -119,42 +119,42 @@ public class MailExtractorTest {
     String fileName = "Silverpeas test archivage 3_3 (html avec pieces jointes).msg";
     Calendar calendar = Calendar.getInstance();
     calendar.set(2013, 0, 21, 13, 56);
-    testHTMLWithFiles(fileName, calendar);
+    String body = "Bonjour,<br/><br/>Ce mail est au format html est permet quelques fantaisies."
+        + "<br/>Le glisser/d&eacute;poser des mails dans la GED est une nouvelle fonction "
+        + "tr&egrave;s<br/>int&eacute;ressante.<br/><br/>R&eacute;alis&eacute;e &agrave; "
+        + "la demande d'Hydrostadium &lt;http://www.hydrostadium.com/&gt; ,<br/>elle va &ecirc;tre "
+        + "certainement utilis&eacute;e par d'autres clients de la<br/>communaut&eacute;.<br/><br/>"
+        + "-- <br/>Nicolas EYSSERIC<br/>Directeur produit<br/>06 59 55 18 25 (Attention nouveau "
+        + "num&eacute;ro)<br/>-----------------------------<br/>Silverpeas<br/>1, place Firmin "
+        + "Gautier<br/>38000 Grenoble<br/><br/>";
+    testHTMLWithFiles(fileName, calendar, body);
+
   }
 
-  private void testHTMLWithFiles(String filename, Calendar calendar) throws Exception {
+  private void testHTMLWithFiles(String filename, Calendar calendar, String body) throws Exception {
     File file = new File(dir + filename);
-
     MailExtractor extractor = Extractor.getExtractor(file);
     Mail mail = extractor.getMail();
-
     // check basic attributes
     assertThat(mail.getSubject(), is("Silverpeas : test archivage 3/3 (hml avec pièces jointes)"));
-
     // check date of transmission
-
-    assertThat(DateUtils.truncate(mail.getDate(), Calendar.MINUTE),
-        is(DateUtils.truncate(calendar.getTime(), Calendar.MINUTE)));
-
+    assertThat(DateUtils.truncate(mail.getDate(), Calendar.MINUTE), is(DateUtils.truncate(
+        calendar.getTime(), Calendar.MINUTE)));
     // check from
     assertThat(mail.getFrom().getAddress(), is("nicolas.eysseric@silverpeas.com"));
-
     // check to
     assertThat(mail.getTo().length, is(1));
     assertThat(mail.getCc().length, is(1));
     assertThat(mail.getAllRecipients().length, is(2));
-
     // check body
     assertThat(mail.getBody(), is(notNullValue()));
-
+    assertThat(mail.getBody(), is(body));
     // check attachments
     List<MailAttachment> attachments = extractor.getAttachments();
     assertThat(attachments.size(), is(2));
-
     MailAttachment attachment = attachments.get(0);
     assertThat(attachment.getName(), is("Liste des applications-V1.0.pdf"));
     assertThat(attachment.getSize(), is(149463L));
-
     attachment = attachments.get(1);
     assertThat(attachment.getName(), is("Silverpeas-SearchEngine.odt"));
   }
