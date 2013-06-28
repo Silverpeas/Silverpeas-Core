e10895ac4c6667823e72a805fa4efb94452ac422
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2013-06-28 10:00:13 +0200

    Fixing little problem where streams might not be closed properly.

--- web-core/src/main/java/com/silverpeas/attachment/web/AttachmentRessource.java
+++ web-core/src/main/java/com/silverpeas/attachment/web/AttachmentRessource.java
@@ -20,21 +20,14 @@
  */
 package com.silverpeas.attachment.web;
 
-import com.silverpeas.annotation.RequestScoped;
-import com.silverpeas.annotation.Service;
-import com.silverpeas.sharing.model.Ticket;
-import com.silverpeas.sharing.security.ShareableAttachment;
-import com.silverpeas.sharing.services.SharingServiceFactory;
-import com.silverpeas.util.MimeTypes;
-import com.silverpeas.util.ZipManager;
-import com.silverpeas.web.RESTWebService;
-import com.stratelia.webactiv.util.FileRepositoryManager;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.output.ByteArrayOutputStream;
-import org.silverpeas.attachment.AttachmentServiceFactory;
-import org.silverpeas.attachment.model.SimpleDocument;
-import org.silverpeas.attachment.model.SimpleDocumentPK;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.StringTokenizer;
+import java.util.UUID;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -45,13 +38,26 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.StreamingOutput;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URI;
-import java.util.StringTokenizer;
-import java.util.UUID;
+import javax.ws.rs.core.UriBuilderException;
+
+import org.silverpeas.attachment.AttachmentServiceFactory;
+import org.silverpeas.attachment.model.SimpleDocument;
+import org.silverpeas.attachment.model.SimpleDocumentPK;
+
+import com.silverpeas.annotation.RequestScoped;
+import com.silverpeas.annotation.Service;
+import com.silverpeas.sharing.model.Ticket;
+import com.silverpeas.sharing.security.ShareableAttachment;
+import com.silverpeas.sharing.services.SharingServiceFactory;
+import com.silverpeas.util.MimeTypes;
+import com.silverpeas.util.ZipManager;
+import com.silverpeas.web.RESTWebService;
+
+import com.stratelia.webactiv.util.FileRepositoryManager;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 
 @Service
 @RequestScoped
@@ -96,8 +102,8 @@ public class AttachmentRessource extends RESTWebService {
   @Produces(MediaType.APPLICATION_JSON)
   public ZipEntity zipFiles(@PathParam("ids") String attachmentIds) {
     StringTokenizer tokenizer = new StringTokenizer(attachmentIds, ",");
-    File folderToZip =
-        FileUtils.getFile(FileRepositoryManager.getTemporaryPath(), UUID.randomUUID().toString());
+    File folderToZip = FileUtils.getFile(FileRepositoryManager.getTemporaryPath(), UUID.randomUUID()
+        .toString());
     while (tokenizer.hasMoreTokens()) {
       SimpleDocumentPK pk = new SimpleDocumentPK(tokenizer.nextToken());
       SimpleDocument attachment = AttachmentServiceFactory.getAttachmentService().
@@ -121,7 +127,11 @@ public class AttachmentRessource extends RESTWebService {
           token).path("zipcontent").path(zipFile.getName()).build();
       long size = ZipManager.compressPathToZip(folderToZip, zipFile);
       return new ZipEntity(getUriInfo().getRequestUri(), downloadUri.toString(), size);
-    } catch (Exception e) {
+    } catch (IllegalArgumentException e) {
+      throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+    } catch (UriBuilderException e) {
+      throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+    } catch (IOException e) {
       throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
     } finally {
       FileUtils.deleteQuietly(folderToZip);
@@ -138,13 +148,19 @@ public class AttachmentRessource extends RESTWebService {
       return Response.ok().build();
     } else {
       ByteArrayOutputStream output = new ByteArrayOutputStream();
+      InputStream in = null;
       try {
-        output.write(new FileInputStream(realFile));
-      } catch (Exception e) {
+        in = new FileInputStream(realFile);
+        output.write(in);
+        return Response.ok().entity(output.toByteArray()).type(MimeTypes.SHORT_ARCHIVE_MIME_TYPE).
+            build();
+      } catch (IOException e) {
         throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+      } finally {
+        IOUtils.closeQuietly(output);
+        IOUtils.closeQuietly(in);
       }
-      return Response.ok().entity(output.toByteArray()).type(MimeTypes.SHORT_ARCHIVE_MIME_TYPE).
-          build();
+
     }
   }
 
