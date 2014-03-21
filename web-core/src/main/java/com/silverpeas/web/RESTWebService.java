50e36d7634acd9c351068e45e219c98f45787e99
Yohann Chastagnier <yohann.chastagnier@silverpeas.com>
2014-03-21 12:36:08 +0100

    Feature #907 : continuing listing the suggestions not published (adding web treatment processing into REST web services mechanism)

--- web-core/src/main/java/com/silverpeas/web/RESTWebService.java
+++ web-core/src/main/java/com/silverpeas/web/RESTWebService.java
@@ -30,18 +30,20 @@ import com.silverpeas.util.StringUtil;
 import com.stratelia.webactiv.SilverpeasRole;
 import com.stratelia.webactiv.beans.admin.UserDetail;
 import com.stratelia.webactiv.util.ResourceLocator;
-import java.util.Collection;
-import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import org.silverpeas.core.admin.OrganisationController;
 import org.silverpeas.notification.message.MessageManager;
 import org.silverpeas.token.Token;
 import org.silverpeas.web.token.SynchronizerTokenService;
 import org.silverpeas.web.token.SynchronizerTokenServiceFactory;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.Collection;
 
 import static com.silverpeas.web.UserPriviledgeValidation.HTTP_AUTHORIZATION;
 import static com.silverpeas.web.UserPriviledgeValidation.HTTP_SESSIONKEY;
@@ -67,6 +69,7 @@ public abstract class RESTWebService {
   private HttpServletResponse httpResponse;
   private UserDetail userDetail = null;
   private Collection<SilverpeasRole> userRoles = null;
+  private SilverpeasRole greaterUserRole;
 
   private ResourceLocator bundle = null;
 
@@ -226,5 +229,90 @@ public abstract class RESTWebService {
       bundle = new ResourceLocator(getBundleLocation(), getUserPreferences().getLanguage());
     }
     return bundle;
+  }
+
+  /**
+   * Gets the greater role of the user behind the service call.
+   * @return
+   */
+  public SilverpeasRole getGreaterUserRole() {
+    if (greaterUserRole == null) {
+      greaterUserRole = SilverpeasRole.getGreaterFrom(getUserRoles());
+    }
+    return greaterUserRole;
+  }
+
+  /**
+   * This method permits to start the setting of a {@link com.silverpeas.web.RESTWebService
+   * .WebTreatment}.
+   * @param webTreatment
+   * @param <RETURN_VALUE>
+   * @return
+   */
+  protected <RETURN_VALUE> WebProcess<RETURN_VALUE> process(
+      WebTreatment<RETURN_VALUE> webTreatment) {
+    return new WebProcess<RETURN_VALUE>(webTreatment);
+  }
+
+  /**
+   * This class handles the execution of a {@link com.silverpeas.web.RESTWebService.WebTreatment}.
+   * It provides the centralization of exception catches and handles the lowest role access that
+   * must the user verify.
+   * @param <RETURN_VALUE> the type of the value returned by the web treatment.
+   * @return the value computed by the specified web treatment.
+   */
+  protected final class WebProcess<RETURN_VALUE> {
+    private final WebTreatment<RETURN_VALUE> webTreatment;
+    private SilverpeasRole lowestRoleAccess = null;
+
+    /**
+     * Default constructor.
+     * @param webTreatment
+     */
+    protected WebProcess(final WebTreatment<RETURN_VALUE> webTreatment) {
+      this.webTreatment = webTreatment;
+    }
+
+    /**
+     * Sets the lowest role access that the user behind the service call must verify.
+     * @param lowestRoleAccess
+     * @return
+     */
+    public WebProcess<RETURN_VALUE> lowestAccessRole(SilverpeasRole lowestRoleAccess) {
+      this.lowestRoleAccess = lowestRoleAccess;
+      return this;
+    }
+
+    /**
+     * This method calls the execute method of a {@link com.silverpeas.web.RESTWebService
+     * .WebTreatment} instance.
+     * One of the aim of this mechanism is to centralize the exception catching and also to avoid
+     * redundant coding around web exceptions.
+     * If a lowest role access is defined, the user must verify it.
+     * If the user isn't authentified, a 401 HTTP code is returned.
+     * If a problem occurs when processing the request, a 503 HTTP code is returned.
+     * @return the value computed by the specified web treatment.
+     */
+    public RETURN_VALUE execute() {
+      try {
+        if (lowestRoleAccess != null && (getGreaterUserRole() == null ||
+            !getGreaterUserRole().isGreaterThanOrEquals(lowestRoleAccess))) {
+          throw new WebApplicationException(Response.Status.FORBIDDEN);
+        }
+        return webTreatment.execute();
+      } catch (final WebApplicationException ex) {
+        throw ex;
+      } catch (final Exception ex) {
+        throw new WebApplicationException(ex, Response.Status.SERVICE_UNAVAILABLE);
+      }
+    }
+  }
+
+  /**
+   * Inner class handled by
+   * @param <RETURN_VALUE>
+   */
+  protected abstract class WebTreatment<RETURN_VALUE> {
+    public abstract RETURN_VALUE execute();
   }
 }
