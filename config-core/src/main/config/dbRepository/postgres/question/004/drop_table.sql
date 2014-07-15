db693f5510cf9a780ea0d6e86de65a6e82f67ac6
ebonnet <etienne.bonnet@silverpeas.com>
2014-07-15 12:11:10 +0200

    feature #5157 update question answer table in order to insert longest URL

---
+++ config-core/src/main/config/dbRepository/postgres/question/004/drop_table.sql
@@ -0,0 +1,4 @@
+DROP TABLE SB_Question_Answer;
+DROP TABLE SB_Question_Question;
+DROP TABLE SB_Question_QuestionResult;
+DROP TABLE SB_Question_Score;
---
+++ config-core/src/main/config/dbRepository/postgres/question/up003/update_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE SB_Question_Answer
+ALTER COLUMN answerImage TYPE varchar(2000);
