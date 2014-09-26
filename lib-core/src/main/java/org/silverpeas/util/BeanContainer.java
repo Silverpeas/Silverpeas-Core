c5c77ca0d2943bc1fb43086e70c2305c10c84503
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2014-09-26 11:52:45 +0200

    Add the ServiceProvider and BeanContainer classes for the unmanaged beans to access the managed beans in CDI

---
+++ lib-core/src/main/java/org/silverpeas/util/BeanContainer.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2000 - 2014 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of the GPL, you may
+ * redistribute this Program in connection with Free/Libre Open Source Software ("FLOSS")
+ * applications as described in Silverpeas's FLOSS exception. You should have received a copy of the
+ * text describing the FLOSS exception, and it is also available here:
+ * "http://www.silverpeas.org/docs/core/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.silverpeas.util;
+
+import java.util.Set;
+
+/**
+ * I represents the strategy used to manage the life-cycle of the objects (aka the beans) in
+ * Silverpeas. Usually, the implementation of this interface is a wrapper or an adaptor to an
+ * existing IoD container (Injection Of Dependency container) such as CDI or Spring.
+ *
+ * All implementation of a bean management container must implement this interface.
+ *
+ * @author mmoquillon
+ */
+public interface BeanContainer {
+
+  public <T> T getBeanByName(String name);
+
+  public <T> T getBeanByType(Class<T> type);
+
+  public <T> Set<T> getAllBeansByType(Class<T> type);
+}
---
+++ lib-core/src/main/java/org/silverpeas/util/CDIContainer.java
@@ -0,0 +1,53 @@
+package org.silverpeas.util;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author mmoquillon
+ */
+public class CDIContainer implements BeanContainer {
+
+  @Override
+  public <T> T getBeanByName(final String name) {
+    BeanManager beanManager = CDI.current().getBeanManager();
+    Bean<T> bean = (Bean<T>) beanManager.getBeans(name).stream()
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("Cannot find an instance of name " + name));
+    CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
+    Type type = bean.getTypes().stream().findFirst().get();
+    T ref = (T) beanManager.getReference(bean, type, ctx);
+
+    return ref;
+  }
+
+  @Override
+  public <T> T getBeanByType(final Class<T> type) {
+    BeanManager beanManager = CDI.current().getBeanManager();
+    Bean<T> bean = (Bean<T>) beanManager.getBeans(type).stream()
+        .findFirst()
+        .orElseThrow(
+            () -> new IllegalStateException("Cannot find an instance of type " + type.getName()));
+    CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
+    T ref = (T) beanManager.getReference(bean, type, ctx);
+
+    return ref;
+  }
+
+  @Override
+  public <T> Set<T> getAllBeansByType(final Class<T> type) {
+    BeanManager beanManager = CDI.current().getBeanManager();
+    Set<T> refs = beanManager.getBeans(type).stream()
+        .map(bean -> {
+          CreationalContext ctx = beanManager.createCreationalContext(bean);
+          return (T) beanManager.getReference(bean, type, ctx);
+        })
+        .collect(Collectors.toSet());
+    return refs;
+  }
+}
