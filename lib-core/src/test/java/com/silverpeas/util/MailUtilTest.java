5cd25f5edc7eca0036e381e69863980e1fb13fef
Yohann Chastagnier <yohann.chastagnier@silverpeas.com>
2015-01-22 16:34:18 +0100

    Adding a centralized mechanism to improve the sending of mails.

--- lib-core/src/test/java/com/silverpeas/util/MailUtilTest.java
+++ lib-core/src/test/java/com/silverpeas/util/MailUtilTest.java
@@ -23,6 +23,7 @@ package com.silverpeas.util;
 
 import javax.mail.internet.InternetAddress;
 
+import org.junit.After;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -33,7 +34,9 @@ import static org.junit.Assert.assertThat;
  */
 public class MailUtilTest {
 
-  public MailUtilTest() {
+  @After
+  public void tearDown() {
+    MailUtil.reloadConfiguration(null);
   }
 
   /**
