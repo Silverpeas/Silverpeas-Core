8705a42cf195919eeaa89be2aa55101844d6cac8
ebonnet <etienne.bonnet@silverpeas.com>
2015-01-26 11:56:37 +0100

    update copyright on ejb-core:questioncontainer

--- ejb-core/questioncontainer/src/main/java/com/stratelia/webactiv/questionContainer/model/QuestionContainerDetail.java
+++ ejb-core/questioncontainer/src/main/java/com/stratelia/webactiv/questionContainer/model/QuestionContainerDetail.java
@@ -21,6 +21,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.stratelia.webactiv.questionContainer.model;
 
 import com.silverpeas.SilverpeasContent;
--- ejb-core/questioncontainer/src/main/java/com/stratelia/webactiv/score/control/ScoreService.java
+++ ejb-core/questioncontainer/src/main/java/com/stratelia/webactiv/score/control/ScoreService.java
@@ -21,6 +21,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.stratelia.webactiv.score.control;
 
 import com.stratelia.webactiv.score.model.ScoreDetail;
