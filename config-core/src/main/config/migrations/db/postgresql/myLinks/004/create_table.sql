b61338ed07bbbb2a1ba0aa3023cc930142fbf5fc
Yohann Chastagnier <yohann.chastagnier@silverpeas.com>
2015-03-04 14:31:23 +0100

    Migrating DB modifications made for features #6192 from 5.15.x to 6.0-SNAPSHOT. Fixing a mistake done during the migration of table st_user.

---
+++ config-core/src/main/config/migrations/db/postgresql/myLinks/004/create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE SB_MyLinks_Link
+(
+	linkId			int		NOT NULL,
+	name			varchar (255)	NOT NULL,
+	description		varchar (255)	NULL,
+	url			varchar (255)	NOT NULL,
+	visible			int		NOT NULL,
+	popup			int		NOT NULL,
+	userId			varchar (50)    NOT NULL,
+	instanceId		varchar (50)	NULL,
+	objectId		varchar	(50)	NULL,
+	position		int		NULL
+)
+;
