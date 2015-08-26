f49e71b79ad0e5fcb78f46e3f68d14515df96898
ebonnet <etienne.bonnet@silverpeas.com>
2015-08-26 10:33:43 +0200

    fixing bug #6681 cleaning guest session when user is connecting

--- lib-core/src/main/java/com/silverpeas/session/SessionInfo.java
+++ lib-core/src/main/java/com/silverpeas/session/SessionInfo.java
@@ -46,7 +46,7 @@ public class SessionInfo {
 
   private static SessionInfo getAnonymousSession() {
     UserDetail anonymousUser = UserDetail.getAnonymousUser();
-    if (UserDetail.getAnonymousUser() != null) {
+    if (anonymousUser != null) {
       return new SessionInfo(null, anonymousUser);
     }
     return NoneSession;
