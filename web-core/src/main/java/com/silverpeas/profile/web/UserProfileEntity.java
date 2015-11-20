da96fcc75afafb8c1531fef7936dd1124c548422
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2015-11-20 23:50:37 +0100

    bug #7118. Remove the HTML encoding of the first name and of the last name of users.

--- web-core/src/main/java/com/silverpeas/profile/web/UserProfileEntity.java
+++ web-core/src/main/java/com/silverpeas/profile/web/UserProfileEntity.java
@@ -154,13 +154,13 @@ public class UserProfileEntity extends UserDetail implements WebEntity {
   @Override
   @XmlElement(required = true)
   public String getFirstName() {
-    return Encode.forHtml(this.user.getFirstName());
+    return this.user.getFirstName();
   }
 
   @Override
   @XmlElement(required = true)
   public String getLastName() {
-    return Encode.forHtml(this.user.getLastName());
+    return this.user.getLastName();
   }
 
   @Override
