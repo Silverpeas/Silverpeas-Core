c8d884f0efdc0afe01cce12665de89eeca2d491c
svuillet <svu@silverpeas.com>
2019-01-23 18:29:16 +0100

    Feature #10406 : Personnalisation plugin identitycard

--- core-war/src/main/webapp/wysiwyg/jsp/ckeditor/plugins/identitycard/plugin.js
+++ core-war/src/main/webapp/wysiwyg/jsp/ckeditor/plugins/identitycard/plugin.js
@@ -1,5 +1,5 @@
 /**
- *    Copyright (C) 2000 - 2018 Silverpeas
+ *    Copyright (C) 2000 - 2019 Silverpeas
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Affero General Public License as
@@ -23,116 +23,112 @@
  */
 
 // Register the plugin within the editor.
-CKEDITOR.plugins.add( 'identitycard', {
-	// This plugin requires the Widgets System defined in the 'widget' plugin.
-	  requires: 'widget',
+CKEDITOR.plugins.add('identitycard', {
+  // This plugin requires the Widgets System defined in the 'widget' plugin.
+  requires : 'widget',
 
-	  // Register the icon used for the toolbar button. It must be the same
-	  // as the name of the widget.
-	  icons: 'identitycard',
+  // Register the icon used for the toolbar button. It must be the same
+  // as the name of the widget.
+  icons : 'identitycard',
 
-	  lang: 'en,fr',
+  lang : 'en,fr',
 
-	  // The plugin initialization logic goes inside this method.
-	  init: function( editor ) {
-	    // Register the editing dialog.
-	    CKEDITOR.dialog.add( 'identitycard', this.path + 'dialogs/identitycard.js' );
+  // The plugin initialization logic goes inside this method.
+  init : function(editor) {
 
-	    // Register the identitycard widget.
-	    editor.widgets.add( 'identitycard', {
+    $.get("/weblib/ckeditor/identitycard/template.html", function(customTemplate) {
 
-	      upcast: function( element ) {
-				return element.name == 'div' && element.hasClass( 'user-card' );
-	      },
+      // Register the editing dialog.
+      CKEDITOR.dialog.add('identitycard', this.path + 'dialogs/identitycard.js');
 
-	      // Minimum HTML which is required by this widget to work.
-	      requiredContent: 'div(user-card)',
+      // Register the identitycard widget.
+      editor.widgets.add('identitycard', {
 
-	      // Define the template of a new Identity Card widget.
-	      template:
-	        '<div class="user-card">' +
-					        '<div class="avatar"><img src="" alt="avatar"/></div>' +
-					        '<span class="field userToZoom">firstName lastName</span>' +
-					        '<span class="field eMail">eMail</span>' +
-					        '<span class="field title">title</span>' +
-					        '<span class="field phone">phone</span>' +
-					        '<span class="field cellularPhone">cellularPhone</span>' +
-					        '</div>',
+        upcast : function(element) {
+          return element.name == 'div' && element.hasClass('user-card');
+        },
 
-					      dialog: 'identitycard',
+        // Minimum HTML which is required by this widget to work.
+        requiredContent : 'div(user-card)',
 
-					      draggable: true,
+        // Define the template of a new Identity Card widget.
+        template : customTemplate,
 
-					      init: function() {
-					        var userId = this.element.getAttribute( 'rel' );
-					        this.setData( 'userId', userId );
-					        updateContent(this);
-					      },
+        dialog : 'identitycard',
 
-					      data: function() {
-					        updateContent(this);
-					      }}
-	    );
+        draggable : true,
 
-	    editor.ui.addButton( 'identitycard', {
-		      label : editor.lang.identitycard.identitycard,
-		      command : 'identitycard',
-	    });
-	  }
+        init : function() {
+          var userId = this.element.getAttribute('rel');
+          this.setData('userId', userId);
+          updateContent(this);
+        },
+
+        data : function() {
+          updateContent(this);
+        }
+      });
+
+      editor.ui.addButton('identitycard', {
+        label : editor.lang.identitycard.identitycard, command : 'identitycard',
+      });
+
+    });
+  }
 });
 
 function updateContent(widget) {
-	  var widgetUI = widget.element;
-	  var widgetData = widget.data;
-	  widgetUI.setAttribute('rel', widget.data.userId);
+  var widgetUI = widget.element;
+  var widgetData = widget.data;
+  widgetUI.setAttribute('rel', widget.data.userId);
 
-	  $.ajax({
-	    type : "GET",
-	    url : '/silverpeas/services/profile/users/' + $(widgetUI).attr('rel') + '?extended=true',
-	    dataType : "json",
-	    cache : false,
-	    success : function(user, status, jqXHR) {
-	      for (var i=0; i < widgetUI.getChildCount(); i++) {
-	        var item = widgetUI.getChildren().getItem(i);
-	        if (item.type == 1) {
-	          if (item.getAttribute('class') == 'field userToZoom') {
-	            item.setAttribute('rel', widgetData.userId);
-	            item.setHtml(user.firstName + ' ' + user.lastName);
-	          } else if (item.getAttribute('class') == 'avatar') {
-	            var avatar = item.getChildren().getItem(0);
-	            avatar.setAttribute('src', user.avatar);
-	          }
-	        }
-	      }
-	      jQuery.each(user, function(key, val) {
-	        for (var i=0; i < widgetUI.getChildCount(); i++) {
-	          var item = widgetUI.getChildren().getItem(i);
-	          if (item.type == 1) {
-	            if (key == 'moreData') {
-	              jQuery.each(val, function(keyMore, valMore) {
-	                for (var j=0; j < widgetUI.getChildCount(); j++) {
-	                  var itemMore = widgetUI.getChildren().getItem(j);
-	                  if (itemMore.type == 1) {
-	                    if (itemMore.getAttribute('class') == 'field ' + keyMore) {
-	                      itemMore.setHtml(valMore);
-	                      break;
-	                    }
-	                  }
-	                }
-	              });
-	            } else {
-	              if (item.getAttribute('class') == 'field ' + key) {
-	                item.setHtml(val);
-	                break;
-	              }
-	            }
-	          }
-	        }
-	      });
-	    },
+  $.ajax({
+    type : "GET",
+    url : '/silverpeas/services/profile/users/' + $(widgetUI).attr('rel') + '?extended=true',
+    dataType : "json",
+    cache : false,
+    success : function(user, status, jqXHR) {
+      for (var i = 0; i < widgetUI.getChildCount(); i++) {
+        var item = widgetUI.getChildren().getItem(i);
+        if (item.type == 1) {
+          if (item.getAttribute('class') == 'field userToZoom') {
+            item.setAttribute('rel', widgetData.userId);
+            item.setHtml(user.firstName + ' ' + user.lastName);
+          } else if (item.getAttribute('class') == 'avatar') {
+            var avatar = item.getChildren().getItem(0);
+            avatar.setAttribute('src', user.avatar);
+          }
+        }
+      }
+      jQuery.each(user, function(key, val) {
+        for (var i = 0; i < widgetUI.getChildCount(); i++) {
+          var item = widgetUI.getChildren().getItem(i);
+          if (item.type == 1) {
+            if (key == 'moreData') {
+              jQuery.each(val, function(keyMore, valMore) {
+                for (var j = 0; j < widgetUI.getChildCount(); j++) {
+                  var itemMore = widgetUI.getChildren().getItem(j);
+                  if (itemMore.type == 1) {
+                    if (itemMore.getAttribute('class') == 'field ' + keyMore) {
+                      itemMore.setHtml(valMore);
+                      break;
+                    }
+                  }
+                }
+              });
+            } else {
+              if (item.getAttribute('class') == 'field ' + key) {
+                item.setHtml(val);
+                break;
+              }
+            }
+          }
+        }
+      });
+    },
 
-	    error : function(jqXHR, status) {
-	      // do nothing
-	    }
-	  });
+    error : function(jqXHR, status) {
+      // do nothing
+    }
+  });
 }
