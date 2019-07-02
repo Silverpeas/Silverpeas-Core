d14915052b6f36649efa3927fe2546b4ea3ceea1
SilverDav <dle@silverpeas.com>
2019-07-02 17:45:36 +0200

    feature #10678 adding msproject and openproject support for online editing

--- lib-core/src/main/java/com/silverpeas/util/MimeTypes.java
+++ lib-core/src/main/java/com/silverpeas/util/MimeTypes.java
@@ -35,6 +35,10 @@ public interface MimeTypes {
   public final String EXCEL_MIME_TYPE2 = "application/vnd.ms-excel";
   public final String POWERPOINT_MIME_TYPE1 = "application/powerpoint";
   public final String POWERPOINT_MIME_TYPE2 = "application/vnd.ms-powerpoint";
+  public final String MSPROJECT_MIME_TYPE = "application/vnd.ms-project";
+
+  public final String OPENPROJECT_MIME_TYPE = "application/x-openproj";
+
   public final String SPINFIRE_MIME_TYPE = "application/xview3d-3d";
   public final String HTML_MIME_TYPE = "text/html";
   public final String XML_MIME_TYPE = "text/xml";
@@ -92,14 +96,14 @@ public interface MimeTypes {
   public static final Set<String> MS_OFFICE_MIME_TYPES = new HashSet<String>(Arrays
       .asList(
           new String[]{WORD_MIME_TYPE, EXCEL_MIME_TYPE1, EXCEL_MIME_TYPE2, POWERPOINT_MIME_TYPE1,
-            POWERPOINT_MIME_TYPE2}));
+            POWERPOINT_MIME_TYPE2, MSPROJECT_MIME_TYPE}));
   public static final Set<String> OPEN_OFFICE_MIME_TYPES = new HashSet<String>(Arrays.asList(
       new String[]{WORD_MIME_TYPE, WORD_2007_MIME_TYPE, WORD_2007_TEMPLATE_MIME_TYPE,
         EXCEL_MIME_TYPE1, EXCEL_MIME_TYPE2, EXCEL_2007_MIME_TYPE, EXCEL_2007_TEMPLATE_MIME_TYPE,
         POWERPOINT_MIME_TYPE1, POWERPOINT_MIME_TYPE2, POWERPOINT_2007_MIME_TYPE,
         POWERPOINT_2007_TEMPLATE_MIME_TYPE, MIME_TYPE_OO_FORMATTED_TEXT, MIME_TYPE_OO_SPREADSHEET,
         MIME_TYPE_OO_PRESENTATION, MIME_TYPE_OO_GRAPHICS, MIME_TYPE_OO_DIAGRAM, MIME_TYPE_OO_FORMULA,
-        MIME_TYPE_OO_DB, MIME_TYPE_OO_IMAGE, MIME_TYPE_OO_MASTER}));
+        MIME_TYPE_OO_DB, MIME_TYPE_OO_IMAGE, MIME_TYPE_OO_MASTER, OPENPROJECT_MIME_TYPE}));
   public static final Set<String> ARCHIVE_MIME_TYPES = new HashSet<String>(Arrays.asList(
       new String[]{BZ2_ARCHIVE_MIME_TYPE, GZ_ARCHIVE_MIME_TYPE, GUNZIP_ARCHIVE_MIME_TYPE,
         TARGZ_ARCHIVE_MIME_TYPE, ARCHIVE_MIME_TYPE, SHORT_ARCHIVE_MIME_TYPE, JAVA_ARCHIVE_MIME_TYPE}));
