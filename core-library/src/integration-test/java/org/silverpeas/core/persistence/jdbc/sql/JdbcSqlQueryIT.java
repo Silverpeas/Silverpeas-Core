6bebc2d6be398d998d91e5de8b79cab2a46cc6b0
Miguel Moquillon <miguel.moquillon@gmail.com>
2020-01-09 09:29:53 +0100

    SqlExecutor and its implementation means to be used only by the SqlQuery: it is now only package-visible

--- core-library/src/integration-test/java/org/silverpeas/core/persistence/jdbc/sql/JdbcSqlQueryIT.java
+++ core-library/src/integration-test/java/org/silverpeas/core/persistence/jdbc/sql/JdbcSqlQueryIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000 - 2019 Silverpeas
+ * Copyright (C) 2000 - 2020 Silverpeas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -9,7 +9,7 @@
  * As a special exception to the terms and conditions of version 3.0 of
  * the GPL, you may redistribute this Program in connection with Free/Libre
  * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
+ * FLOSS exception. You should have received a copy of the text describing
  * the FLOSS exception, and it is also available here:
  * "https://www.silverpeas.org/legal/floss_exception.html"
  *
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.silverpeas.core.persistence.jdbc;
+package org.silverpeas.core.persistence.jdbc.sql;
 
 import com.ninja_squad.dbsetup.Operations;
 import com.ninja_squad.dbsetup.operation.Insert;
@@ -34,10 +34,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.silverpeas.core.persistence.Transaction;
-import org.silverpeas.core.persistence.jdbc.sql.JdbcSqlExecutorProvider;
-import org.silverpeas.core.persistence.jdbc.sql.JdbcSqlQuery;
-import org.silverpeas.core.persistence.jdbc.sql.ResultSetWrapper;
-import org.silverpeas.core.persistence.jdbc.sql.SelectResultRowProcess;
 import org.silverpeas.core.test.WarBuilder4LibCore;
 import org.silverpeas.core.test.rule.DbSetupRule;
 
