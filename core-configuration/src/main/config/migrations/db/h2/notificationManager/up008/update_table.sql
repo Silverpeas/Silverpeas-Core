56bf49f42d92b562ba8334c425c744b0d1d0804e
SilverDav <dle@silverpeas.com>
2020-03-11 18:07:17 +0100

    Feature #11387 update message column length in one request

--- core-configuration/src/main/config/migrations/db/h2/notificationManager/up008/update_table.sql
+++ core-configuration/src/main/config/migrations/db/h2/notificationManager/up008/update_table.sql
@@ -1,3 +1,1 @@
-ALTER TABLE st_delayednotification ADD COLUMN message2 character varying(10000);
-ALTER TABLE st_delayednotification DROP COLUMN message;
-ALTER TABLE st_delayednotification RENAME COLUMN message2 to message;
+ALTER TABLE st_delayednotification ALTER COLUMN message VARCHAR(10000);
