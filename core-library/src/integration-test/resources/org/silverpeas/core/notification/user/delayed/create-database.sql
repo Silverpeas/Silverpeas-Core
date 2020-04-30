192655ecf13e14901f1f2e113115f54c980a5b99
SilverYoCha <yohann.chastagnier@silverpeas.com>
2020-04-30 14:19:23 +0200

    Bug #11462: making the variable $notification_serverurl$ well filled into context of a synthesis notification.

--- core-library/src/integration-test/resources/org/silverpeas/core/notification/user/delayed/create-database.sql
+++ core-library/src/integration-test/resources/org/silverpeas/core/notification/user/delayed/create-database.sql
@@ -4,6 +4,44 @@ CREATE TABLE IF NOT EXISTS uniqueId (
 	tableName varchar(100) NOT NULL
 );
 
+CREATE TABLE ST_Domain
+(
+    id                   int                        NOT NULL,
+    name                 varchar(100)               NOT NULL,
+    description          varchar(400)               NULL,
+    propFileName         varchar(100)               NOT NULL,
+    className            varchar(100)               NOT NULL,
+    authenticationServer varchar(100)               NOT NULL,
+    theTimeStamp         varchar(100) DEFAULT ('0') NOT NULL,
+    silverpeasServerURL  varchar(400)               NULL
+);
+
+CREATE TABLE ST_User
+(
+    id                            INT                  NOT NULL,
+    domainId                      INT                  NOT NULL,
+    specificId                    VARCHAR(500)         NOT NULL,
+    firstName                     VARCHAR(100),
+    lastName                      VARCHAR(100)         NOT NULL,
+    email                         VARCHAR(100),
+    login                         VARCHAR(50)          NOT NULL,
+    loginMail                     VARCHAR(100),
+    accessLevel                   CHAR(1) DEFAULT 'U'  NOT NULL,
+    loginquestion                 VARCHAR(200),
+    loginanswer                   VARCHAR(200),
+    creationDate                  TIMESTAMP,
+    saveDate                      TIMESTAMP,
+    version                       INT DEFAULT 0        NOT NULL,
+    tosAcceptanceDate             TIMESTAMP,
+    lastLoginDate                 TIMESTAMP,
+    nbSuccessfulLoginAttempts     INT DEFAULT 0        NOT NULL,
+    lastLoginCredentialUpdateDate TIMESTAMP,
+    expirationDate                TIMESTAMP,
+    state                         VARCHAR(30)          NOT NULL,
+    stateSaveDate                 TIMESTAMP            NOT NULL,
+    notifManualReceiverLimit      INT
+);
+
 CREATE TABLE st_delayednotifusersetting (
    id 			int NOT NULL ,
    userId		int NOT NULL ,
