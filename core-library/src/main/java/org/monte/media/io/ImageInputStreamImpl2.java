05c633300fcc0de71650c8b1a3cb6223e2a30bea
Miguel Moquillon <miguel.moquillon@gmail.com>
2020-06-05 12:16:58 +0200

    Feature #11551
    Take into account of Silverpeas-Project 1.3-SNAPSHOT that define that the
    any child projects will be on Java 11.
    The code org.monte.media.* have been updated to be compilant to Java 11.

--- core-library/src/main/java/org/monte/media/io/ImageInputStreamImpl2.java
+++ core-library/src/main/java/org/monte/media/io/ImageInputStreamImpl2.java
@@ -1,25 +1,7 @@
-/*
- * Copyright (C) 2000 - 2020 Silverpeas
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * As a special exception to the terms and conditions of version 3.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * "https://www.silverpeas.org/legal/floss_exception.html"
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/* @(#)ImageInputStreamImpl2.java 
+ * 
+ * Copyright (c) 2011 Werner Randelshofer, Switzerland. 
+ * You may only use this file in compliance with the accompanying license terms.
  */
 package org.monte.media.io;
 
@@ -28,45 +10,51 @@ import java.nio.ByteOrder;
 import javax.imageio.stream.ImageInputStreamImpl;
 
 /**
- * {@code ImageInputStreamImpl2} fixes bugs in ImageInputStreamImpl. <p> ImageInputStreamImpl uses
- * read(byte[]) instead of readFully(byte[]) inside of readShort. This results in corrupt data input
- * if the underlying stream can not fulfill the read operation in a single step.
+ * {@code ImageInputStreamImpl2} fixes bugs in ImageInputStreamImpl.
+ * <p>
+ * ImageInputStreamImpl uses read(byte[]) instead of readFully(byte[]) inside of
+ * readShort. This results in corrupt data input if the underlying stream can
+ * not fulfill the read operation in a single step.
  *
  * @author Werner Randelshofer
- * @version $Id: ImageInputStreamImpl2.java 134 2011-12-02 16:23:00Z werner $
+ * @version $Id: ImageInputStreamImpl2.java 348 2015-09-23 17:46:43Z werner $
  */
 public abstract class ImageInputStreamImpl2 extends ImageInputStreamImpl {
-  // Length of the buffer used for readFully(type[], int, int)
+    // Length of the buffer used for readFully(type[], int, int)
+    private static final int BYTE_BUF_LENGTH = 8192;
+    /**
+     * Byte buffer used for readFully(type[], int, int).  Note that this
+     * array is also used for bulk reads in readShort(), readInt(), etc, so
+     * it should be large enough to hold a primitive value (i.e. >= 8 bytes).
+     * Also note that this array is package protected, so that it can be
+     * used by ImageOutputStreamImpl in a similar manner.
+     */
+    protected byte[] byteBuf = new byte[BYTE_BUF_LENGTH];
 
-  private static final int BYTE_BUF_LENGTH = 8192;
-  /**
-   * Byte buffer used for readFully(type[], int, int). Note that this array is also used for bulk
-   * reads in readShort(), readInt(), etc, so it should be large enough to hold a primitive value
-   * (i.e. >= 8 bytes). Also note that this array is package protected, so that it can be used by
-   * ImageOutputStreamImpl in a similar manner.
-   */
-  byte[] byteBuf = new byte[BYTE_BUF_LENGTH];
+    @Override
+        public short readShort() throws IOException {
+        readFully(byteBuf, 0, 2);
 
-  @Override
-  public short readShort() throws IOException {
-    readFully(byteBuf, 0, 2);
-
-    if (byteOrder == ByteOrder.BIG_ENDIAN) {
-      return (short) (((byteBuf[0] & 0xff) << 8) | ((byteBuf[1] & 0xff) << 0));
-    } else {
-      return (short) (((byteBuf[1] & 0xff) << 8) | ((byteBuf[0] & 0xff) << 0));
+        if (byteOrder == ByteOrder.BIG_ENDIAN) {
+            return (short)
+                (((byteBuf[0] & 0xff) << 8) | ((byteBuf[1] & 0xff) << 0));
+        } else {
+            return (short)
+                (((byteBuf[1] & 0xff) << 8) | ((byteBuf[0] & 0xff) << 0));
+        }
     }
-  }
+    public int readInt() throws IOException {
+        readFully(byteBuf, 0, 4);
 
-  public int readInt() throws IOException {
-    readFully(byteBuf, 0, 4);
-
-    if (byteOrder == ByteOrder.BIG_ENDIAN) {
-      return (((byteBuf[0] & 0xff) << 24) | ((byteBuf[1] & 0xff) << 16)
-        | ((byteBuf[2] & 0xff) << 8) | ((byteBuf[3] & 0xff) << 0));
-    } else {
-      return (((byteBuf[3] & 0xff) << 24) | ((byteBuf[2] & 0xff) << 16)
-        | ((byteBuf[1] & 0xff) << 8) | ((byteBuf[0] & 0xff) << 0));
+        if (byteOrder == ByteOrder.BIG_ENDIAN) {
+            return
+                (((byteBuf[0] & 0xff) << 24) | ((byteBuf[1] & 0xff) << 16) |
+                 ((byteBuf[2] & 0xff) <<  8) | ((byteBuf[3] & 0xff) <<  0));
+        } else {
+            return
+                (((byteBuf[3] & 0xff) << 24) | ((byteBuf[2] & 0xff) << 16) |
+                 ((byteBuf[1] & 0xff) <<  8) | ((byteBuf[0] & 0xff) <<  0));
+        }
     }
-  }
+
 }
--- core-library/src/main/java/org/monte/media/jpeg/CMYKJPEGImageReaderSpi.java
+++ core-library/src/main/java/org/monte/media/jpeg/CMYKJPEGImageReaderSpi.java
@@ -1,63 +1,47 @@
-/*
- * Copyright (C) 2000 - 2020 Silverpeas
+/* @(#)CMYKJPEGImageReaderSpi.java  1.2  2011-02-17
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * As a special exception to the terms and conditions of version 3.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have received a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * "https://www.silverpeas.org/legal/floss_exception.html"
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (c) 2010-2011 Werner Randelshofer, Switzerland.
+ * You may only use this file in compliance with the accompanying license terms.
  */
 package org.monte.media.jpeg;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Locale;
 import javax.imageio.ImageReader;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.stream.ImageInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
 
 /**
  * A reader for JPEG images in the CMYK color space.
  *
  * @author Werner Randelshofer
- * @version 1.2 2011-02-17 Removes support for JMF. <br>1.0 2010-07-23 Created.
+ * @version 1.2 2011-02-17 Removes support for JMF.
+ * <br>1.0 2010-07-23 Created.
  */
 public class CMYKJPEGImageReaderSpi extends ImageReaderSpi {
 
   public CMYKJPEGImageReaderSpi() {
     super("Werner Randelshofer",//vendor name
-      "1.0",//version
-      new String[]{"JPEG", "JPG"},//names
-      new String[]{"jpg"},//suffixes,
-      new String[]{"image/jpg"},// MIMETypes,
-      "org.monte.media.jpeg.CMYKJPEGImageReader",// readerClassName,
-      new Class[]{ImageInputStream.class, InputStream.class, byte[].class},// inputTypes,
-      null,// writerSpiNames,
-      false,// supportsStandardStreamMetadataFormat,
-      null,// nativeStreamMetadataFormatName,
-      null,// nativeStreamMetadataFormatClassName,
-      null,// extraStreamMetadataFormatNames,
-      null,// extraStreamMetadataFormatClassNames,
-      false,// supportsStandardImageMetadataFormat,
-      null,// nativeImageMetadataFormatName,
-      null,// nativeImageMetadataFormatClassName,
-      null,// extraImageMetadataFormatNames,
-      null// extraImageMetadataFormatClassNames
-      );
+        "1.0",//version
+        new String[]{"JPEG","JPG"},//names
+        new String[]{"jpg"},//suffixes,
+        new String[]{"image/jpg"},// MIMETypes,
+        "org.monte.media.jpeg.CMYKJPEGImageReader",// readerClassName,
+        new Class<?>[]{ImageInputStream.class,InputStream.class,byte[].class},// inputTypes,
+        null,// writerSpiNames,
+        false,// supportsStandardStreamMetadataFormat,
+        null,// nativeStreamMetadataFormatName,
+        null,// nativeStreamMetadataFormatClassName,
+        null,// extraStreamMetadataFormatNames,
+        null,// extraStreamMetadataFormatClassNames,
+        false,// supportsStandardImageMetadataFormat,
+        null,// nativeImageMetadataFormatName,
+        null,// nativeImageMetadataFormatClassName,
+        null,// extraImageMetadataFormatNames,
+        null// extraImageMetadataFormatClassNames
+    );
   }
 
   @Override
