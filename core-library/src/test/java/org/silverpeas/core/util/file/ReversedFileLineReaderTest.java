b42dc699ac4380cb2ac28f551742113ae19f0fe4
Miguel Moquillon <miguel.moquillon@gmail.com>
2021-04-29 18:08:03 +0200

    Fix unit test ReversedFileLineReaderTest

--- core-library/src/test/java/org/silverpeas/core/util/file/ReversedFileLineReaderTest.java
+++ core-library/src/test/java/org/silverpeas/core/util/file/ReversedFileLineReaderTest.java
@@ -23,10 +23,8 @@
  */
 package org.silverpeas.core.util.file;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.silverpeas.core.test.UnitTest;
 import org.silverpeas.core.util.Charsets;
 import org.silverpeas.core.util.StringUtil;
@@ -37,8 +35,8 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -49,33 +47,24 @@ import static org.hamcrest.Matchers.*;
  * @author Yohann Chastagnier
  */
 @UnitTest
-public class ReversedFileLineReaderTest {
-
-  @Rule
-  public TemporaryFolder temporaryFolder = new TemporaryFolder();
-
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
+class ReversedFileLineReaderTest {
 
   @Test
-  public void readFromNotExistingFile() throws Exception {
-    Path notExistingFile = Paths.get(temporaryFolder.newFolder().getPath(), "file.log");
-
-    thrown.expect(IOException.class);
-    thrown.expectMessage(startsWith(String.format("%1$s (", notExistingFile.toFile().getPath())));
-
-    ReversedFileLineReader.readLastLines(notExistingFile, 10);
+  void readFromNotExistingFile() throws Exception {
+    Path notExistingFile = Files.createTempDirectory("toto").resolve("file.log");
+    Assertions.assertThrows(IOException.class,
+        () -> ReversedFileLineReader.readLastLines(notExistingFile, 10));
   }
 
   @Test
-  public void readLinesWithAccentInContent() throws Exception {
+  void readLinesWithAccentInContent() throws Exception {
     Path fileWithAccentInContent = createFileWithContent("éùï" + System.lineSeparator() + "ô\n\n");
     List<String> lastLines = ReversedFileLineReader.readLastLines(fileWithAccentInContent, 0);
     assertThat(lastLines, contains("éùï", "ô", ""));
   }
 
   @Test
-  public void readLinesWithAccentInContentWithEncodingConflict() throws Exception {
+  void readLinesWithAccentInContentWithEncodingConflict() throws Exception {
     String utf8String = "éùï" + System.lineSeparator() + "ô\n\n";
     String usAsciiString = new String(utf8String.getBytes(Charsets.US_ASCII));
     assertThat(utf8String, not(is(usAsciiString)));
@@ -86,42 +75,42 @@ public class ReversedFileLineReaderTest {
   }
 
   @Test
-  public void read10LinesFromEmptyFile() throws Exception {
+  void read10LinesFromEmptyFile() throws Exception {
     Path emptyFile = createFileOfSpecifiedLines(0);
     List<String> lastLines = ReversedFileLineReader.readLastLines(emptyFile, 10);
     assertThat(lastLines, empty());
   }
 
   @Test
-  public void read10LinesFromFileContaining1Line() throws Exception {
+  void read10LinesFromFileContaining1Line() throws Exception {
     Path emptyFile = createFileOfSpecifiedLines(1);
     List<String> lastLines = ReversedFileLineReader.readLastLines(emptyFile, 10);
     assertThat(lastLines, contains("1"));
   }
 
   @Test
-  public void readAllLinesFromFileContaining10Lines() throws Exception {
+  void readAllLinesFromFileContaining10Lines() throws Exception {
     Path emptyFile = createFileOfSpecifiedLines(10);
     List<String> lastLines = ReversedFileLineReader.readLastLines(emptyFile, 0);
     assertThat(lastLines, contains("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
   }
 
   @Test
-  public void read5LinesFromFileContaining10LinesFromOldestToMostRecent() throws Exception {
+  void read5LinesFromFileContaining10LinesFromOldestToMostRecent() throws Exception {
     Path emptyFile = createFileOfSpecifiedLines(10);
     List<String> lastLines = ReversedFileLineReader.readLastLines(emptyFile, 5, false);
     assertThat(lastLines, contains("10", "9", "8", "7", "6"));
   }
 
   @Test
-  public void read5LinesFromFileContaining10LinesFromMostRecentToOldest() throws Exception {
+  void read5LinesFromFileContaining10LinesFromMostRecentToOldest() throws Exception {
     Path emptyFile = createFileOfSpecifiedLines(10);
     List<String> lastLines = ReversedFileLineReader.readLastLines(emptyFile, 5);
     assertThat(lastLines, contains("6", "7", "8", "9", "10"));
   }
 
   @Test
-  public void readAllLinesFromHugeFileContent() throws Exception {
+  void readAllLinesFromHugeFileContent() throws Exception {
     Path hugeFile = createFileOfSpecifiedLines(ReversedFileLineReader.FULL_FILE_NB_LINE_LIMIT * 2);
     for (int nbLastLines : new int[]{0, -1, (ReversedFileLineReader.FULL_FILE_NB_LINE_LIMIT * 2)}) {
       List<String> lastLines = ReversedFileLineReader.readLastLines(hugeFile, nbLastLines);
@@ -132,7 +121,7 @@ public class ReversedFileLineReaderTest {
   }
 
   private Path createFileOfSpecifiedLines(int nbLines) throws Exception {
-    Path path = Paths.get(temporaryFolder.newFile().toURI());
+    Path path = Files.createTempFile("test-" + System.currentTimeMillis(), "txt");
     if (nbLines > 0) {
       try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(path.toFile()))) {
         for (int i = 0; i < nbLines; i++) {
@@ -149,7 +138,7 @@ public class ReversedFileLineReaderTest {
   }
 
   private Path createFileWithContent(String content, Charset charset) throws Exception {
-    Path path = Paths.get(temporaryFolder.newFile().toURI());
+    Path path = Files.createTempFile("test-" + System.currentTimeMillis(), "txt");
     if (StringUtil.isDefined(content)) {
       try (BufferedOutputStream fileWriter = new BufferedOutputStream(
           new FileOutputStream(path.toFile()))) {
