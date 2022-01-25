3df54af449af1ef89524681fbc7112892ea9594b
NicolasEysseric <nicolas.eysseric@silverpeas.com>
2022-01-25 11:51:18 +0100

    fixing bug #12841

--- core-services/questioncontainer/src/main/java/org/silverpeas/core/questioncontainer/result/model/Results.java
+++ core-services/questioncontainer/src/main/java/org/silverpeas/core/questioncontainer/result/model/Results.java
@@ -2,22 +2,25 @@ package org.silverpeas.core.questioncontainer.result.model;
 
 import org.silverpeas.core.admin.user.model.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Results {
 
-  private Map<String, QuestionResult> questionsResults = new HashMap<>();
+  private Map<String, List<QuestionResult>> questionsResults = new HashMap<>();
   private Map<String, User> users = new HashMap<>();
 
   public void addQuestionResult(QuestionResult qr) {
     String key = qr.getQuestionPK().getId()+"-"+qr.getUserId()+"-"+qr.getParticipationId();
-    questionsResults.put(key, qr);
+    List<QuestionResult> results = questionsResults.computeIfAbsent(key, k -> new ArrayList<>());
+    results.add(qr);
     String userId = qr.getUserId();
     users.computeIfAbsent(userId, User::getById);
   }
 
-  public QuestionResult getQuestionResultByQuestion(String questionId, String userId,
+  public List<QuestionResult> getQuestionResultByQuestion(String questionId, String userId,
       int participationId) {
     String key = questionId+"-"+userId+"-"+participationId;
     return questionsResults.get(key);
