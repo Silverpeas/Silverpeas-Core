82226f10b3e4ea871b47525a83bc315d26a0d3e6
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2022-09-02 10:04:15 +0200

    Some resources created in very old Silverpeas versions have no creation date. This peculiar case is now taken into account by the CmisObjectFactory class.

--- core-library/src/main/java/org/silverpeas/core/cmis/model/CmisObjectFactory.java
+++ core-library/src/main/java/org/silverpeas/core/cmis/model/CmisObjectFactory.java
@@ -48,16 +48,16 @@ import java.util.List;
 
 /**
  * A factory of objects in the CMIS model by using as source object a Silverpeas resource. It is
- * responsible to map a Silverpeas resource (spaces, applications, nodes, contributions, ...) to
- * a CMIS object.
+ * responsible to map a Silverpeas resource (spaces, applications, nodes, contributions, ...) to a
+ * CMIS object.
  * @author mmoquillon
  */
 @Provider
 public class CmisObjectFactory {
 
   /**
-   * Gets an instance of the {@link CmisObjectFactory} class. This method invokes the underlying
-   * IoC system to get such an instance.
+   * Gets an instance of the {@link CmisObjectFactory} class. This method invokes the underlying IoC
+   * system to get such an instance.
    * @return a {@link CmisObjectFactory} instance.
    */
   public static CmisObjectFactory getInstance() {
@@ -99,11 +99,12 @@ public class CmisObjectFactory {
       fatherId = (space.getFatherId().startsWith(idPrefix) ? "" : idPrefix) + space.getFatherId();
     }
     final User creator = space.getCreator();
+    final long creationDate = inMilliseconds(space.getCreationDate(), space.getLastUpdateDate());
     final User lastModifier;
     final long lastModificationDate;
     if (space.getUpdatedBy() < 0) {
       lastModifier = creator;
-      lastModificationDate = space.getCreationDate().getTime();
+      lastModificationDate = creationDate;
     } else {
       lastModifier = space.getLastUpdater();
       lastModificationDate = space.getLastUpdateDate().getTime();
@@ -112,8 +113,7 @@ public class CmisObjectFactory {
         .setParentId(fatherId)
         .setDescription(space.getDescription(language))
         .setCreator(creator.getDisplayedName())
-        .setCreationDate(space.getCreationDate()
-            .getTime())
+        .setCreationDate(creationDate)
         .setLastModifier(lastModifier.getDisplayedName())
         .setLastModificationDate(lastModificationDate)
         .setAcesSupplier(this::theCommonsACE);
@@ -127,23 +127,22 @@ public class CmisObjectFactory {
    */
   public Application createApplication(final ComponentInstLight component, final String language) {
     final User creator = User.getById(String.valueOf(component.getCreatedBy()));
+    final long creationDate =
+        inMilliseconds(component.getCreationDate(), component.getLastUpdateDate());
     final User lastModifier;
     final long lastModificationDate;
     if (component.getUpdatedBy() < 0) {
       lastModifier = creator;
-      lastModificationDate = component.getCreationDate()
-          .getTime();
+      lastModificationDate = creationDate;
     } else {
       lastModifier = User.getById(String.valueOf(component.getUpdatedBy()));
-      lastModificationDate = component.getLastUpdateDate()
-          .getTime();
+      lastModificationDate = component.getLastUpdateDate().getTime();
     }
     return new Application(component.getIdentifier(), component.getName(language), language)
         .setParentId(component.getSpaceId())
         .setDescription(component.getDescription(language))
         .setCreator(creator.getDisplayedName())
-        .setCreationDate(component.getCreationDate()
-            .getTime())
+        .setCreationDate(creationDate)
         .setLastModifier(lastModifier.getDisplayedName())
         .setLastModificationDate(lastModificationDate)
         .setAcesSupplier(this::theCommonsACE);
@@ -152,8 +151,8 @@ public class CmisObjectFactory {
   /**
    * Creates a CMIS representation of the specified node descriptor in a given Silverpeas
    * application. A node is a technical object used in Silverpeas to categorize or organize in a
-   * generic way any user contributions. Because a node can also contain others nodes, the nodes
-   * can be linked to each of them within a tree of nodes (hence the name {@code node}).
+   * generic way any user contributions. Because a node can also contain others nodes, the nodes can
+   * be linked to each of them within a tree of nodes (hence the name {@code node}).
    * @param node a descriptor about a node.
    * @param language the language to use in the localization of the CMIS object to construct.
    * @return a CMIS contribution folder as a CMIS folder.
@@ -169,7 +168,7 @@ public class CmisObjectFactory {
         .setParentId(parentId)
         .setDescription(node.getDescription(language))
         .setCreator(node.getCreator().getDisplayedName())
-        .setCreationDate(node.getCreationDate().getTime())
+        .setCreationDate(inMilliseconds(node.getCreationDate(), node.getLastUpdateDate()))
         .setLastModifier(node.getLastUpdater().getDisplayedName())
         .setLastModificationDate(node.getLastUpdateDate().getTime())
         .setAcesSupplier(this::theCommonsACE);
@@ -177,9 +176,9 @@ public class CmisObjectFactory {
 
   /**
    * Creates a CMIS representation of the specified publication in a given folder of a Silverpeas
-   * application. A publication in our CMIS implementation is always contained into a folder. If
-   * the application in Silverpeas doesn't organize them in folders, then the folder here should be
-   * a virtual one (a root folder).
+   * application. A publication in our CMIS implementation is always contained into a folder. If the
+   * application in Silverpeas doesn't organize them in folders, then the folder here should be a
+   * virtual one (a root folder).
    * @param pub detail about the publication.
    * @param folder the folder in which is contained this publication.
    * @param language the language to use in the localization of the CMIS object to construct.
@@ -189,11 +188,12 @@ public class CmisObjectFactory {
       final ContributionIdentifier folder, final String language) {
     String parentId = folder.getLocalId().equals(NodePK.ROOT_NODE_ID) ?
         folder.getComponentInstanceId() : folder.asString();
+    //noinspection removal
     return new Publication(pub.getIdentifier(), pub.getName(language), language)
         .setParentId(parentId)
         .setDescription(pub.getDescription(language))
         .setCreator(pub.getCreator().getDisplayedName())
-        .setCreationDate(pub.getCreationDate().getTime())
+        .setCreationDate(inMilliseconds(pub.getCreationDate(), pub.getLastUpdateDate()))
         .setLastModifier(pub.getLastUpdater().getDisplayedName())
         .setLastModificationDate(pub.getLastUpdateDate().getTime())
         .setAcesSupplier(this::theCommonsACE);
@@ -218,7 +218,8 @@ public class CmisObjectFactory {
       updater = document.getLastUpdater();
     }
     boolean readOnly = document.isReadOnly() || (document.isVersioned() && !document.isEdited());
-    return new DocumentFile(document.getIdentifier(), document.getFilename(), document.getLanguage())
+    return new DocumentFile(document.getIdentifier(), document.getFilename(),
+        document.getLanguage())
         .setTitle(document.getTitle())
         .setLastComment(document.getComment())
         .setMimeType(document.getContentType())
@@ -227,7 +228,7 @@ public class CmisObjectFactory {
         .setParentId(parentId.asString())
         .setDescription(document.getDescription())
         .setCreator(document.getCreator().getDisplayedName())
-        .setCreationDate(document.getCreationDate().getTime())
+        .setCreationDate(inMilliseconds(document.getCreationDate(), document.getLastUpdateDate()))
         .setLastModifier(updater.getDisplayedName())
         .setLastModificationDate(updateDate.getTime())
         .setAcesSupplier(this::theCommonsACE);
@@ -240,6 +241,28 @@ public class CmisObjectFactory {
     entry.getPermissions().add(BasicPermissions.READ);
     entry.setDirect(true);
     return Collections.singletonList(entry);
+  }
+
+  /**
+   * Converts in milliseconds the specified date by taking into account it can be null. If null, the
+   * given default date will be taken. if the default date is also null, then the creation date or
+   * the last state change date of the platform administrator is taken. This method is for resources
+   * having no creation date which shouldn't be occurred unless the resource had been created with a
+   * very old version of Silverpeas.
+   * @param date a date to convert in milliseconds.
+   * @param defaultDate a default date to use if the date above is null. If null, the default date
+   * will be either the creation date or the last state change date of the Silverpeas
+   * administrator.
+   * @return the number of milliseconds from EPOCH represented by the specified date or default date
+   */
+  private long inMilliseconds(final Date date, final Date defaultDate) {
+    Date dateToUse = date == null ? defaultDate : date;
+    if (dateToUse == null) {
+      User admin = User.getMainAdministrator();
+      dateToUse =
+          admin.getCreationDate() != null ? admin.getCreationDate() : admin.getStateSaveDate();
+    }
+    return dateToUse.getTime();
   }
 
 }
