db2bafc49aaa4fec235ddd59ca3cf547e4c441e6
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2023-05-03 10:47:18 +0200

    Fix the SQL statement for Oracle in the migration node/up006

--- core-configuration/src/main/config/migrations/db/oracle/node/up006/deleteOrphansAndDuplicates.sql
+++ core-configuration/src/main/config/migrations/db/oracle/node/up006/deleteOrphansAndDuplicates.sql
@@ -1,14 +1,8 @@
 /* Delete orphans nodesi18n (does not exist any more in sb_node_node)... */
-DELETE FROM sb_node_nodei18n
-WHERE nodeId NOT IN (SELECT nodeId from SB_NODE_NODE);
+DELETE FROM sb_node_nodei18n WHERE nodeId NOT IN (SELECT nodeId from SB_NODE_NODE);
 
 /* Delete duplicates nodesi18n. */
-DELETE sb_node_nodei18n FROM sb_node_nodei18n
-LEFT OUTER JOIN (
-SELECT MAX(id) as id, nodeId, lang
-FROM sb_node_nodei18n
-GROUP BY nodeId, lang
-) as t1
-ON sb_node_nodei18n.id = t1.id
-WHERE t1.id IS NULL;
+DELETE FROM sb_node_nodei18n a
+WHERE a.id != (
+    SELECT max(id) FROM sb_node_nodei18n b WHERE a.nodeid = b.nodeid AND a.lang = b.lang);
 
