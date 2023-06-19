741be9927c5de617e1b3806ef64e423ce0923ec1
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2023-06-19 16:44:01 +0200

    Add a new annotation to perform automatic benchmarking on the methods call of the objects on which the annotation is applied

---
+++ core-library/src/main/java/org/silverpeas/core/util/annotation/TimeMeasuring.java
@@ -0,0 +1,21 @@
+package org.silverpeas.core.util.annotation;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@InterceptorBinding
+@Documented
+@Target({TYPE, METHOD})
+@Retention(RUNTIME)
+@Inherited
+public @interface TimeMeasuring {
+
+
+}
---
+++ core-library/src/main/java/org/silverpeas/core/util/annotation/TimeMeasuringProcessor.java
@@ -0,0 +1,53 @@
+package org.silverpeas.core.util.annotation;
+
+import org.silverpeas.core.util.logging.SilverLogger;
+
+import javax.annotation.Priority;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.lang.reflect.Method;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Interceptor
+@TimeMeasuring
+@Priority(Interceptor.Priority.APPLICATION)
+public class TimeMeasuringProcessor {
+
+    private static final String LOG_NAMESPACE = "silverpeas.core.benchmark";
+    private static final String PROXY_PREFIX = "$Proxy";
+
+    @AroundInvoke
+    public Object processAuthentication(InvocationContext context) throws Exception {
+        long start = System.nanoTime();
+        Object result = context.proceed();
+        long end = System.nanoTime();
+
+        String target = getObjectClassName(context.getTarget());
+        String method = getMethodName(context.getMethod());
+        String parameters = Stream.of(context.getParameters())
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+
+        SilverLogger.getLogger(LOG_NAMESPACE).info("MEASURED TIME OF {0}#{1}({2}): {3}ms", target, method, parameters,
+                (end - start) / 1000000);
+        return result;
+    }
+
+    private String getObjectClassName(Object object) {
+        String className = object.getClass().getSimpleName();
+        if (className.contains(PROXY_PREFIX)) {
+            return className.substring(0, className.indexOf(PROXY_PREFIX));
+        }
+        return className;
+    }
+
+    private String getMethodName(Method method) {
+        String methodName = method.getName();
+        if (methodName.contains(PROXY_PREFIX)) {
+            return methodName.substring(0, methodName.indexOf(PROXY_PREFIX));
+        }
+        return methodName;
+    }
+}
