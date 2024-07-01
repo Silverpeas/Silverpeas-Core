8b19f5260e036409566b11f02a952e92796083de
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2024-07-01 16:33:15 +0200

    In some context, when a newsletter has a rich content full of text, with the regexp to detect potential SQL injections, we can have false positive. This fix is to involve some regexp for SQL injections so that they can be more accurate to identify a possible injection attack and hence to reduce the false positives.

--- core-web/src/main/java/org/silverpeas/core/web/filter/MassiveWebSecurityFilter.java
+++ core-web/src/main/java/org/silverpeas/core/web/filter/MassiveWebSecurityFilter.java
@@ -90,9 +90,9 @@ public class MassiveWebSecurityFilter implements Filter {
 
   private static final Pattern SQL_SELECT_FROM_PATTERN = Pattern.compile("(?i)select.*from");
   private static final Pattern SQL_INSERT_VALUES_PATTERN =
-      Pattern.compile("(?i)insert( .*|.* )into.*values");
+      Pattern.compile("(?i)insert.*into.*values");
   private static final Pattern SQL_UPDATE_PATTERN = Pattern.compile("(?i)update.*set");
-  private static final Pattern SQL_DELETE_PATTERN = Pattern.compile("(?i)delete( .*|.* )from");
+  private static final Pattern SQL_DELETE_PATTERN = Pattern.compile("(?i)delete.*from");
   private static String sqlSelectPatternInspectDeeplyCacheKey = null;
 
 
@@ -113,10 +113,12 @@ public class MassiveWebSecurityFilter implements Filter {
 
     SQL_PATTERNS = new ArrayList<>(6);
     SQL_PATTERNS.add(
-        Pattern.compile("(?i)(grant|revoke)(( .*|.* )(select|insert|update|delete))+( .*|.* )on"));
+        Pattern.compile("(?i)(grant|revoke)((\\s+.*\\s*)(select|insert|update|delete))+(\\s+" +
+            ".*\\s+)on"));
     SQL_PATTERNS.add(
-        Pattern.compile("(?i)(grant|revoke)(( .*|.* )(references|alter|index|all))+( .*|.* )on"));
-    SQL_PATTERNS.add(Pattern.compile("(?i)(create|drop|alter)( .*|.* )(table|database|schema)"));
+        Pattern.compile("(?i)(grant|revoke)((\\s+.*\\s*)(references|alter|index|all))+(\\s+" +
+            ".*\\s+)on"));
+    SQL_PATTERNS.add(Pattern.compile("(?i)(create|drop|alter)(\\s+.*\\s*)(table|database|schema)"));
     SQL_PATTERNS.add(SQL_SELECT_FROM_PATTERN);
     SQL_PATTERNS.add(SQL_INSERT_VALUES_PATTERN);
     SQL_PATTERNS.add(SQL_UPDATE_PATTERN);
@@ -305,7 +307,7 @@ public class MassiveWebSecurityFilter implements Filter {
    *
    * @param matcherFound a pattern matcher
    * @param statement a SQL statement to check
-   * @return true of the SQL statement is considered as safe. False otherwise.
+   * @return true if the SQL statement is considered as safe. False otherwise.
    */
   private boolean verifySqlDeeply(final Matcher matcherFound, String statement) {
     boolean isVerified = true;
