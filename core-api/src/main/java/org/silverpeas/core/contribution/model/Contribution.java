7eac2329e0c1b00709f4abb202a2e6db64b9d4af
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2024-12-19 11:20:58 +0100

    Take profit of the AuthorizationRequestCache in Contribution#canBeAccessedBy(User) default implementation

--- core-api/src/main/java/org/silverpeas/core/contribution/model/Contribution.java
+++ core-api/src/main/java/org/silverpeas/core/contribution/model/Contribution.java
@@ -26,6 +26,7 @@ package org.silverpeas.core.contribution.model;
 import org.silverpeas.core.Instance;
 import org.silverpeas.core.SilverpeasResource;
 import org.silverpeas.core.admin.user.model.User;
+import org.silverpeas.core.security.AuthorizationRequestCache;
 import org.silverpeas.core.security.Securable;
 import org.silverpeas.core.security.authorization.ComponentAccessControl;
 import org.silverpeas.core.util.ResourcePath;
@@ -34,9 +35,10 @@ import java.io.Serializable;
 import java.util.Optional;
 
 /**
- * A contribution in Silverpeas. A contribution is an identifiable resource that is pushed by a
- * user onto Silverpeas and that is manageable by the users within Silverpeas. A contribution has
- * always a content that can be of any type (simple text, WYSIWYG, form, image, ...).
+ * A contribution in Silverpeas. A contribution is an identifiable resource that is pushed by a user
+ * onto Silverpeas and that is manageable by the users within Silverpeas. A contribution has always
+ * a content that can be of any type (simple text, WYSIWYG, form, image, ...).
+ *
  * @author mmoquillon
  */
 public interface Contribution extends SilverpeasResource, Serializable, Securable,
@@ -44,6 +46,7 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
 
   /**
    * Gets the unique identifier of this contribution.
+   *
    * @return the unique identifier of the contribution.
    */
   @SuppressWarnings("unchecked")
@@ -51,8 +54,9 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
 
   /**
    * Gets the title of this contribution if any. By default returns an empty String.
-   * @return the contribution's title in the specified language.
-   * Can be empty if no title was set or no title is defined for a such contribution.
+   *
+   * @return the contribution's title in the specified language. Can be empty if no title was set or
+   * no title is defined for a such contribution.
    */
   default String getTitle() {
     return "";
@@ -60,6 +64,7 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
 
   /**
    * Gets the name of this contribution. The name is in a contribution its title.
+   *
    * @return the contribution title.
    */
   default String getName() {
@@ -68,6 +73,7 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
 
   /**
    * Gets a description about this contribution if any. By default returns an empty String.
+   *
    * @return the description on this contribution. Can be empty if no description was set or no
    * description is defined for a such contribution.
    */
@@ -77,8 +83,9 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
 
   /**
    * Gets the type of this contribution. The type is a label that identifies uniquely a kind of
-   * contribution handled by a Silverpeas application.
-   * By default, this method returns the simple name of the class implementing this interface.
+   * contribution handled by a Silverpeas application. By default, this method returns the simple
+   * name of the class implementing this interface.
+   *
    * @return the label of the type of this contribution.
    */
   default String getContributionType() {
@@ -87,6 +94,7 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
 
   /**
    * Is this contribution indexable? By default true.
+   *
    * @return a boolean indicating if this contribution can be taken in charge by the Indexation
    * Engine. By default, any contribution in Silverpeas are indexable unless specified otherwise.
    */
@@ -97,22 +105,22 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
   /**
    * Is the specified user can access this contribution?
    * <p>
-   * By default {@link Securable#canBeAccessedBy(User)} is implemented so that a user can access
-   * a contribution if it has enough rights to access the application instance in which is
-   * managed this contribution.<br>
-   * Indeed, this behavior is mostly the common one.<br>
-   * But In the case the application instance distributes its contribution along of a
-   * categorization tree and the nodes of this tree support access rights, then the user must
-   * have also the rights to access the node to which belongs the content.<br>
-   * Of course it could exist other access rules...
+   * By default {@link Securable#canBeAccessedBy(User)} is implemented so that a user can access a
+   * contribution if it has enough rights to access the application instance in which is managed
+   * this contribution.<br> Indeed, this behavior is mostly the common one.<br> But In the case the
+   * application instance distributes its contribution along of a categorization tree and the nodes
+   * of this tree support access rights, then the user must have also the rights to access the node
+   * to which belongs the content.<br> Of course it could exist other access rules...
    * </p>
+   *
    * @param user a user in Silverpeas.
    * @return true if the user can access this content, false otherwise.
    */
   @Override
   default boolean canBeAccessedBy(final User user) {
-    return ComponentAccessControl.get()
-        .isUserAuthorized(user.getId(), getIdentifier().getComponentInstanceId());
+    return AuthorizationRequestCache.canBeAccessedBy(user, getIdentifier().getLocalId(),
+        u -> ComponentAccessControl.get()
+            .isUserAuthorized(user.getId(), getIdentifier().getComponentInstanceId()));
   }
 
   /**
@@ -120,6 +128,7 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
    * concrete type of this contribution. If not overridden, it returns by default a
    * {@link DefaultContributionModel} instance that access the business properties of the
    * contribution by reflection.
+   *
    * @return a {@link ContributionModel} object. By default, if this method isn't overridden, a
    * {@link DefaultContributionModel} instance is returned.
    */
@@ -130,8 +139,9 @@ public interface Contribution extends SilverpeasResource, Serializable, Securabl
   /**
    * Gets the {@link ResourcePath <T>} of the contribution represented by the given identifier.
    * <p>
-   *   Returns empty by default if not implemented.
+   * Returns empty by default if not implemented.
    * </p>
+   *
    * @param <T> the type the contribution.
    * @return the optional {@link ResourcePath<T>} instance.
    * @throws IllegalStateException when the type of the contribution is not handled by the
