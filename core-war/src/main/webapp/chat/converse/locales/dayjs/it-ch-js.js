116efc76af5ce90e91ca01d6970263e420a11ec6
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2025-03-07 09:21:33 +0100

    Upgrade ConverseJS to version 10.1.8 Upgrade the ConverseJS plugins we use: - action - jitsimeet - and screencast Because of the modification in the Plugin API of ConverseJS since version 8, our own plugin silverpeas-muc-invitations has been also modified to be compliant with the new Plugin API.

--- core-war/src/main/webapp/chat/converse/locales/dayjs/it-ch-js.js
+++ core-war/src/main/webapp/chat/converse/locales/dayjs/it-ch-js.js
@@ -1,9 +1,56 @@
 (self["webpackChunkconverse_js"] = self["webpackChunkconverse_js"] || []).push([[6010],{
 
-/***/ 3706:
+/***/ 2268:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
-!function(e,o){ true?module.exports=o(__webpack_require__(7484)):0}(this,(function(e){"use strict";function o(e){return e&&"object"==typeof e&&"default"in e?e:{default:e}}var n=o(e),t={name:"it-ch",weekdays:"domenica_lunedì_martedì_mercoledì_giovedì_venerdì_sabato".split("_"),months:"gennaio_febbraio_marzo_aprile_maggio_giugno_luglio_agosto_settembre_ottobre_novembre_dicembre".split("_"),weekStart:1,weekdaysShort:"dom_lun_mar_mer_gio_ven_sab".split("_"),monthsShort:"gen_feb_mar_apr_mag_giu_lug_ago_set_ott_nov_dic".split("_"),weekdaysMin:"do_lu_ma_me_gi_ve_sa".split("_"),ordinal:function(e){return e},formats:{LT:"HH:mm",LTS:"HH:mm:ss",L:"DD.MM.YYYY",LL:"D MMMM YYYY",LLL:"D MMMM YYYY HH:mm",LLLL:"dddd D MMMM YYYY HH:mm"},relativeTime:{future:"tra %s",past:"%s fa",s:"alcuni secondi",m:"un minuto",mm:"%d minuti",h:"un'ora",hh:"%d ore",d:"un giorno",dd:"%d giorni",M:"un mese",MM:"%d mesi",y:"un anno",yy:"%d anni"}};return n.default.locale(t,null,!0),t}));
+!function (e, o) {
+   true ? module.exports = o(__webpack_require__(2028)) : 0;
+}(this, function (e) {
+  "use strict";
+
+  function o(e) {
+    return e && "object" == typeof e && "default" in e ? e : {
+      default: e
+    };
+  }
+  var n = o(e),
+    t = {
+      name: "it-ch",
+      weekdays: "domenica_lunedì_martedì_mercoledì_giovedì_venerdì_sabato".split("_"),
+      months: "gennaio_febbraio_marzo_aprile_maggio_giugno_luglio_agosto_settembre_ottobre_novembre_dicembre".split("_"),
+      weekStart: 1,
+      weekdaysShort: "dom_lun_mar_mer_gio_ven_sab".split("_"),
+      monthsShort: "gen_feb_mar_apr_mag_giu_lug_ago_set_ott_nov_dic".split("_"),
+      weekdaysMin: "do_lu_ma_me_gi_ve_sa".split("_"),
+      ordinal: function (e) {
+        return e;
+      },
+      formats: {
+        LT: "HH:mm",
+        LTS: "HH:mm:ss",
+        L: "DD.MM.YYYY",
+        LL: "D MMMM YYYY",
+        LLL: "D MMMM YYYY HH:mm",
+        LLLL: "dddd D MMMM YYYY HH:mm"
+      },
+      relativeTime: {
+        future: "tra %s",
+        past: "%s fa",
+        s: "alcuni secondi",
+        m: "un minuto",
+        mm: "%d minuti",
+        h: "un'ora",
+        hh: "%d ore",
+        d: "un giorno",
+        dd: "%d giorni",
+        M: "un mese",
+        MM: "%d mesi",
+        y: "un anno",
+        yy: "%d anni"
+      }
+    };
+  return n.default.locale(t, null, !0), t;
+});
 
 /***/ })
 
