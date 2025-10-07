3ed3fbbbf13bbab4d867ff316c7b16ddb9ed14e4
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2025-10-07 10:09:31 +0200

    Feature #14962
    Add a new group implementation for the groups managed directly by a
    Silverpeas application (and not by the backoffice).
    Add a service with which the implementation of a Silverpeas component
    can use to manage such groups.

---
+++ core-configuration/src/main/config/migrations/db/mssql/busCore/047/init_production.sql
@@ -0,0 +1,80 @@
+insert into calendarCategory(categoryId, name) values (1, 'Réunion')
+;
+insert into calendarCategory(categoryId, name) values (2, 'Déplacement')
+;
+insert into calendarCategory(categoryId, name) values (3, 'Vacances')
+;
+insert into calendarCategory(categoryId, name) values (4, 'Personnel')
+;
+insert into calendarCategory(categoryId, name) values (5, 'Brain Storming')
+;
+insert into calendarCategory(categoryId, name) values (6, 'Formation')
+;
+
+insert into days(day) values ('01')
+;
+insert into days(day) values ('02')
+;
+insert into days(day) values ('03')
+;
+insert into days(day) values ('04')
+;
+insert into days(day) values ('05')
+;
+insert into days(day) values ('06')
+;
+insert into days(day) values ('07')
+;
+insert into days(day) values ('08')
+;
+insert into days(day) values ('09')
+;
+insert into days(day) values ('10')
+;
+insert into days(day) values ('11')
+;
+insert into days(day) values ('12')
+;
+insert into days(day) values ('13')
+;
+insert into days(day) values ('14')
+;
+insert into days(day) values ('15')
+;
+insert into days(day) values ('16')
+;
+insert into days(day) values ('17')
+;
+insert into days(day) values ('18')
+;
+insert into days(day) values ('19')
+;
+insert into days(day) values ('20')
+;
+insert into days(day) values ('21')
+;
+insert into days(day) values ('22')
+;
+insert into days(day) values ('23')
+;
+insert into days(day) values ('24')
+;
+insert into days(day) values ('25')
+;
+insert into days(day) values ('26')
+;
+insert into days(day) values ('27')
+;
+insert into days(day) values ('28')
+;
+insert into days(day) values ('29')
+;
+insert into days(day) values ('30')
+;
+insert into days(day) values ('31')
+;
+
+insert into ST_FormDesigner_Connectors
+	(ID, NAME, DESCRIPTION, DRIVER, URL, LOGIN, PASSWD, SQLQUERY, TYPE) values
+	(0, '________', ' ', ' ', ' ', ' ', ' ', ' ', '')
+;
