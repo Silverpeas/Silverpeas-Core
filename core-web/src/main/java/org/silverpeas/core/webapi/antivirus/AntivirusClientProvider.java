a31246491d368c23f49a552cbe5fd6f50183d93e
svuillet <svu@silvepeas.com>
2026-03-17 10:31:24 +0100

    add javadoc

--- core-web/src/main/java/org/silverpeas/core/webapi/antivirus/AntivirusClientProvider.java
+++ core-web/src/main/java/org/silverpeas/core/webapi/antivirus/AntivirusClientProvider.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2000 - 2026 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have received a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "https://www.silverpeas.org/legal/floss_exception.html"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.silverpeas.core.webapi.antivirus;
 
 import org.silverpeas.core.annotation.Provider;
@@ -8,6 +31,13 @@ import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import java.util.Optional;
 
+/**
+ * Provides an instance of {@link AntivirusClient} based on the application settings.
+ * <p>
+ * The provider checks if antivirus functionality is enabled and returns the client
+ * matching the configured name. If antivirus is disabled or no matching client is found,
+ * an empty {@link Optional} is returned.
+ */
 @Provider
 public class AntivirusClientProvider {
     @Inject
