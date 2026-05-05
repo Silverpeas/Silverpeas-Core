275f190b8f20ef798fe428ab5ac1081096267c6c
SilverDav <>
2026-05-05 18:10:24 +0200

    feature #15286 : Conversion of office documents is now possible by a remote saas service

--- core-library/src/main/java/org/silverpeas/core/contribution/converter/openoffice/OpenOfficeService.java
+++ core-library/src/main/java/org/silverpeas/core/contribution/converter/openoffice/OpenOfficeService.java
@@ -28,6 +28,8 @@ import org.jodconverter.core.office.OfficeException;
 import org.jodconverter.core.office.OfficeManager;
 import org.jodconverter.core.office.OfficeUtils;
 import org.jodconverter.local.office.LocalOfficeManager;
+import org.jodconverter.remote.office.RemoteOfficeManager;
+import org.jodconverter.remote.ssl.SslConfig;
 import org.silverpeas.core.annotation.Service;
 import org.silverpeas.core.initialization.Initialization;
 import org.silverpeas.kernel.SilverpeasRuntimeException;
@@ -53,6 +55,7 @@ public class OpenOfficeService implements Initialization {
       ".openoffice");
   private static final String OPENOFFICE_PORT = "openoffice.port";
   private static final String OPENOFFICE_HOME = "openoffice.home";
+  private static final String OPENOFFICE_HOST = "openoffice.host";
   private static final String OPENOFFICE_QUEUE_TIMEOUT = "openoffice.task.queueTimeout";
   private static final String OPENOFFICE_EXECUTION_TIMEOUT = "openoffice.task.executionTimeout";
 
@@ -61,7 +64,8 @@ public class OpenOfficeService implements Initialization {
   @Override
   public void init() {
     String home = settings.getString(OPENOFFICE_HOME, null);
-    String ports = settings.getString(OPENOFFICE_PORT, "8100");
+    String ports = settings.getString(OPENOFFICE_PORT, "");
+    String host = settings.getString(OPENOFFICE_HOST);
     long taskQueueTimeout = settings.getLong(OPENOFFICE_QUEUE_TIMEOUT, DEFAULT_TASK_QUEUE_TIMEOUT);
     long taskExecutionTimeout = settings.getLong(OPENOFFICE_EXECUTION_TIMEOUT,
         DEFAULT_TASK_EXECUTION_TIMEOUT);
@@ -69,17 +73,31 @@ public class OpenOfficeService implements Initialization {
         .map(String::trim)
         .mapToInt(Integer::parseInt)
         .toArray();
-    LocalOfficeManager.Builder config = LocalOfficeManager.builder()
+    switch (host)
+    {
+      case "":
+        LocalOfficeManager.Builder localBuilder = LocalOfficeManager.builder()
         .install()
         .officeHome(home)
         .portNumbers(portNumbers)
         .taskExecutionTimeout(taskExecutionTimeout)
         .taskQueueTimeout(taskQueueTimeout);
-    startOfficeManager(config);
+        officeManager = localBuilder.build();
+        break;
+      default:
+        final SslConfig sslConfig = new SslConfig();
+        sslConfig.setEnabled(true);
+        officeManager =
+            RemoteOfficeManager.builder()
+                .urlConnection(host)
+                .sslConfig(sslConfig)
+                .build();
+        break;
+    }
+    startOfficeManager();
   }
 
-  private void startOfficeManager(LocalOfficeManager.Builder builder) {
-    officeManager = builder.build();
+  private void startOfficeManager() {
     try {
       officeManager.start();
     } catch (OfficeException e) {
