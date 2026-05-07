104fb698e53876b92215c5d71c785b3eb38b0d58
SilverDav <>
2026-05-07 16:53:06 +0200

    default local port 8100 (previously an error was encountered if it was empty)

--- core-library/src/main/java/org/silverpeas/core/contribution/converter/openoffice/OpenOfficeService.java
+++ core-library/src/main/java/org/silverpeas/core/contribution/converter/openoffice/OpenOfficeService.java
@@ -62,7 +62,7 @@ public class OpenOfficeService implements Initialization {
   @Override
   public void init() throws Exception {
     String home = settings.getString(OPENOFFICE_HOME, null);
-    String ports = settings.getString(OPENOFFICE_PORT, "");
+    String ports = settings.getString(OPENOFFICE_PORT, "8100");
     String host = settings.getString(OPENOFFICE_HOST, "");
     long taskQueueTimeout = settings.getLong(OPENOFFICE_QUEUE_TIMEOUT, DEFAULT_TASK_QUEUE_TIMEOUT);
     long taskExecutionTimeout = settings.getLong(OPENOFFICE_EXECUTION_TIMEOUT,
