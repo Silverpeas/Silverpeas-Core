f718b3878b6923746cf1f1be8d388924deff8f1d
Sébastien Vuillet <sebastien.vuillet@silverpeas.com>
2026-09-22 23:43:46 +0200

    Feature #13707: add two-factor recovery codes REST URI

---
+++ core-web/src/main/java/org/silverpeas/core/webapi/twofactor/TwoFactorAuthenticationResourceURIs.java
@@ -0,0 +1,10 @@
+package org.silverpeas.core.webapi.twofactor;
+
+/** Base URIs for the two-factor authentication REST resource. */
+public final class TwoFactorAuthenticationResourceURIs {
+  public static final String BASE_URI = "two-factor";
+  public static final String RECOVERY_CODES_URI_PART = "recovery-codes";
+
+  private TwoFactorAuthenticationResourceURIs() {
+  }
+}
