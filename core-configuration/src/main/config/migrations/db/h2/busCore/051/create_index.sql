e80896dddac6d1ba69f87b3b74d60414ca1ea653
Sébastien Vuillet <sebastien.vuillet@silverpeas.com>
2026-09-22 22:36:24 +0200

    Feature #13707: add 051 recovery code schema

---
+++ core-configuration/src/main/config/migrations/db/h2/busCore/051/create_index.sql
@@ -0,0 +1,5 @@
+CREATE INDEX IDX_SB_CONTRIBUTION_TRACKING_CONTRIBUTION
+    ON SB_Contribution_Tracking (contrib_id, contrib_type, contrib_instanceId);
+
+CREATE INDEX IDX_User_2FA_Recovery_User
+    ON ST_User_2FA_Recovery (userId);
