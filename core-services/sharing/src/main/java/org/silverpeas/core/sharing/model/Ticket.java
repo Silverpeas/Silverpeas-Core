d2ec8450226616c3781dbcf8445a8540a60319c9
Sébastien Vuillet <sebastien.vuillet@silverpeas.com>
2026-09-24 17:22:12 +0200

    Introduce Ticket builder API

--- core-services/sharing/src/main/java/org/silverpeas/core/sharing/model/Ticket.java
+++ core-services/sharing/src/main/java/org/silverpeas/core/sharing/model/Ticket.java
@@ -85,22 +85,71 @@ public abstract class Ticket extends BasicJpaEntity<Ticket, UuidIdentifier>
   protected Ticket() {
   }
 
-  protected Ticket(int sharedObjectId, String componentId, UserDetail creator, Date creationDate,
-                   Date endDate, int nbAccessMax, String securityCode) {
-    this(sharedObjectId, componentId, creator.getId(), creationDate, endDate, nbAccessMax, securityCode);
+  protected Ticket(TicketDetail detail) {
+    this.sharedObjectId = detail.getSharedObjectId();
+    this.componentId = detail.getComponentId();
+    this.creatorId = detail.getCreatorId();
+    this.creationDate = detail.getCreationDate().getTime();
+    if (detail.getEndDate() != null) {
+      this.endDate = detail.getEndDate().getTime();
+    }
+    this.nbAccessMax = detail.getNbAccessMax();
+    this.securityCode = detail.getSecurityCode();
+    if (StringUtil.isDefined(detail.getToken())) {
+      setId(detail.getToken());
+    }
   }
 
-  protected Ticket(int sharedObjectId, String componentId, String creatorId, Date creationDate,
-                   Date endDate, int nbAccessMax, String securityCode) {
-    this.sharedObjectId = sharedObjectId;
-    this.componentId = componentId;
-    this.creatorId = creatorId;
-    this.creationDate = creationDate.getTime();
-    if (endDate != null) {
-      this.endDate = endDate.getTime();
+  public abstract static class Builder<B extends Builder<B>> {
+    protected final TicketDetail.Builder detail = TicketDetail.builder();
+
+    protected abstract B self();
+
+    public B setSharedObjectId(long sharedObjectId) {
+      detail.setSharedObjectId(sharedObjectId);
+      return self();
     }
-    this.nbAccessMax = nbAccessMax;
-    this.securityCode = securityCode;
+
+    public B setComponentId(String componentId) {
+      detail.setComponentId(componentId);
+      return self();
+    }
+
+    public B setCreatorId(String creatorId) {
+      detail.setCreatorId(creatorId);
+      return self();
+    }
+
+    public B setCreationDate(Date creationDate) {
+      detail.setCreationDate(creationDate);
+      return self();
+    }
+
+    public B setEndDate(Date endDate) {
+      detail.setEndDate(endDate);
+      return self();
+    }
+
+    public B setNbAccessMax(int nbAccessMax) {
+      detail.setNbAccessMax(nbAccessMax);
+      return self();
+    }
+
+    public B setSecurityCode(String securityCode) {
+      detail.setSecurityCode(securityCode);
+      return self();
+    }
+
+    public B setToken(String token) {
+      detail.setToken(token);
+      return self();
+    }
+
+    protected TicketDetail buildDetail(String type) {
+      return detail.setSharedObjectType(type).build();
+    }
+
+    public abstract Ticket build();
   }
 
   public long getSharedObjectId() {
